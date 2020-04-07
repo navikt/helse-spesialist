a0e38570f16014201bdd57323a0fcf5693e3477e
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-04-07 18:58:47 +0200

    Ignorerer midertidig duplikate spleisbehov part 3: the plot twist

--- src/main/kotlin/no/nav/helse/mediator/kafka/SpleisbehovMediator.kt
+++ src/main/kotlin/no/nav/helse/mediator/kafka/SpleisbehovMediator.kt
@@ -30,7 +30,7 @@ internal class SpleisbehovMediator(
     private val log = LoggerFactory.getLogger(SpleisbehovMediator::class.java)
 
     internal fun håndter(godkjenningMessage: GodkjenningMessage) {
-        if (spleisbehovDao.findBehov(godkjenningMessage.id) == null) {
+        if (spleisbehovDao.findBehov(godkjenningMessage.id) != null) {
             log.warn(
                 "Mottok duplikat godkjenning behov, {}, {}",
                 keyValue("spleisBehovId", godkjenningMessage.id),
