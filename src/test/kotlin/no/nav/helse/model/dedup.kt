3b70ef69ae9365d6c5f0c1f59dadc609a1c7c711
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-04-08 15:17:07 +0200

    Legger til tjenestesteder

--- src/test/kotlin/no/nav/helse/model/dedup.kt
+++ src/test/kotlin/no/nav/helse/model/dedup.kt
@@ -2,12 +2,28 @@ package no.nav.helse.model
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.util.regex.Pattern
+
+val pattern = Pattern.compile(".+\\((\\d+),")
+fun findNr(input: String) = pattern.matcher(input).let {
+    it.find()
+    it.group(1).toInt()
+}
 
 fun main() {
-    val initial = Files.readAllLines(Paths.get("src/main/resources/db/migration/V2__legg_til_enheter.sql"))
-    val full = Files.readAllLines(Paths.get("src/main/resources/db/migration/V12__utgåtte_enheter.sql_original"))
+    val a = Files.readAllLines(Paths.get("src/main/resources/db/migration/V2__legg_til_enheter.sql"))
+    val b = Files.readAllLines(Paths.get("src/main/resources/db/migration/V12__utgåtte_enheter.sql"))
+    val enheter = (a + b).map(::findNr)
+    println(enheter.sorted().distinct())
 
-    val result = full.filter { !initial.contains(it) }
+    val ny = Files.readAllLines(Paths.get("src/main/resources/db/migration/V13__tjenestesteder.sql_original"))
+    println(ny.sorted().distinct())
+
+    val result = ny.map { nyEnhet -> nyEnhet to findNr(nyEnhet) }
+        .filterNot { enheter.contains(it.second) }
+        .sortedBy { it.second }
+        .map { it.first }
+    //result.forEach(::println)
+    //println(result.size)
     result.forEach(::println)
-    println(result.size)
 }
