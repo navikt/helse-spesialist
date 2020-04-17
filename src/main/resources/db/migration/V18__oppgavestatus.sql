21a623719380748c5bc71906abc58fbf671ab723
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-04-17 14:46:32 +0200

    Skriver om status på oppgave så den matcher enum
    Co-authored-by: Marthe Skogen <Marthe.skogen@nav.no>

--- src/main/resources/db/migration/V18__oppgavestatus.sql
+++ src/main/resources/db/migration/V18__oppgavestatus.sql
@@ -6,9 +6,13 @@ UPDATE oppgave SET ferdigstilt_av='System' WHERE status='System' AND ferdigstilt
 ALTER TABLE oppgave ALTER COLUMN status TYPE VARCHAR(64);
 DROP TYPE løsningstype_type;
 CREATE TYPE oppgavestatus AS ENUM('AvventerSystem', 'AvventerSaksbehandler', 'Ferdigstilt', 'Invalidert');
+
+UPDATE oppgave SET status='AvventerSystem' WHERE status='System';
+UPDATE oppgave SET status='AvventerSaksbehandler' WHERE status='Saksbehandler';
+UPDATE oppgave SET status='Ferdigstilt' WHERE ferdigstilt IS NOT NULL;
+
 ALTER TABLE oppgave ALTER COLUMN status TYPE oppgavestatus USING (status::oppgavestatus);
 
-UPDATE oppgave SET status='Ferdigstilt'::oppgavestatus WHERE ferdigstilt IS NOT NULL;
 ALTER TABLE oppgave RENAME COLUMN ferdigstilt TO oppdatert;
 
 UPDATE oppgave SET oppdatert=now() WHERE oppdatert IS NULL;
