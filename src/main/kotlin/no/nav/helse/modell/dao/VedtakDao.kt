ddbf471a820bf69030732ed2f7e04f4e43b40622
Martin Solheim <martin.solheim@me.com>
2020-04-30 13:25:41 +0200

    Forbedrer sql query
    Co-authored-by: Marte Tårnes <marte.tarnes@nav.no>

--- src/main/kotlin/no/nav/helse/modell/dao/VedtakDao.kt
+++ src/main/kotlin/no/nav/helse/modell/dao/VedtakDao.kt
@@ -38,7 +38,7 @@ internal class VedtakDao(private val dataSource: DataSource) {
     internal fun findVedtakByPersonRef(personRef: Int): VedtakDto? =
         using(sessionOf(dataSource)) { session ->
             session.run(
-                queryOf("SELECT * FROM vedtak WHERE id = (SELECT MAX(id) FROM vedtak WHERE person_ref=?);", personRef)
+                queryOf("SELECT * FROM vedtak WHERE person_ref=? ORDER BY id DESC LIMIT 1;", personRef)
                     .map (::vedtakDto)
                     .asSingle
             )
