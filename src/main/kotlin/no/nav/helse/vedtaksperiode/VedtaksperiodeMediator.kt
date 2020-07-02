aa07373291bb46ccbfbff650d94c2ae0f44f58c3
Martin Solheim <martin.solheim@me.com>
2020-07-02 10:58:47 +0200

    Terminerer uendelig session
    Co-authored-by: Kevin Sillerud <kevin.sillerud@gmail.com>
    Co-authored-by: Anders Østby <anders.ostby@nav.no>

--- src/main/kotlin/no/nav/helse/vedtaksperiode/VedtaksperiodeMediator.kt
+++ src/main/kotlin/no/nav/helse/vedtaksperiode/VedtaksperiodeMediator.kt
@@ -2,6 +2,7 @@ package no.nav.helse.vedtaksperiode
 
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.module.kotlin.readValue
+import kotliquery.Session
 import kotliquery.sessionOf
 import no.nav.helse.measureAsHistogram
 import no.nav.helse.modell.arbeidsgiver.findArbeidsgiver
@@ -15,67 +16,74 @@ import no.nav.helse.objectMapper
 import java.util.*
 import javax.sql.DataSource
 
-internal class VedtaksperiodeMediator(
-    dataSource: DataSource
-) {
-    private val session = sessionOf(dataSource, returnGeneratedKey = true)
+internal class VedtaksperiodeMediator(val dataSource: DataSource) {
     fun byggSpeilSnapshotForFnr(fnr: String) =
-        measureAsHistogram("byggSpeilSnapshotForFnr") { session.findVedtakByFnr(fnr) }?.let(::byggSpeilSnapshot)
-
-    fun byggSpeilSnapshotForAktørId(aktørId: String) =
-        measureAsHistogram("byggSpeilSnapshotForAktørId") { session.findVedtakByAktørId(aktørId) }?.let(::byggSpeilSnapshot)
-
-    fun byggSpeilSnapshotForVedtaksperiodeId(vedtaksperiodeId: UUID) =
-        measureAsHistogram("byggSpeilSnapshotForVedtaksperiodeId") {
-            session.findVedtakByVedtaksperiodeId(vedtaksperiodeId)
-        }?.let(::byggSpeilSnapshot)
-
-    private fun byggSpeilSnapshot(vedtak: VedtaksperiodeDto) = measureAsHistogram("byggSpeilSnapshot") {
-        val arbeidsgiverDto = measureAsHistogram("byggSpeilSnapshot_findArbeidsgiver") {
-            requireNotNull(session.findArbeidsgiver(vedtak.arbeidsgiverRef)) { "Fant ikke arbeidsgiver" }
-        }
-        val infotrygdutbetalinger = measureAsHistogram("byggSpeilSnapshot_findInfotrygdutbetalinger") {
-            session.findInfotrygdutbetalinger(vedtak.fødselsnummer.toLong())?.let { objectMapper.readTree(it) }
-        }
-        val speilSnapshot = measureAsHistogram("byggSpeilSnapshot_findSpeilSnapshot") {
-            requireNotNull(session.findSpeilSnapshot(vedtak.speilSnapshotRef)) { "Fant ikke speilSnapshot" }
-                .let { objectMapper.readValue<PersonFraSpleisDto>(it) }
-        }
-        val arbeidsgivere = speilSnapshot.arbeidsgivere.map {
-            val arbeidsgivernavn =
-                if (it.organisasjonsnummer == arbeidsgiverDto.organisasjonsnummer)
-                    arbeidsgiverDto.navn
-                else
-                    "Ikke tilgjengelig"
-            ArbeidsgiverForSpeilDto(
-                organisasjonsnummer = it.organisasjonsnummer,
-                navn = arbeidsgivernavn,
-                id = it.id,
-                vedtaksperioder = it.vedtaksperioder
-            )
-        }
-        measureAsHistogram("byggSpeilSnapshot_behovForVedtaksperiode_akkumulert") {
-            speilSnapshot.arbeidsgivere.forEach { arbeidsgiver ->
-                arbeidsgiver.vedtaksperioder.forEach { vedtaksperiode ->
-                    val vedtaksperiodeId = UUID.fromString(vedtaksperiode["id"].asText())
-                    val eventId = session.eventIdForVedtaksperiode(vedtaksperiodeId)
-                    vedtaksperiode as ObjectNode
-                    vedtaksperiode.put("oppgavereferanse", eventId?.toString())
-                }
+        measureAsHistogram("byggSpeilSnapshotForFnr") {
+            sessionOf(dataSource).use { session ->
+                session.findVedtakByFnr(fnr)?.let { byggSpeilSnapshot(session, it) }
             }
         }
 
-        val enhet = measureAsHistogram("byggSpeilSnapshot_findEnhet") {
-            session.findEnhet(vedtak.fødselsnummer.toLong())
+    fun byggSpeilSnapshotForAktørId(aktørId: String) =
+        measureAsHistogram("byggSpeilSnapshotForAktørId") {
+            sessionOf(dataSource).use { session ->
+                session.findVedtakByAktørId(aktørId)?.let { byggSpeilSnapshot(session, it) }
+            }
         }
-        PersonForSpeilDto(
-            aktørId = speilSnapshot.aktørId,
-            fødselsnummer = speilSnapshot.fødselsnummer,
-            navn = NavnDto(vedtak.fornavn, vedtak.mellomnavn, vedtak.etternavn),
-            arbeidsgivere = arbeidsgivere,
-            infotrygdutbetalinger = infotrygdutbetalinger,
-            enhet = enhet
-        )
-    }
 
+    fun byggSpeilSnapshotForVedtaksperiodeId(vedtaksperiodeId: UUID) =
+        measureAsHistogram("byggSpeilSnapshotForVedtaksperiodeId") {
+            sessionOf(dataSource).use { session ->
+                session.findVedtakByVedtaksperiodeId(vedtaksperiodeId)?.let { byggSpeilSnapshot(session, it) }
+            }
+        }
+
+    private fun byggSpeilSnapshot(session: Session, vedtak: VedtaksperiodeDto) =
+        measureAsHistogram("byggSpeilSnapshot") {
+            val arbeidsgiverDto = measureAsHistogram("byggSpeilSnapshot_findArbeidsgiver") {
+                requireNotNull(session.findArbeidsgiver(vedtak.arbeidsgiverRef)) { "Fant ikke arbeidsgiver" }
+            }
+            val infotrygdutbetalinger = measureAsHistogram("byggSpeilSnapshot_findInfotrygdutbetalinger") {
+                session.findInfotrygdutbetalinger(vedtak.fødselsnummer.toLong())?.let { objectMapper.readTree(it) }
+            }
+            val speilSnapshot = measureAsHistogram("byggSpeilSnapshot_findSpeilSnapshot") {
+                requireNotNull(session.findSpeilSnapshot(vedtak.speilSnapshotRef)) { "Fant ikke speilSnapshot" }
+                    .let { objectMapper.readValue<PersonFraSpleisDto>(it) }
+            }
+            val arbeidsgivere = speilSnapshot.arbeidsgivere.map {
+                val arbeidsgivernavn =
+                    if (it.organisasjonsnummer == arbeidsgiverDto.organisasjonsnummer)
+                        arbeidsgiverDto.navn
+                    else
+                        "Ikke tilgjengelig"
+                ArbeidsgiverForSpeilDto(
+                    organisasjonsnummer = it.organisasjonsnummer,
+                    navn = arbeidsgivernavn,
+                    id = it.id,
+                    vedtaksperioder = it.vedtaksperioder
+                )
+            }
+            measureAsHistogram("byggSpeilSnapshot_behovForVedtaksperiode_akkumulert") {
+                speilSnapshot.arbeidsgivere.forEach { arbeidsgiver ->
+                    arbeidsgiver.vedtaksperioder.forEach { vedtaksperiode ->
+                        val vedtaksperiodeId = UUID.fromString(vedtaksperiode["id"].asText())
+                        val eventId = session.eventIdForVedtaksperiode(vedtaksperiodeId)
+                        vedtaksperiode as ObjectNode
+                        vedtaksperiode.put("oppgavereferanse", eventId?.toString())
+                    }
+                }
+            }
+
+            val enhet = measureAsHistogram("byggSpeilSnapshot_findEnhet") {
+                session.findEnhet(vedtak.fødselsnummer.toLong())
+            }
+            PersonForSpeilDto(
+                aktørId = speilSnapshot.aktørId,
+                fødselsnummer = speilSnapshot.fødselsnummer,
+                navn = NavnDto(vedtak.fornavn, vedtak.mellomnavn, vedtak.etternavn),
+                arbeidsgivere = arbeidsgivere,
+                infotrygdutbetalinger = infotrygdutbetalinger,
+                enhet = enhet
+            )
+        }
 }
