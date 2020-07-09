93e44e3a0a9d9d21fb3acacc736ddd8ea8eafea0
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-07-09 15:20:46 +0200

    Invaliderer oppgaver som er stuck i AvventerSaksbehandler status
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

---
+++ src/main/resources/db/migration/V38__invalider_stuck_oppgaver.sql
@@ -0,0 +1,7 @@
+UPDATE oppgave SET status='Invalidert'::oppgavestatus WHERE id=1031677;
+UPDATE oppgave SET status='Invalidert'::oppgavestatus WHERE id=414516;
+UPDATE oppgave SET status='Invalidert'::oppgavestatus WHERE id=397691;
+UPDATE oppgave SET status='Invalidert'::oppgavestatus WHERE id=387893;
+UPDATE oppgave SET status='Invalidert'::oppgavestatus WHERE id=230240;
+UPDATE oppgave SET status='Invalidert'::oppgavestatus WHERE id=189014;
+UPDATE oppgave SET status='Invalidert'::oppgavestatus WHERE id=181946;
