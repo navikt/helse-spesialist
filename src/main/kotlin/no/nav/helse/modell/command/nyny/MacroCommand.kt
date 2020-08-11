adb3a3957423778fcfd2d842258c314fa160d5ab
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2020-08-11 11:41:03 +0200

    Tar vare på hierarki av nåværende kommandoer i en kjede
    Co-authored-by: David Steinsland <david@steinsland.email>

--- src/main/kotlin/no/nav/helse/modell/command/nyny/MacroCommand.kt
+++ src/main/kotlin/no/nav/helse/modell/command/nyny/MacroCommand.kt
@@ -1,15 +1,24 @@
 package no.nav.helse.modell.command.nyny
 
 internal abstract class MacroCommand : Command() {
+    private val indices: MutableList<Int> = mutableListOf()
     private var currentIndex: Int = 0
     protected abstract val commands: List<Command>
 
-    final override fun execute() : Boolean {
-        require(commands.isNotEmpty())
+    private fun execute(indices: MutableList<Int>): Boolean {
         commands.listIterator(currentIndex).forEach {
-            if(!it.execute()) return false
+            val executeOk = if (it is MacroCommand) it.execute(indices) else it.execute()
+            if(!executeOk) return false.also { indices.add(currentIndex) }
             currentIndex += 1
         }
         return true
+    }
+
+    internal fun state() = indices.reversed().toList()
+
+    final override fun execute() : Boolean {
+        require(commands.isNotEmpty())
+        indices.clear()
+        return execute(this.indices)
     }
 }
--- src/test/kotlin/no/nav/helse/modell/command/nyny/MacroCommandTest.kt
+++ src/test/kotlin/no/nav/helse/modell/command/nyny/MacroCommandTest.kt
@@ -7,45 +7,65 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 internal class MacroCommandTest {
-    private val testConstants: MutableList<String> = mutableListOf()
+    private val constants: MutableList<String> = mutableListOf()
 
     @BeforeEach
     fun beforeEach() {
-        testConstants.clear()
+        constants.clear()
     }
 
     @Test
     fun `Kommandoer utføres i rekkefølge`() {
-        val command1 = command { testConstants.add("Kommando A"); true }
-        val command2 = command { testConstants.add("Kommando B"); true }
+        val command1 = command { constants.add("Kommando A"); true }
+        val command2 = command { constants.add("Kommando B"); true }
         val macroCommand = command1 + command2
         assertTrue(macroCommand.execute())
         assertRekkefølge("Kommando A", "Kommando B")
+        assertTrue(macroCommand.state().isEmpty())
     }
 
     @Test
     fun `Kommandoer kan suspenderes`() {
-        val command1 = command { testConstants.add("Kommando A"); false }
-        val command2 = command { testConstants.add("Kommando B"); true }
+        val command1 = command { constants.add("Kommando A"); false }
+        val command2 = command { constants.add("Kommando B"); true }
         val macroCommand = command1 + command2
         assertFalse(macroCommand.execute())
         assertRekkefølge("Kommando A")
+        assertEquals(listOf(0), macroCommand.state())
     }
 
     @Test
     fun `Kommandoer kan fortsette`() {
         var Ok = false
-        val command1 = command { testConstants.add("Kommando A ${if(!Ok) "Før" else "Etter"}"); Ok }
-        val command2 = command { testConstants.add("Kommando B"); true }
+        val command1 = command { constants.add("Kommando A ${if(!Ok) "Før" else "Etter"}"); Ok }
+        val command2 = command { constants.add("Kommando B"); true }
         val macroCommand = command1 + command2
         macroCommand.execute()
         Ok = true
         assertTrue(macroCommand.execute())
         assertRekkefølge("Kommando A Før", "Kommando A Etter", "Kommando B")
+        assertTrue(macroCommand.state().isEmpty())
+    }
+
+    @Test
+    fun `Nestede makrokommandoer`() {
+        var ok = false
+        val command1 = command { constants.add("A"); true }
+        val macroCommand1 = command { constants.add("B"); ok } + command { constants.add("C"); !ok }
+        val macroCommand2 = command1 + macroCommand1
+        macroCommand2.execute()
+        assertEquals(listOf(1, 0), macroCommand2.state())
+        ok = true
+        macroCommand2.execute()
+        assertEquals(listOf(1, 1), macroCommand2.state())
+        ok = false
+        macroCommand2.execute()
+        assertTrue(macroCommand2.state().isEmpty())
+        assertRekkefølge("A", "B", "B", "C", "C")
     }
 
     private fun assertRekkefølge(vararg konstanter: String) {
-        assertEquals(konstanter.toList(), testConstants)
+        assertEquals(konstanter.toList(), constants)
     }
 
     private operator fun Command.plus(other: Command): MacroCommand {
