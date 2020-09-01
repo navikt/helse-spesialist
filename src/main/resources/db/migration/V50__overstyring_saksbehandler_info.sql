f506fb91a399e01fcb982f57803e76d5dcb93b28
Kevin Sillerud <kevin.sillerud@gmail.com>
2020-09-01 10:34:37 +0200

    Tømmer overstyrte dager siden overstyring kan ha fler dager
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- src/main/resources/db/migration/V50__overstyring_saksbehandler_info.sql
+++ src/main/resources/db/migration/V50__overstyring_saksbehandler_info.sql
@@ -1,5 +1,6 @@
 CREATE TABLE saksbehandler(oid UUID NOT NULL PRIMARY KEY, navn VARCHAR(64), epost VARCHAR(128));
 
+DELETE FROM overstyrtdag;
 DELETE FROM overstyring;
 ALTER TABLE overstyring
     ADD COLUMN saksbehandler_ref UUID NOT NULL REFERENCES saksbehandler(oid);
