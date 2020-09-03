0d664107465845d008e91ef6f9eadc21d436c68d
David Steinsland <david@steinsland.email>
2020-09-03 10:51:58 +0200

    Tester OpprettPersonCommand

---
+++ src/main/kotlin/no/nav/helse/modell/command/nyny/OpprettPersonCommand.kt
@@ -0,0 +1,61 @@
+package no.nav.helse.modell.command.nyny
+
+import no.nav.helse.modell.person.HentEnhetLøsning
+import no.nav.helse.modell.person.HentInfotrygdutbetalingerLøsning
+import no.nav.helse.modell.person.HentPersoninfoLøsning
+import no.nav.helse.modell.person.PersonDao
+import org.slf4j.LoggerFactory
+import java.time.LocalDate
+
+internal class OpprettPersonCommand(
+    private val fødselsnummer: String,
+    private val aktørId: String,
+    private val personDao: PersonDao
+) : Command {
+
+    private companion object {
+        private val log = LoggerFactory.getLogger(OpprettPersonCommand::class.java)
+    }
+
+    override fun execute(context: CommandContext): Boolean {
+        if (personDao.findPersonByFødselsnummer(fødselsnummer.toLong()) != null) return ignorer()
+        return behandle(context)
+    }
+
+    override fun resume(context: CommandContext): Boolean {
+        return behandle(context)
+    }
+
+    private fun ignorer(): Boolean {
+        log.info("Person finnes fra før")
+        return true
+    }
+
+    private fun behandle(context: CommandContext): Boolean {
+        val enhet = context.get<HentEnhetLøsning>() ?: return trengerMerInformasjon(context)
+        val personinfo = context.get<HentPersoninfoLøsning>() ?: return trengerMerInformasjon(context)
+        val infotrygdutbetalinger = context.get<HentInfotrygdutbetalingerLøsning>() ?: return trengerMerInformasjon(context)
+        return opprettPerson(enhet, personinfo, infotrygdutbetalinger)
+    }
+
+    // TODO: om CommandContext persisteres sammen med løsningene så kunne vi bare
+    // ha trengt å bedt om det som faktisk manglet
+    private fun trengerMerInformasjon(context: CommandContext): Boolean {
+        log.info("Trenger mer informasjon for å opprette person")
+        context.behov("HentPersoninfo")
+        context.behov("HentEnhet")
+        context.behov("HentInfotrygdutbetalinger", mapOf(
+            "historikkFom" to LocalDate.now().minusYears(3),
+            "historikkTom" to LocalDate.now()
+        ))
+        return false
+    }
+
+    private fun opprettPerson(enhet: HentEnhetLøsning, personinfo: HentPersoninfoLøsning, infotrygdutbetalinger: HentInfotrygdutbetalingerLøsning): Boolean {
+        log.info("Oppretter person")
+        val navnId = personinfo.lagre(personDao)
+        val infotrygdutbetalingerId = infotrygdutbetalinger.lagre(personDao)
+        enhet.lagrePerson(personDao, fødselsnummer, aktørId, navnId, infotrygdutbetalingerId)
+        return true
+    }
+}
