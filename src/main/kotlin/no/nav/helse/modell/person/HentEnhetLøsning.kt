8edf591fbf2ea937bdf2542ecf474b6e1e5dc25e
David Steinsland <david@steinsland.email>
2020-09-03 09:49:47 +0200

    lagre person og oppdatere enhet i HentEnhetLøsning

--- src/main/kotlin/no/nav/helse/modell/person/HentEnhetLøsning.kt
+++ src/main/kotlin/no/nav/helse/modell/person/HentEnhetLøsning.kt
@@ -1,3 +1,16 @@
 package no.nav.helse.modell.person
 
-internal class HentEnhetLøsning(internal val enhetNr: String)
+internal class HentEnhetLøsning(internal val enhetNr: String) {
+
+    internal fun lagrePerson(personDao: PersonDao, fødselsnummer: String, aktørId: String, navnId: Int, infotrygdutbetalingerId: Int) =
+        personDao.insertPerson(
+            fødselsnummer = fødselsnummer.toLong(),
+            aktørId = aktørId.toLong(),
+            navnId = navnId,
+            enhetId = enhetNr.toInt(),
+            infotrygdutbetalingerId = infotrygdutbetalingerId
+        )
+
+    fun oppdater(personDao: PersonDao, fødselsnummer: String) =
+        personDao.updateEnhet(fødselsnummer.toLong(), enhetNr.toInt())
+}
---
+++ src/test/kotlin/no/nav/helse/modell/person/HentEnhetLøsningTest.kt
@@ -0,0 +1,32 @@
+package no.nav.helse.modell.person
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+
+internal class HentEnhetLøsningTest {
+    private companion object {
+        private const val FNR = "12345678911"
+        private const val AKTØR = "4321098765432"
+        private const val ENHET_OSLO = "0301"
+        private const val ENHET_SVALBARD = "2100"
+        private const val NAVN_REF = 1
+        private const val INFOTRYGDUTBETALINGER_REF = 2
+    }
+
+    private val dao = mockk<PersonDao>(relaxed = true)
+
+    @Test
+    fun `lagre person`() {
+        val enhet = HentEnhetLøsning(ENHET_OSLO)
+        enhet.lagrePerson(dao, FNR, AKTØR, NAVN_REF, INFOTRYGDUTBETALINGER_REF)
+        verify(exactly = 1) { dao.insertPerson(FNR.toLong(), AKTØR.toLong(), NAVN_REF, ENHET_OSLO.toInt(), INFOTRYGDUTBETALINGER_REF) }
+    }
+
+    @Test
+    fun `oppdatere enhet`() {
+        val enhet = HentEnhetLøsning(ENHET_SVALBARD)
+        enhet.oppdater(dao, FNR)
+        verify(exactly = 1) { dao.updateEnhet(FNR.toLong(), ENHET_SVALBARD.toInt()) }
+    }
+}
