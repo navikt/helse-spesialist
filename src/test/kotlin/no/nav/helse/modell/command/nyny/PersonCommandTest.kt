57ea1d8b781199d18f2c4fb961c38e69f6f9b66d
David Steinsland <david@steinsland.email>
2020-09-03 11:29:41 +0200

    samle OpprettPerson og OppdaterPerson i en kommando

---
+++ src/test/kotlin/no/nav/helse/modell/command/nyny/PersonCommandTest.kt
@@ -0,0 +1,134 @@
+package no.nav.helse.modell.command.nyny
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.helse.modell.person.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.util.*
+
+internal class PersonCommandTest {
+    private companion object {
+        private const val FNR = "12345678911"
+        private const val AKTØR = "4321098765432"
+        private const val FORNAVN = "KARI"
+        private const val MELLOMNAVN = "Mellomnavn"
+        private const val ETTERNAVN = "Nordmann"
+        private const val ENHET_OSLO = "0301"
+        private val FØDSELSDATO = LocalDate.EPOCH
+        private val KJØNN = Kjønn.Kvinne
+
+        private val objectMapper = jacksonObjectMapper()
+    }
+
+    private val dao = mockk<PersonDao>(relaxed = true)
+    private val command = PersonCommand(FNR, AKTØR, dao)
+    private lateinit var context: CommandContext
+
+    @BeforeEach
+    fun setup() {
+        context = CommandContext(UUID.randomUUID())
+        clearMocks(dao)
+    }
+
+    @Test
+    fun `oppretter person`() {
+        personFinnesIkke()
+        context.add(HentPersoninfoLøsning(FORNAVN, MELLOMNAVN, ETTERNAVN, FØDSELSDATO, KJØNN))
+        context.add(HentEnhetLøsning(ENHET_OSLO))
+        context.add(HentInfotrygdutbetalingerLøsning(objectMapper.createObjectNode()))
+        assertTrue(command.execute(context))
+        assertFalse(context.harBehov())
+        verify(exactly = 1) { dao.insertPerson(FNR.toLong(), AKTØR.toLong(), any(), any(), any()) }
+    }
+
+    @Test
+    fun `person finnes, men personinfo er utdatert`() {
+        personFinnes()
+        altUtdatert()
+        assertFalse(command.execute(context))
+        assertHarBehov(listOf("HentPersoninfo"))
+    }
+
+    @Test
+    fun `person finnes, men enhet og utbetalinger utdatert`() {
+        personFinnes()
+        personinfoOppdatert()
+        assertFalse(command.execute(context))
+        assertHarBehov(listOf("HentEnhet"))
+    }
+
+    @Test
+    fun `person finnes, men utbetalinger utdatert`() {
+        personFinnes()
+        utdatertUtbetalinger()
+        assertFalse(command.execute(context))
+        assertHarBehov(listOf("HentInfotrygdutbetalinger"))
+    }
+
+    @Test
+    fun `oppdaterer utdatert person`() {
+        personFinnes()
+        altUtdatert()
+        val personinfo = mockk<HentPersoninfoLøsning>(relaxed = true)
+        val enhet = mockk<HentEnhetLøsning>(relaxed = true)
+        val utbetalinger = mockk<HentInfotrygdutbetalingerLøsning>(relaxed = true)
+        context.add(personinfo)
+        context.add(enhet)
+        context.add(utbetalinger)
+        assertTrue(command.execute(context))
+        verify(exactly = 1) { personinfo.oppdater(dao, FNR) }
+        verify(exactly = 1) { enhet.oppdater(dao, FNR) }
+        verify(exactly = 1) { utbetalinger.oppdater(dao, FNR) }
+    }
+
+    @Test
+    fun `person finnes, alt er oppdatert`() {
+        personFinnes()
+        altOppdatert()
+        assertTrue(command.execute(context))
+    }
+
+    private fun assertHarBehov(forventetBehov: List<String>) {
+        assertTrue(context.harBehov())
+        assertEquals(forventetBehov, context.behov().keys.toList())
+        verify(exactly = 0) { dao.insertPerson(FNR.toLong(), any(), any(), any(), any()) }
+    }
+
+    private fun personFinnes() {
+        every { dao.findPersonByFødselsnummer(FNR.toLong()) } returns 1
+    }
+
+    private fun personFinnesIkke() {
+        every { dao.findPersonByFødselsnummer(FNR.toLong()) } returns null
+    }
+
+    private fun altOppdatert() {
+        every { dao.findPersoninfoSistOppdatert(FNR.toLong()) } returns LocalDate.now()
+        every { dao.findEnhetSistOppdatert(FNR.toLong()) } returns LocalDate.now()
+        every { dao.findITUtbetalingsperioderSistOppdatert(FNR.toLong()) } returns LocalDate.now()
+    }
+
+    private fun altUtdatert() {
+        every { dao.findPersoninfoSistOppdatert(FNR.toLong()) } returns LocalDate.now().minusYears(1)
+        every { dao.findEnhetSistOppdatert(FNR.toLong()) } returns LocalDate.now().minusYears(1)
+        every { dao.findITUtbetalingsperioderSistOppdatert(FNR.toLong()) } returns LocalDate.now().minusYears(1)
+    }
+
+    private fun personinfoOppdatert() {
+        every { dao.findPersoninfoSistOppdatert(FNR.toLong()) } returns LocalDate.now()
+        every { dao.findEnhetSistOppdatert(FNR.toLong()) } returns LocalDate.now().minusYears(1)
+        every { dao.findITUtbetalingsperioderSistOppdatert(FNR.toLong()) } returns LocalDate.now().minusYears(1)
+    }
+
+    private fun utdatertUtbetalinger() {
+        every { dao.findPersoninfoSistOppdatert(FNR.toLong()) } returns LocalDate.now()
+        every { dao.findEnhetSistOppdatert(FNR.toLong()) } returns LocalDate.now()
+        every { dao.findITUtbetalingsperioderSistOppdatert(FNR.toLong()) } returns LocalDate.now().minusYears(1)
+    }
+}
