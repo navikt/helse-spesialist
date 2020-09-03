d6f8f7270932e94ab0fdf534206f3159a8582777
David Steinsland <david@steinsland.email>
2020-09-03 09:44:44 +0200

    lagrer og oppdaterer IT-utbetalinger i HentInfotrygdutbetalingerLøsning

--- src/main/kotlin/no/nav/helse/modell/person/HentInfotrygdutbetalingerLøsning.kt
+++ src/main/kotlin/no/nav/helse/modell/person/HentInfotrygdutbetalingerLøsning.kt
@@ -2,4 +2,17 @@ package no.nav.helse.modell.person
 
 import com.fasterxml.jackson.databind.JsonNode
 
-internal class HentInfotrygdutbetalingerLøsning(internal val utbetalinger: JsonNode)
+internal class HentInfotrygdutbetalingerLøsning(internal val utbetalinger: JsonNode) {
+
+    internal fun lagre(personDao: PersonDao) =
+        personDao.insertInfotrygdutbetalinger(utbetalinger)
+
+    fun oppdater(personDao: PersonDao, fødselsnummer: String) {
+        if (personDao.findInfotrygdutbetalinger(fødselsnummer.toLong()) != null) {
+            personDao.updateInfotrygdutbetalinger(fødselsnummer.toLong(), utbetalinger)
+        } else {
+            val utbetalingRef = personDao.insertInfotrygdutbetalinger(utbetalinger)
+            personDao.updateInfotrygdutbetalingerRef(fødselsnummer.toLong(), utbetalingRef)
+        }
+    }
+}
