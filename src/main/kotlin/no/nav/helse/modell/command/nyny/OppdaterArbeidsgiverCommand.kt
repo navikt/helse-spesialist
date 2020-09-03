f17aa36b640243190380a1d1e6212dcf783b65ab
David Steinsland <david@steinsland.email>
2020-09-03 11:14:38 +0200

    oppretter/oppdatere arbeidsgiver

---
+++ src/main/kotlin/no/nav/helse/modell/command/nyny/OppdaterArbeidsgiverCommand.kt
@@ -0,0 +1,36 @@
+package no.nav.helse.modell.command.nyny
+
+import no.nav.helse.modell.arbeidsgiver.ArbeidsgiverDao
+import no.nav.helse.modell.arbeidsgiver.ArbeidsgiverLøsning
+import java.time.LocalDate
+
+internal class OppdaterArbeidsgiverCommand(private val orgnummer: String, private val arbeidsgiverDao: ArbeidsgiverDao) : Command {
+    override fun execute(context: CommandContext): Boolean {
+        if (erOppdatert()) return true
+        return true
+        // TODO: Faktisk hente arbeidsgiver info
+        // return behandle(context)
+    }
+
+    override fun resume(context: CommandContext): Boolean {
+        return behandle(context)
+    }
+
+    private fun erOppdatert(): Boolean {
+        val sistOppdatert = arbeidsgiverDao.findNavnSistOppdatert(orgnummer.toLong())
+        return sistOppdatert > LocalDate.now().minusDays(14)
+    }
+
+    private fun behandle(context: CommandContext): Boolean {
+        val løsning = context.get<ArbeidsgiverLøsning>() ?: return trengerMerInformasjon(context)
+        løsning.oppdater(arbeidsgiverDao, orgnummer)
+        return true
+    }
+
+    private fun trengerMerInformasjon(context: CommandContext): Boolean {
+        context.behov("HentArbeidsgiver", mapOf(
+            "organisasjonsnummer" to orgnummer
+        ))
+        return false
+    }
+}
---
+++ src/test/kotlin/no/nav/helse/modell/command/nyny/OppdaterArbeidsgiverCommandTest.kt
@@ -0,0 +1,45 @@
+package no.nav.helse.modell.command.nyny
+
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.helse.modell.arbeidsgiver.ArbeidsgiverDao
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.util.*
+
+internal class OppdaterArbeidsgiverCommandTest {
+    private companion object {
+        private const val ORGNR = "123456789"
+    }
+
+    private val dao = mockk<ArbeidsgiverDao>(relaxed = true)
+
+    private lateinit var context: CommandContext
+    private val command = OppdaterArbeidsgiverCommand(ORGNR, dao)
+
+    @BeforeEach
+    fun setup() {
+        context = CommandContext(UUID.randomUUID())
+        clearMocks(dao)
+    }
+
+    @Test
+    fun `oppdaterer ikke når informasjonen er ny`() {
+        every { dao.findNavnSistOppdatert(ORGNR.toLong()) } returns LocalDate.now()
+        assertTrue(command.execute(context))
+        verify(exactly = 0) { dao.updateNavn(any(), any()) }
+    }
+
+    @Test
+    fun `oppdaterer ikke når informasjonen er gammel`() {
+        every { dao.findNavnSistOppdatert(ORGNR.toLong()) } returns LocalDate.now().minusYears(1)
+        assertTrue(command.execute(context))
+        assertFalse(context.harBehov())
+        verify(exactly = 0) { dao.updateNavn(any(), any()) }
+    }
+}
