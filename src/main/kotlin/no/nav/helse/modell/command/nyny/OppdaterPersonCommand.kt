bba4c110c09fd5cff00bc9205109e8884a3b174c
David Steinsland <david@steinsland.email>
2020-09-04 08:56:33 +0200

    flytter felles logikk til abstrakt klasse
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- src/main/kotlin/no/nav/helse/modell/command/nyny/OppdaterPersonCommand.kt
+++ src/main/kotlin/no/nav/helse/modell/command/nyny/OppdaterPersonCommand.kt
@@ -21,7 +21,11 @@ internal class OppdaterPersonCommand(
         OppdaterInfotrygdutbetalingerCommand(fødselsnummer, personDao)
     )
 
-    private abstract class OppdaterCommand(private val fødselsnummer: String, private val personDao: PersonDao) : Command {
+    private abstract class OppdaterCommand(
+        private val fødselsnummer: String,
+        private val personDao: PersonDao,
+        private val behov: String
+    ) : Command {
         override fun execute(context: CommandContext): Boolean {
             if (erOppdatert(personDao, fødselsnummer)) return ignorer()
             return behandle(context, personDao, fødselsnummer)
@@ -32,16 +36,22 @@ internal class OppdaterPersonCommand(
         }
 
         private fun ignorer(): Boolean {
-            log.info("informasjonen er ny nok")
+            log.info("har ikke behov for $behov, informasjonen er ny nok")
             return true
         }
 
         protected abstract fun erOppdatert(personDao: PersonDao, fødselsnummer: String): Boolean
 
         protected abstract fun behandle(context: CommandContext, personDao: PersonDao, fødselsnummer: String): Boolean
+
+        protected fun trengerMerInformasjon(context: CommandContext): Boolean {
+            log.info("trenger oppdatert $behov")
+            context.behov(behov)
+            return false
+        }
     }
 
-    private class OppdaterPersoninfoCommand(fødselsnummer: String, personDao: PersonDao) : OppdaterCommand(fødselsnummer, personDao) {
+    private class OppdaterPersoninfoCommand(fødselsnummer: String, personDao: PersonDao) : OppdaterCommand(fødselsnummer, personDao, "HentPersoninfo") {
         override fun erOppdatert(personDao: PersonDao, fødselsnummer: String): Boolean {
             val sistOppdatert = personDao.findPersoninfoSistOppdatert(fødselsnummer)
             return sistOppdatert > LocalDate.now().minusDays(14)
@@ -53,15 +63,9 @@ internal class OppdaterPersonCommand(
             personinfo.oppdater(personDao, fødselsnummer)
             return true
         }
-
-        private fun trengerMerInformasjon(context: CommandContext): Boolean {
-            log.info("trenger oppdatert personinfo")
-            context.behov("HentPersoninfo")
-            return false
-        }
     }
 
-    private class OppdaterEnhetCommand(fødselsnummer: String, personDao: PersonDao) : OppdaterCommand(fødselsnummer, personDao) {
+    private class OppdaterEnhetCommand(fødselsnummer: String, personDao: PersonDao) : OppdaterCommand(fødselsnummer, personDao, "HentEnhet") {
         override fun erOppdatert(personDao: PersonDao, fødselsnummer: String): Boolean {
             val sistOppdatert = personDao.findEnhetSistOppdatert(fødselsnummer)
             return sistOppdatert > LocalDate.now().minusDays(5)
@@ -73,15 +77,9 @@ internal class OppdaterPersonCommand(
             enhet.oppdater(personDao, fødselsnummer)
             return true
         }
-
-        private fun trengerMerInformasjon(context: CommandContext): Boolean {
-            log.info("trenger oppdatert enhetsnr")
-            context.behov("HentEnhet")
-            return false
-        }
     }
 
-    private class OppdaterInfotrygdutbetalingerCommand(fødselsnummer: String, personDao: PersonDao) : OppdaterCommand(fødselsnummer, personDao) {
+    private class OppdaterInfotrygdutbetalingerCommand(fødselsnummer: String, personDao: PersonDao) : OppdaterCommand(fødselsnummer, personDao, "HentInfotrygdutbetalinger") {
         override fun erOppdatert(personDao: PersonDao, fødselsnummer: String): Boolean {
             val sistOppdatert = personDao.findITUtbetalingsperioderSistOppdatert(fødselsnummer)
             return sistOppdatert > LocalDate.now().minusDays(1)
@@ -92,12 +90,6 @@ internal class OppdaterPersonCommand(
             log.info("oppdaterer utbetalinger fra Infotrygd")
             utbetalinger.oppdater(personDao, fødselsnummer)
             return true
-        }
-
-        private fun trengerMerInformasjon(context: CommandContext): Boolean {
-            log.info("trenger oppdatert utbetalinger fra Infotrygd")
-            context.behov("HentInfotrygdutbetalinger")
-            return false
         }
     }
 }
