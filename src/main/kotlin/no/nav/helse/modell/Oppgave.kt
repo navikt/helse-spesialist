7309b7e4ef51b5d0f971f5d359f08d17505a923d
David Steinsland <david@steinsland.email>
2020-09-09 10:09:00 +0200

    eget objekt for oppgave
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

---
+++ src/main/kotlin/no/nav/helse/modell/Oppgave.kt
@@ -0,0 +1,47 @@
+package no.nav.helse.modell
+
+import no.nav.helse.Oppgavestatus
+import no.nav.helse.modell.command.OppgaveDao
+import java.util.*
+
+internal class Oppgave private constructor(
+    private val navn: String,
+    private var status: Oppgavestatus,
+    private val vedtaksperiodeId: UUID
+) {
+    private var id: Long? = null
+    private var ferdigstiltAvIdent: String? = null
+    private var ferdigstiltAvOid: UUID? = null
+
+    internal companion object {
+        fun avventerSaksbehandler(navn: String, vedtaksperiodeId: UUID) = Oppgave(navn, Oppgavestatus.AvventerSaksbehandler, vedtaksperiodeId)
+    }
+
+    internal fun ferdigstill(id: Long, ident: String, oid: UUID) {
+        this.id = id
+        status = Oppgavestatus.Ferdigstilt
+        ferdigstiltAvIdent = ident
+        ferdigstiltAvOid = oid
+    }
+
+    internal fun lagre(oppgaveDao: OppgaveDao, vedtakDao: VedtakDao, hendelseId: UUID, contextId: UUID) {
+        id?.also { oppdater(oppgaveDao, it) } ?: opprett(oppgaveDao, vedtakDao, hendelseId, contextId)
+    }
+
+    private fun opprett(oppgaveDao: OppgaveDao, vedtakDao: VedtakDao, hendelseId: UUID, contextId: UUID) {
+        val vedtakRef = requireNotNull(vedtakDao.findVedtak(vedtaksperiodeId)?.id)
+        oppgaveDao.insertOppgave(
+            hendelseId,
+            contextId,
+            navn,
+            status,
+            null,
+            null,
+            vedtakRef
+        )
+    }
+
+    private fun oppdater(oppgaveDao: OppgaveDao, id: Long) {
+        oppgaveDao.updateOppgave(id, status, ferdigstiltAvIdent, ferdigstiltAvOid)
+    }
+}
---
+++ src/test/kotlin/no/nav/helse/modell/OppgaveTest.kt
@@ -0,0 +1,51 @@
+package no.nav.helse.modell
+
+import io.mockk.clearMocks
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.helse.Oppgavestatus
+import no.nav.helse.modell.command.OppgaveDao
+import no.nav.helse.modell.vedtak.VedtakDto
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+import kotlin.random.Random
+
+internal class OppgaveTest {
+    private companion object {
+        private const val OPPGAVENAVN = "Utbetalingsgodkjenning"
+        private val VEDTAKSPERIODE_ID = UUID.randomUUID()
+        private val HENDELSE_ID = UUID.randomUUID()
+        private val COMMAND_CONTEXT_ID = UUID.randomUUID()
+        private const val SAKSBEHANDLERIDENT = "Z999999"
+        private val SAKSBEHANDLEROID = UUID.randomUUID()
+        private val OPPGAVE_ID = Random.nextLong()
+        private val VEDTAKREF = Random.nextLong()
+        private val VEDTAK = VedtakDto(VEDTAKREF, 1L)
+    }
+
+    private val oppgaveDao = mockk<OppgaveDao>(relaxed = true)
+    private val vedtakDao = mockk<VedtakDao>()
+
+    private val oppgave = Oppgave.avventerSaksbehandler(OPPGAVENAVN, VEDTAKSPERIODE_ID)
+
+    @BeforeEach
+    fun setup() {
+        clearMocks(oppgaveDao, vedtakDao)
+    }
+
+    @Test
+    fun `oppretter ny oppgave`() {
+        every { vedtakDao.findVedtak(VEDTAKSPERIODE_ID) } returns VEDTAK
+        oppgave.lagre(oppgaveDao, vedtakDao, HENDELSE_ID, COMMAND_CONTEXT_ID)
+        verify(exactly = 1) { oppgaveDao.insertOppgave(HENDELSE_ID, COMMAND_CONTEXT_ID, OPPGAVENAVN, Oppgavestatus.AvventerSaksbehandler, null, null, VEDTAKREF) }
+    }
+
+    @Test
+    fun `oppdater oppgave`() {
+        oppgave.ferdigstill(OPPGAVE_ID, SAKSBEHANDLERIDENT, SAKSBEHANDLEROID)
+        oppgave.lagre(oppgaveDao, vedtakDao, HENDELSE_ID, COMMAND_CONTEXT_ID)
+        verify(exactly = 1) { oppgaveDao.updateOppgave(OPPGAVE_ID, Oppgavestatus.Ferdigstilt, SAKSBEHANDLERIDENT, SAKSBEHANDLEROID) }
+    }
+}
