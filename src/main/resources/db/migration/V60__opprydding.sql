11d4dc1450d42815961b3c197c6c110e628ef791
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2020-10-01 14:59:46 +0200

    Cascading delete på hendelse-id i warning-tabell
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
    Co-authored-by: Sondre Halvorsen <sondre.halvorsen@nav.no>

--- src/main/resources/db/migration/V60__opprydding.sql
+++ src/main/resources/db/migration/V60__opprydding.sql
@@ -1,3 +1,6 @@
+ALTER TABLE warning DROP CONSTRAINT warning_spleisbehov_ref_fkey;
+ALTER TABLE warning ADD CONSTRAINT warning_hendelse_ref_fkey FOREIGN KEY(hendelse_id) REFERENCES hendelse(id) ON DELETE CASCADE;
+
 DELETE FROM hendelse WHERE fodselsnummer IS NULL OR type = 'Godkjenningsbehov';
 ALTER TABLE hendelse DROP COLUMN original, DROP COLUMN spleis_referanse, ALTER COLUMN fodselsnummer SET NOT NULL;
 ALTER TABLE hendelse RENAME CONSTRAINT spleisbehov_pkey TO hendelse_pkey;
