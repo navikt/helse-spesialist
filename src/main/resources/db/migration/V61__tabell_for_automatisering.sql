9bf8f9fe7d11b600263388c38bfe3b9a46ae0d35
Knut Nygaard <knut.nygaard@nav.no>
2020-10-02 09:25:31 +0200

    Ikke cascade delete i automatiseringstabellen

--- src/main/resources/db/migration/V61__tabell_for_automatisering.sql
+++ src/main/resources/db/migration/V61__tabell_for_automatisering.sql
@@ -1,6 +1,6 @@
 CREATE TABLE automatisering
 (
-    vedtaksperiode_ref INT  NOT NULL REFERENCES vedtak (id) ON DELETE CASCADE,
+    vedtaksperiode_ref INT  NOT NULL REFERENCES vedtak (id),
     hendelse_ref       uuid NOT NULL REFERENCES hendelse (id),
     automatisert       BOOLEAN,
     PRIMARY KEY (vedtaksperiode_ref, hendelse_ref)
