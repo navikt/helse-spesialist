f85ac92135718e1d8a1436afe2de203ed6462d94
David Steinsland <david@steinsland.email>
2020-11-29 19:03:10 +0100

    forsøk å legge på indekser på felter som referer til hendelse.id
    En delete på hendelse-tabellen trigger cascade-effekter, som virker å ta uendelig lang tid uten indekser.
    
    https://dba.stackexchange.com/a/138270

---
+++ src/main/resources/db/migration/V95__indekser.sql
@@ -0,0 +1,4 @@
+CREATE INDEX ON automatisering(hendelse_ref);
+CREATE INDEX ON automatisering_problem(hendelse_ref);
+CREATE INDEX ON command_context(hendelse_id);
+CREATE INDEX ON vedtaksperiode_hendelse(hendelse_ref);
