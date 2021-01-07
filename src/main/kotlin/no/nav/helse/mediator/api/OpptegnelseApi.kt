8c4e1aea62385cb691c7a3af5d9413f2394f6c5d
Erlend Viddal <eviddal@gmail.com>
2021-01-07 10:16:09 +0100

    Henter bare oid for saksbehandler ved abonnering
    Co-authored-by: Sindre Broch <sindre.broch@nav.no>

--- src/main/kotlin/no/nav/helse/mediator/api/OpptegnelseApi.kt
+++ src/main/kotlin/no/nav/helse/mediator/api/OpptegnelseApi.kt
@@ -2,33 +2,41 @@ package no.nav.helse.mediator.api
 
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.util.pipeline.*
 import no.nav.helse.mediator.OpptegnelseMediator
-import no.nav.helse.mediator.api.modell.Saksbehandler
+import java.util.*
 
 internal fun Route.opptegnelseApi(opptegnelseMediator: OpptegnelseMediator) {
     post("/api/opptegnelse/abonner/{aktørId}") {
+        val saksbehandlerreferanse = getSaksbehandlerOid()
         val aktørId = requireNotNull(call.parameters["aktørId"]?.toLong()) { "Ugyldig aktørId i path parameter" }
-        val saksbehandler = Saksbehandler.fraToken(requireNotNull(call.principal()))
 
-        opptegnelseMediator.opprettAbonnement(saksbehandler.oid, aktørId)
+        opptegnelseMediator.opprettAbonnement(saksbehandlerreferanse, aktørId)
         call.respond(HttpStatusCode.OK, mapOf("status" to "OK"))
     }
 
     get("/api/opptegnelse/hent/{sisteSekvensId}") {
-        val sisteSekvensId = requireNotNull(call.parameters["sisteSekvensId"]?.toInt()) { "Ugyldig aktørId i path parameter" }
-        val saksbehandler = Saksbehandler.fraToken(requireNotNull(call.principal()))
+        val saksbehandlerreferanse = getSaksbehandlerOid()
+        val sisteSekvensId =
+            requireNotNull(call.parameters["sisteSekvensId"]?.toInt()) { "Ugyldig siste seksvensid i path parameter" }
+        val opptegnelser = opptegnelseMediator.hentAbonnerteOpptegnelser(saksbehandlerreferanse, sisteSekvensId)
 
-        val oppdateringer = opptegnelseMediator.hentAbonnerteOpptegnelser(saksbehandler.oid, sisteSekvensId)
-        call.respond(HttpStatusCode.OK, oppdateringer)
+        call.respond(HttpStatusCode.OK, opptegnelser)
     }
 
     get("/api/opptegnelse/hent") {
-        val saksbehandler = Saksbehandler.fraToken(requireNotNull(call.principal()))
+        val saksbehandlerreferanse = getSaksbehandlerOid()
+        val opptegnelser = opptegnelseMediator.hentAbonnerteOpptegnelser(saksbehandlerreferanse)
 
-        val oppdateringer = opptegnelseMediator.hentAbonnerteOpptegnelser(saksbehandler.oid)
-        call.respond(HttpStatusCode.OK, oppdateringer)
+        call.respond(HttpStatusCode.OK, opptegnelser)
     }
+}
+
+private fun PipelineContext<Unit, ApplicationCall>.getSaksbehandlerOid(): UUID {
+    val accessToken = requireNotNull(call.principal<JWTPrincipal>()) { "mangler access token" }
+    return UUID.fromString(accessToken.payload.getClaim("oid").asString())
 }
