b8cda32cb322898e05577df51524070f3b185052
Peter Stensby <peter.stensby@nav.no>
2021-02-10 16:30:43 +0100

    Retter feil i migrering
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.qeriksen@nav.no>

--- src/main/resources/db/migration/V119__slett_duplikate_arbeidsgivere.sql
+++ src/main/resources/db/migration/V119__slett_duplikate_arbeidsgivere.sql
@@ -4,14 +4,15 @@ WHERE id NOT IN (
     FROM   arbeidsgiver
     GROUP  BY orgnummer);
 
-delete from arbeidsgiver_navn
-where id in (select navn_ref from arbeidsgiver a join lol on a.id = lol.id);
+delete from arbeidsgiver
+using lol;
 
 delete from arbeidsgiver_bransjer
 where id in (select bransjer_ref from arbeidsgiver a join lol on a.id = lol.id);
 
-delete from arbeidsgiver
-where id in (select * from lol);
+delete from arbeidsgiver_navn
+    where id in (select navn_ref from arbeidsgiver a join lol on a.id = lol.id);
+
 drop table if exists lol;
 
 ALTER TABLE arbeidsgiver ADD UNIQUE(orgnummer);
