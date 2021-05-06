1d4e93ed80ec98e4b90435deb4418999730afd58
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2021-05-06 21:22:37 +0200

    fiks gradle bygg

--- spesialist-selve/build.gradle.kts
+++ spesialist-selve/build.gradle.kts
@@ -1,3 +1,24 @@
 dependencies {
     implementation(project(":spesialist-api"))
 }
+
+tasks {
+    named<Jar>("jar") {
+        archiveBaseName.set("app")
+
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
+        }
+    }
+}
