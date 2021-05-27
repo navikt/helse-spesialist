d42fe41015a7ea31dd6d33d2c2f518fddb10d76e
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2021-05-27 15:59:37 +0200

    Egen funksjon for selve lagringen av snapshot'et

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/SnapshotDao.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/SnapshotDao.kt
@@ -15,27 +15,8 @@ internal class SnapshotDao(private val dataSource: DataSource) {
             val versjon = objectMapper.readTree(snapshot)["versjon"].asInt()
             if (versjon > tx.finnGlobalVersjon())
                 tx.oppdaterGlobalVersjon(versjon)
-
-            @Language("PostgreSQL")
-            val statement = """
-            INSERT INTO speil_snapshot(person_ref, data, versjon)
-                VALUES(:person_ref, CAST(:snapshot as json), :versjon)
-            ON CONFLICT(person_ref) DO UPDATE
-                SET data = CAST(:snapshot as json), sist_endret = now(), versjon = :versjon;
-        """
-            requireNotNull(
-                tx.run(
-                    queryOf(
-                        statement,
-                        mapOf(
-                            "person_ref" to personRef,
-                            "snapshot" to snapshot,
-                            "versjon" to versjon
-                        )
-                    ).asUpdateAndReturnGeneratedKey
-                )
-            )
-        }.toInt()
+            tx.lagre(personRef, snapshot, versjon)
+        }
     }
 
     fun utdatert(fødselsnummer: String) = sessionOf(dataSource).use { session ->
@@ -45,6 +26,28 @@ internal class SnapshotDao(private val dataSource: DataSource) {
 
             versjonForSnapshot?.let { it < sisteGjeldendeVersjon } ?: true
         }
+    }
+
+    private fun TransactionalSession.lagre(personRef: Int, snapshot: String, versjon: Int): Int {
+        @Language("PostgreSQL")
+        val statement = """
+            INSERT INTO speil_snapshot(person_ref, data, versjon)
+                VALUES(:person_ref, CAST(:snapshot as json), :versjon)
+            ON CONFLICT(person_ref) DO UPDATE
+                SET data = CAST(:snapshot as json), sist_endret = now(), versjon = :versjon;
+        """
+        return requireNotNull(
+            run(
+                queryOf(
+                    statement,
+                    mapOf(
+                        "person_ref" to personRef,
+                        "snapshot" to snapshot,
+                        "versjon" to versjon
+                    )
+                ).asUpdateAndReturnGeneratedKey
+            )
+        ).toInt()
     }
 
     private fun TransactionalSession.finnSnapshotVersjon(fødselsnummer: String): Int? {
@@ -57,7 +60,7 @@ internal class SnapshotDao(private val dataSource: DataSource) {
         return run(queryOf(statement, fødselsnummer.toLong()).map { it.int("versjon") }.asSingle)
     }
 
-    private fun TransactionalSession.finnPersonRef(fødselsnummer: String): Int? {
+    private fun TransactionalSession.finnPersonRef(fødselsnummer: String): Int {
         @Language("PostgreSQL")
         val statement = "SELECT id FROM person WHERE fodselsnummer = ?"
         return requireNotNull(this.run(queryOf(statement, fødselsnummer.toLong()).map { it.int("id") }.asSingle))
