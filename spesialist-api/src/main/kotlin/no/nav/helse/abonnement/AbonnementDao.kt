780b94537d478e056debc372e8dd8b95284a17f6
Håkon <hakon.a.holmstedt@gmail.com>
2021-08-11 10:39:54 +0200

    erstatter ? med named parameters

--- spesialist-api/src/main/kotlin/no/nav/helse/abonnement/AbonnementDao.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/abonnement/AbonnementDao.kt
@@ -12,10 +12,10 @@ class AbonnementDao(private val dataSource: DataSource) {
         @Language("PostgreSQL")
         val statement = """
             INSERT INTO abonnement_for_opptegnelse
-                SELECT ?, p.id, MAX(o.sekvensnummer) FROM person p LEFT JOIN opptegnelse o on p.id = o.person_id WHERE p.aktor_id = ? GROUP BY p.id
+                SELECT :saksbehandlerId, p.id, MAX(o.sekvensnummer) FROM person p LEFT JOIN opptegnelse o on p.id = o.person_id WHERE p.aktor_id = :aktorId GROUP BY p.id
             ON CONFLICT DO NOTHING;
         """
-        session.run(queryOf(statement, saksbehandlerId, aktørId).asUpdate)
+        session.run(queryOf(statement, mapOf("saksbehandlerId" to saksbehandlerId, "aktorId" to aktørId)).asUpdate)
     }
 
     fun registrerSistekvensnummer(saksbehandlerIdent: UUID, sisteSekvensId: Int) =
@@ -23,9 +23,9 @@ class AbonnementDao(private val dataSource: DataSource) {
             @Language("PostgreSQL")
             val statement = """
                 UPDATE abonnement_for_opptegnelse
-                SET siste_sekvensnummer=?
-                WHERE saksbehandler_id=?;
+                SET siste_sekvensnummer=:sisteSekvensId
+                WHERE saksbehandler_id=:saksbehandlerId;
             """
-            session.run(queryOf(statement, sisteSekvensId, saksbehandlerIdent).asUpdate)
+            session.run(queryOf(statement, mapOf("sisteSekvensId" to sisteSekvensId, "saksbehandlerId" to saksbehandlerIdent)).asUpdate)
         }
 }
