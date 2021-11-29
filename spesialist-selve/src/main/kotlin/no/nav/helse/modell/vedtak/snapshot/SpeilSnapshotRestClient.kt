25db7c98dad05e9d77617a60062599cb25a8e594
Knut Nygaard <knutnyg@gmail.com>
2021-11-29 15:08:33 +0100

    Forbedrer logging
    Co-authored-by: David Steinsland <david@davidsteinsland.net>
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtak/snapshot/SpeilSnapshotRestClient.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtak/snapshot/SpeilSnapshotRestClient.kt
@@ -7,6 +7,7 @@ import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import net.logstash.logback.argument.StructuredArguments.keyValue
 import no.nav.helse.AccessTokenClient
 import org.slf4j.LoggerFactory
 import java.io.IOException
@@ -30,11 +31,16 @@ class SpeilSnapshotRestClient(
 
     private suspend fun hentSpeilSpapshot(fnr: String, retries: Int): String = try {
         val accessToken = accessTokenClient.hentAccessToken(spleisClientId)
-        sikkerLogg.info("Henter nytt speil-snapshot for personnummer: $fnr")
+        val callId = UUID.randomUUID().toString()
+        sikkerLogg.info(
+            "Henter nytt speil-snapshot for {}, {}",
+            keyValue("fødselsnummer", fnr),
+            keyValue("callId", callId)
+        )
         val response: HttpResponse = httpClient.get("http://spleis-api.tbd.svc.nais.local/api/person-snapshot") {
             header("Authorization", "Bearer $accessToken")
             header("fnr", fnr)
-            header("callId", UUID.randomUUID().toString())
+            header("callId", callId)
             accept(ContentType.Application.Json)
         }
         if (!response.status.isSuccess()) {
@@ -42,16 +48,22 @@ class SpeilSnapshotRestClient(
         }
         response.receive()
     } catch (e: IOException) {
-        sikkerLogg.error("Feil ved henting av snapshot for personnummer: $fnr. Vi prøver ${retries - 1} ganger til.")
+        sikkerLogg.error(
+            "Feil ved henting av snapshot for fødselsnummer: $fnr. Vi prøver ${retries - 1} ganger til.",
+            e
+        )
         if (retries <= 1) {
-            sikkerLogg.error("Gir opp etter 5 forsøk på å hente snapshot for personnummer: $fnr", e)
+            sikkerLogg.error("Gir opp etter 5 forsøk på å hente snapshot for fødselsnummer: $fnr", e)
             throw e
         } else {
             delay(retryInterval)
             hentSpeilSpapshot(fnr, retries - 1)
         }
     } catch (e: RuntimeException) {
-        sikkerLogg.error("Feil ved henting av snapshot for personnummer: $fnr. Vi prøver ${retries - 1} ganger til.", e)
+        sikkerLogg.error(
+            "Feil ved henting av snapshot for fødselsnummer: $fnr. Vi prøver ${retries - 1} ganger til.",
+            e
+        )
         throw e
     }
 }
