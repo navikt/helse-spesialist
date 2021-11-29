55c82291f0f2a9cc3c8d7e8696eae9c4a908d2fb
Knut Nygaard <knutnyg@gmail.com>
2021-11-29 14:41:21 +0100

    Legger på masse logging rundt kallet til spleis-api og litt feilhåndtering
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtak/snapshot/SpeilSnapshotRestClient.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtak/snapshot/SpeilSnapshotRestClient.kt
@@ -1,13 +1,16 @@
 package no.nav.helse.modell.vedtak.snapshot
 
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import no.nav.helse.AccessTokenClient
+import org.slf4j.LoggerFactory
 import java.io.IOException
+import java.util.*
 
 class SpeilSnapshotRestClient(
     private val httpClient: HttpClient,
@@ -15,6 +18,10 @@ class SpeilSnapshotRestClient(
     private val spleisClientId: String,
     private val retryInterval: Long = 5000L
 ) {
+    private companion object {
+        val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
+    }
+
     internal fun hentSpeilSpapshot(fnr: String): String {
         return runBlocking {
             hentSpeilSpapshot(fnr, 5)
@@ -23,17 +30,28 @@ class SpeilSnapshotRestClient(
 
     private suspend fun hentSpeilSpapshot(fnr: String, retries: Int): String = try {
         val accessToken = accessTokenClient.hentAccessToken(spleisClientId)
-        httpClient.get<HttpStatement>("http://spleis-api.tbd.svc.nais.local/api/person-snapshot") {
+        sikkerLogg.info("Henter nytt speil-snapshot for personnummer: $fnr")
+        val response: HttpResponse = httpClient.get("http://spleis-api.tbd.svc.nais.local/api/person-snapshot") {
             header("Authorization", "Bearer $accessToken")
             header("fnr", fnr)
+            header("callId", UUID.randomUUID().toString())
             accept(ContentType.Application.Json)
-        }.receive()
+        }
+        if (!response.status.isSuccess()) {
+            throw RuntimeException("Feil ved henting av speilsnapshot. Statuskode: ${response.status.value}")
+        }
+        response.receive()
     } catch (e: IOException) {
+        sikkerLogg.error("Feil ved henting av snapshot for personnummer: $fnr. Vi prøver ${retries - 1} ganger til.")
         if (retries <= 1) {
+            sikkerLogg.error("Gir opp etter 5 forsøk på å hente snapshot for personnummer: $fnr", e)
             throw e
         } else {
             delay(retryInterval)
             hentSpeilSpapshot(fnr, retries - 1)
         }
+    } catch (e: RuntimeException) {
+        sikkerLogg.error("Feil ved henting av snapshot for personnummer: $fnr. Vi prøver ${retries - 1} ganger til.", e)
+        throw e
     }
 }
