840f8fbce61f00f9109e4adc269f4b391f4a8301
Knut Nygaard <knutnyg@gmail.com>
2021-12-22 10:52:19 +0100

    Delegerer automatisk avvisning av vedtaksperiode til en dedikert kommando
    I dag er det to (snart 3) årsaker til at vi automatisk avviser godkjenningsbehovet:
     - Bruker er egen ansatt
     - Bruker tilhører utenlandsenheten
     - (Bruker er under vergemål)
    
    Hver av disse sjekkene har i dag håndtert dette i isolasjon, som fører til uoversiktlig flyt og potensielt duplikate avslag. Jeg har trukket ut selve avslagsbiten og delegert det til en separat kommando.

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/egenansatt/EgenAnsattCommand.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/egenansatt/EgenAnsattCommand.kt
@@ -1,20 +1,12 @@
 package no.nav.helse.modell.egenansatt
 
-import no.nav.helse.avvistPåGrunnAvEgenAnsattTeller
-import no.nav.helse.mediator.GodkjenningMediator
 import no.nav.helse.mediator.meldinger.EgenAnsattløsning
-import no.nav.helse.modell.UtbetalingsgodkjenningMessage
 import no.nav.helse.modell.kommando.Command
 import no.nav.helse.modell.kommando.CommandContext
 import org.slf4j.LoggerFactory
-import java.util.*
 
 internal class EgenAnsattCommand(
     private val egenAnsattDao: EgenAnsattDao,
-    private val godkjenningsbehovJson: String,
-    private val vedtaksperiodeId: UUID,
-    private val fødselsnummer: String,
-    private val godkjenningMediator: GodkjenningMediator,
 ) : Command {
 
     private companion object {
@@ -29,17 +21,6 @@ internal class EgenAnsattCommand(
 
     override fun resume(context: CommandContext): Boolean {
         val løsning = context.get<EgenAnsattløsning>() ?: return false
-        val erEgenAnsatt = løsning.evaluer()
-
-        if (erEgenAnsatt) {
-            val behov = UtbetalingsgodkjenningMessage(godkjenningsbehovJson)
-            behov.avvisAutomatisk(listOf("Egen ansatt"))
-            context.publiser(behov.toJson())
-            context.publiser(godkjenningMediator.lagVedtaksperiodeAvvist(vedtaksperiodeId, fødselsnummer, behov).toJson())
-            avvistPåGrunnAvEgenAnsattTeller.inc()
-            logg.info("Automatisk avvisning for vedtaksperiode $vedtaksperiodeId")
-        }
-
         løsning.lagre(egenAnsattDao)
         return true
     }
--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/kommando/KlargjørPersonCommand.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/kommando/KlargjørPersonCommand.kt
@@ -1,19 +1,14 @@
 package no.nav.helse.modell.kommando
 
-import no.nav.helse.mediator.GodkjenningMediator
 import no.nav.helse.modell.person.PersonDao
-import java.util.*
 
 internal class KlargjørPersonCommand(
     fødselsnummer: String,
     aktørId: String,
     personDao: PersonDao,
-    godkjenningsbehovJson: String,
-    vedtaksperiodeId: UUID,
-    godkjenningMediator: GodkjenningMediator,
 ) : MacroCommand() {
     override val commands: List<Command> = listOf(
-        OpprettPersonCommand(fødselsnummer, aktørId, personDao, godkjenningsbehovJson, vedtaksperiodeId, godkjenningMediator),
-        OppdaterPersonCommand(fødselsnummer, personDao, godkjenningsbehovJson, vedtaksperiodeId, godkjenningMediator)
+        OpprettPersonCommand(fødselsnummer, aktørId, personDao),
+        OppdaterPersonCommand(fødselsnummer, personDao)
     )
 }
