936d00be2139149d33c0daab9f858276a547a166
Stephen Ramthun <stephenramthun@gmail.com>
2022-01-03 15:38:16 +0100

    Ny tabell for snapshots hentet med graphql
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- spesialist-felles/src/main/resources/db/migration/V178__legg_til_snapshot_tabell.sql
+++ spesialist-felles/src/main/resources/db/migration/V178__legg_til_snapshot_tabell.sql
@@ -1,10 +1,8 @@
-CREATE TABLE snapshot
-(
-    id         SERIAL PRIMARY KEY,
-    data       JSON NOT NULL,
-    versjon    INT  NOT NULL,
-    person_ref INT UNIQUE REFERENCES person (id)
+CREATE TABLE snapshot (
+  id   SERIAL PRIMARY KEY,
+  data JSON NOT NULL,
+  versjon INT NOT NULL,
+  person_ref INT UNIQUE REFERENCES person(id)
 );
 
-ALTER TABLE vedtak
-    ADD COLUMN snapshot_ref INT REFERENCES snapshot (id) default null;
+ALTER TABLE vedtak ADD COLUMN snapshot_ref INT REFERENCES snapshot(id) default null;
