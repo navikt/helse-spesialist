48750468761b67b9c72d794270093fba1faa0f49
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-01-19 18:37:41 +0100

    JDK17 og andre bumps'n'upgrades
    * Kotlin -> 1.6.10
    * Java -> 17
    * Gradle -> 7.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
-val junitJupiterVersion = "5.8.1"
-val ktorVersion = "1.6.6"
+val junitJupiterVersion = "5.8.2"
+val ktorVersion = "1.6.7"
 val graphqlKotlinVersion = "5.3.1"
+val jvmTargetVersion = "17"
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.10"
 }
 
 allprojects {
@@ -17,12 +18,12 @@ allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
 
     dependencies {
-        implementation("com.github.navikt:rapids-and-rivers:2021.11.29-21.57.443e21ff5a6c")
+        implementation("com.github.navikt:rapids-and-rivers:2022.01.19-09.53.b526ca84a9e4")
         implementation("io.ktor:ktor-server-cio:$ktorVersion")
         implementation("com.papertrailapp:logback-syslog4j:1.0.0") //August, 2014
-        implementation("com.zaxxer:HikariCP:5.0.0")
+        implementation("com.zaxxer:HikariCP:5.0.1")
         implementation("no.nav:vault-jdbc:1.3.7")
-        implementation("org.flywaydb:flyway-core:8.1.0")
+        implementation("org.flywaydb:flyway-core:8.4.1")
         implementation("com.github.seratch:kotliquery:1.6.0") //April, 2019
         implementation("io.ktor:ktor-client-cio:$ktorVersion")
         implementation("io.ktor:ktor-client-apache:$ktorVersion")
@@ -33,7 +34,7 @@ allprojects {
             exclude(group = "junit")
         }
 
-        implementation("no.finn.unleash:unleash-client-java:4.4.0")
+        implementation("no.finn.unleash:unleash-client-java:4.4.1")
 
         implementation("com.expediagroup:graphql-kotlin-client:$graphqlKotlinVersion")
         implementation("com.expediagroup:graphql-kotlin-client-jackson:$graphqlKotlinVersion")
@@ -66,10 +67,10 @@ subprojects {
 
     tasks {
         compileKotlin {
-            kotlinOptions.jvmTarget = "16"
+            kotlinOptions.jvmTarget = jvmTargetVersion
         }
         compileTestKotlin {
-            kotlinOptions.jvmTarget = "16"
+            kotlinOptions.jvmTarget = jvmTargetVersion
         }
 
         withType<Test> {
@@ -83,7 +84,7 @@ subprojects {
         }
 
         withType<Wrapper> {
-            gradleVersion = "7.3"
+            gradleVersion = "7.3.3"
         }
     }
 }
