27559a1c3d58dea2c99fcdbdc36b10223e38e205
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-05-10 17:05:19 +0200

    Logging når GosysOppgaveEndret-kommandokjeden avbrytes
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/oppgave/SjekkAtOppgaveFortsattErÅpenCommand.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/oppgave/SjekkAtOppgaveFortsattErÅpenCommand.kt
@@ -2,16 +2,21 @@ package no.nav.helse.modell.oppgave
 
 import no.nav.helse.modell.kommando.Command
 import no.nav.helse.modell.kommando.CommandContext
-import no.nav.helse.oppgave.OppgaveDao
 import no.nav.helse.modell.kommando.CommandContext.Companion.ferdigstill
+import no.nav.helse.oppgave.OppgaveDao
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 internal class SjekkAtOppgaveFortsattErÅpenCommand(
     private val fødselsnummer: String,
     private val oppgaveDao: OppgaveDao,
 ) : Command {
+    private val sikkerLogger: Logger = LoggerFactory.getLogger("tjenestekall")
+
     override fun execute(context: CommandContext): Boolean {
         val åpenOppgave = oppgaveDao.finnOppgaveId(fødselsnummer)
         if (åpenOppgave == null) {
+            sikkerLogger.info("Ingen åpne oppgaver for $fødselsnummer, kommandokjeden ferdigstilles/avsluttes.")
             ferdigstill(context)
         }
         return true
