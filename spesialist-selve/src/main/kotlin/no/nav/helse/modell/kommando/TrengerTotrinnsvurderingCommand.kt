eedfbe863343b75f99f795d58047425841e91d6a
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-05-25 12:56:55 +0200

    Legger til logging av TrengerTotrinnsvurderingCommand

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/kommando/TrengerTotrinnsvurderingCommand.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/kommando/TrengerTotrinnsvurderingCommand.kt
@@ -3,15 +3,21 @@ package no.nav.helse.modell.kommando
 import java.util.UUID
 import no.nav.helse.modell.WarningDao
 import no.nav.helse.oppgave.OppgaveDao
+import org.slf4j.LoggerFactory
 
 internal class TrengerTotrinnsvurderingCommand(
     private val vedtaksperiodeId: UUID,
     private val warningDao: WarningDao,
     private val oppgaveDao: OppgaveDao
 ) : Command {
+
+    private companion object {
+        private val logg = LoggerFactory.getLogger(TrengerTotrinnsvurderingCommand::class.java)
+    }
     override fun execute(context: CommandContext): Boolean {
 
         if (harMedlemskapsVarsel() || harOppgaveMedEndring()) {
+            logg.info("Vedtaksperioden: " + vedtaksperiodeId + " setter trenger totrinnsvurdering")
             oppgaveDao.setTrengerTotrinnsvurdering(vedtaksperiodeId)
         }
 
@@ -21,12 +27,20 @@ internal class TrengerTotrinnsvurderingCommand(
     private fun harMedlemskapsVarsel(): Boolean {
         val medlemSkapVarsel = "Vurder lovvalg og medlemskap"
 
-        return warningDao.finnAktiveWarningsMedMelding(vedtaksperiodeId, medlemSkapVarsel).isNotEmpty()
+        val harMedlemskapsVarsel = warningDao.finnAktiveWarningsMedMelding(vedtaksperiodeId, medlemSkapVarsel).isNotEmpty()
+
+        logg.info("Vedtaksperioden: " + vedtaksperiodeId + " harMedlemskapsVarsel: $harMedlemskapsVarsel")
+
+        return harMedlemskapsVarsel
     }
 
     private fun harOppgaveMedEndring(): Boolean {
 
-        return oppgaveDao.harOppgaveMedEndring(vedtaksperiodeId)
+        val harOppgaveMedEndring = oppgaveDao.harOppgaveMedEndring(vedtaksperiodeId)
+
+        logg.info("Vedtaksperioden: " + vedtaksperiodeId + " harOppgaveMedEndring: $harOppgaveMedEndring")
+
+        return harOppgaveMedEndring
     }
 
 }
