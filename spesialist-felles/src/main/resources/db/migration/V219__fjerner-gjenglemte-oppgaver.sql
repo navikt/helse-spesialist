321e05009117d8bd23758aa70702f798c373104c
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-07-01 11:04:36 +0200

    Fjerner en dud-oppgave

---
+++ spesialist-felles/src/main/resources/db/migration/V219__fjerner-gjenglemte-oppgaver.sql
@@ -0,0 +1,7 @@
+
+-- Det ligger igjen en oppgaver som er avsluttet i spleis, den ble godkjent 27.12.21.
+-- Må vel ha vært en cosmic ray
+
+update oppgave
+set status = 'Invalidert'
+where id = 2538704
