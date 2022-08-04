448bd78e38d17abb4ca222df147de09ed5231e09
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-08-04 11:18:24 +0200

    Fjerner villedende IntelliJ-feilmelding
    Analyzeren mistet tråden da vi annoterte en expression body som
    kjører en spørring med @Language=PostgreSQL.
    
    Gevinst: fjerner potensialet for overflødige feilmeldinger.

--- spesialist-api/src/main/kotlin/no/nav/helse/abonnement/OpptegnelseDao.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/abonnement/OpptegnelseDao.kt
@@ -7,27 +7,31 @@ import javax.sql.DataSource
 
 class OpptegnelseDao(dataSource: DataSource) : HelseDao(dataSource) {
 
-    @Language("PostgreSQL")
-    fun opprettOpptegnelse(fødselsnummer: String, payload: OpptegnelsePayload, type: OpptegnelseType) =
-        """
+    fun opprettOpptegnelse(fødselsnummer: String, payload: OpptegnelsePayload, type: OpptegnelseType): Int {
+        @Language("PostgreSQL")
+        val query = """
             INSERT INTO opptegnelse (person_id, payload, type)
-            VALUES ((SELECT id FROM person WHERE fodselsnummer=:fodselsnummer), cast(:payload as jsonb), :type);
-            """.update(mapOf("fodselsnummer" to fødselsnummer.toLong(), "payload" to payload.toJson(), "type" to "$type"))
-
-    @Language("PostgreSQL")
-    fun finnOpptegnelser(saksbehandlerIdent: UUID) =
+            SELECT id, :payload::jsonb, :type
+            FROM person
+            WHERE fodselsnummer=:fodselsnummer
         """
+        return query.update(
+            mapOf("fodselsnummer" to fødselsnummer.toLong(), "payload" to payload.toJson(), "type" to "$type")
+        )
+    }
+
+    fun finnOpptegnelser(saksbehandlerIdent: UUID): List<OpptegnelseDto> {
+        @Language("PostgreSQL")
+        val query = """
             SELECT o.sekvensnummer, p.aktor_id, o.payload, o.type
             FROM opptegnelse o
             JOIN person p ON o.person_id = p.id
             JOIN abonnement_for_opptegnelse a ON a.person_id = o.person_id
 
             WHERE a.saksbehandler_id= :saksbehandlerIdent
-            AND (
-            a.siste_sekvensnummer IS NULL
-            OR o.SEKVENSNUMMER > a.siste_sekvensnummer
-            );
-        """.list(mapOf("saksbehandlerIdent" to saksbehandlerIdent)) { row ->
+            AND (a.siste_sekvensnummer IS NULL OR o.SEKVENSNUMMER > a.siste_sekvensnummer)
+        """
+        return query.list(mapOf("saksbehandlerIdent" to saksbehandlerIdent)) { row ->
             OpptegnelseDto(
                 payload = row.string("payload"),
                 aktørId = row.long("aktor_id"),
@@ -35,4 +39,5 @@ class OpptegnelseDao(dataSource: DataSource) : HelseDao(dataSource) {
                 type = OpptegnelseType.valueOf(row.string("type"))
             )
         }
+    }
 }
