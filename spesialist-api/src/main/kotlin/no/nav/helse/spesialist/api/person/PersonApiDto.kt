c329bf9727ac3fd8ce71f763c32aff0dad65ad87
Stephen Ramthun <stephenramthun@gmail.com>
2022-09-08 11:33:53 +0200

    Fjern ubrukte dataklasser

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/person/PersonApiDto.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/person/PersonApiDto.kt
@@ -1,11 +1,5 @@
 package no.nav.helse.spesialist.api.person
 
-import com.fasterxml.jackson.databind.JsonNode
-import no.nav.helse.spesialist.api.arbeidsgiver.ArbeidsforholdApiDto
-import no.nav.helse.spesialist.api.arbeidsgiver.ArbeidsgiverApiDto
-import no.nav.helse.spesialist.api.tildeling.TildelingApiDto
-import no.nav.helse.spesialist.api.utbetaling.UtbetalingApiDto
-import no.nav.helse.spesialist.api.vedtaksperiode.EnhetDto
 import java.time.LocalDate
 
 data class PersoninfoApiDto(
@@ -17,12 +11,6 @@ data class PersoninfoApiDto(
     val adressebeskyttelse: Adressebeskyttelse
 )
 
-data class PersonMetadataApiDto(
-    val fødselsnummer: String,
-    val aktørId: String,
-    val personinfo: PersoninfoApiDto
-)
-
 enum class Kjønn { Mann, Kvinne, Ukjent }
 
 enum class Adressebeskyttelse {
@@ -32,18 +20,3 @@ enum class Adressebeskyttelse {
     StrengtFortroligUtland,
     Ukjent
 }
-
-data class PersonForSpeilDto(
-    val utbetalinger: List<UtbetalingApiDto>,
-    val aktørId: String,
-    val fødselsnummer: String,
-    val dødsdato: LocalDate?,
-    val personinfo: PersoninfoApiDto,
-    val arbeidsgivere: List<ArbeidsgiverApiDto>,
-    val infotrygdutbetalinger: JsonNode?,
-    val enhet: EnhetDto,
-    val arbeidsforhold: List<ArbeidsforholdApiDto>,
-    val inntektsgrunnlag: JsonNode,
-    val vilkårsgrunnlagHistorikk: JsonNode?,
-    val tildeling: TildelingApiDto?
-)
