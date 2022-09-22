2994c4d99f597107a8cdd70a7a9c2d92bed041cd
Sindre Broch <sindre.broch@nav.no>
2022-09-22 13:42:23 +0200

    Legger til logging for vedtaksperiode reberegnet periodehistorikk
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/kommando/VedtaksperiodeReberegnetPeriodehistorikk.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/kommando/VedtaksperiodeReberegnetPeriodehistorikk.kt
@@ -4,6 +4,7 @@ import java.util.UUID
 import no.nav.helse.modell.utbetaling.UtbetalingDao
 import no.nav.helse.spesialist.api.periodehistorikk.PeriodehistorikkDao
 import no.nav.helse.spesialist.api.periodehistorikk.PeriodehistorikkType
+import org.slf4j.LoggerFactory
 
 internal class VedtaksperiodeReberegnetPeriodehistorikk(
     private val vedtaksperiodeId: UUID,
@@ -11,12 +12,17 @@ internal class VedtaksperiodeReberegnetPeriodehistorikk(
     private val periodehistorikkDao: PeriodehistorikkDao
 ) : Command {
 
+    private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
+
     override fun execute(context: CommandContext): Boolean {
         val utbetalinger = utbetalingDao.utbetalingerForVedtaksperiode(vedtaksperiodeId)
         val utbetalingId = utbetalinger.firstOrNull()?.utbetalingId
 
         if(utbetalingId != null) {
+            sikkerLogg.info("Vedtaksperiode reberegnet lagres som periodehistorikk på vedtaksperiode {} med utbetalingId {}", vedtaksperiodeId, utbetalingId)
             periodehistorikkDao.lagre(PeriodehistorikkType.VEDTAKSPERIODE_REBEREGNET, null, utbetalingId)
+        } else {
+            sikkerLogg.info("Kunne ikke legge til periodehistorikk ved reberegnet vedtaksperiode. Finner ikke utbetalingId for vedtaksperiodeId {} ", vedtaksperiodeId)
         }
 
         return true
