3e6c849c1fc46d08344c431294d8b8cadf77f735
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2022-09-26 14:03:06 +0200

    Oppgraderer postgres igjen
    Tipper det var flyway-oppgraderingen som ødela.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val flywayCoreVersion = "8.5.11"
 val kotliqueryVersion = "1.9.0"
 val kotlinTestVersion = "1.6.21"
 val mockkVersion = "1.12.8"
-val postgressqlVersion = "42.3.7"
+val postgressqlVersion = "42.5.0"
 
 plugins {
     kotlin("jvm") version "1.6.21"
