ba677f1b11effb9c41d286b64c7fa7dae9aaaf29
Øyvind Hagen <oyvind.hagen@nav.no>
2022-10-04 16:04:19 +0200

    Sletter duplikate personer

--- spesialist-felles/src/main/resources/db/migration/V238__slette_duplikat_person.sql
+++ spesialist-felles/src/main/resources/db/migration/V238__slette_duplikat_person.sql
@@ -1,21 +1,9 @@
-delete
-from person
-where id = 447909;
-delete
-from person
-where id = 453317;
+delete from person where id = 447909;
+delete from person where id = 453317;
 
-delete
-from person_info
-where id = 447937;
-delete
-from person_info
-where id = 453345;
+delete from person_info where id = 447937;
+delete from person_info where id = 453345;
 
-delete
-from infotrygdutbetalinger
-where id = 442041;
-delete
-from infotrygdutbetalinger
-where id = 447478;
+delete from infotrygdutbetalinger where id = 442041;
+delete from infotrygdutbetalinger where id = 447478;
 
