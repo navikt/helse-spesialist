7303af03ab61835573b45046ab41fd6d1b3d4a19
Stephen Ramthun <stephenramthun@gmail.com>
2022-10-07 13:18:18 +0200

    Flytt graphql-plugin fra selve til api

--- spesialist-api/build.gradle.kts
+++ spesialist-api/build.gradle.kts
@@ -1,6 +1,52 @@
+import com.expediagroup.graphql.plugin.gradle.tasks.GraphQLGenerateSDLTask
+
 val testcontainersVersion = "1.17.3"
+val graphQLKotlinVersion = "5.5.0"
+
+plugins {
+    kotlin("plugin.serialization") version "1.6.21"
+    id("com.expediagroup.graphql") version "5.4.1"
+}
 
 dependencies {
     implementation(project(":spesialist-felles"))
+    implementation("com.expediagroup:graphql-kotlin-server:$graphQLKotlinVersion")
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
 }
+val graphqlIntrospectSchema by tasks.getting(com.expediagroup.graphql.plugin.gradle.tasks.GraphQLIntrospectSchemaTask::class) {
+    endpoint.set("https://spleis-api.dev.intern.nav.no/graphql")
+    outputFile.set(File("${project.projectDir}/src/main/resources/graphql/schema.graphql"))
+}
+
+// Disabler automatisk kjøring av introspection siden henting av schema feiler under bygg på Github.
+// Kan kjøres manuelt for å hente nytt schema.
+graphqlIntrospectSchema.enabled = System.getenv("CI") != "true"
+
+val graphqlGenerateClient by tasks.getting(com.expediagroup.graphql.plugin.gradle.tasks.GraphQLGenerateClientTask::class) {
+    val baseDir = "${project.projectDir}/src/main/resources/graphql"
+
+    serializer.set(com.expediagroup.graphql.plugin.gradle.config.GraphQLSerializer.JACKSON)
+    packageName.set("no.nav.helse.spesialist.api.graphql")
+    schemaFile.set(graphqlIntrospectSchema.outputFile)
+    queryFiles.from(
+        listOf(
+            File("$baseDir/hentSnapshot.graphql")
+        )
+    )
+
+    if (graphqlIntrospectSchema.enabled) {
+        dependsOn("graphqlIntrospectSchema")
+    }
+}
+
+val graphqlGenerateSDL by tasks.getting(GraphQLGenerateSDLTask::class) {
+    packages.set(
+        listOf(
+            "no.nav.helse.mediator.api.graphql",
+            "no.nav.helse.mediator.api.graphql.schema",
+            "no.nav.helse.mediator.graphql"
+        )
+    )
+}
+
+graphqlGenerateSDL.enabled = false
--- spesialist-selve/build.gradle.kts
+++ spesialist-selve/build.gradle.kts
@@ -1,8 +1,3 @@
-import com.expediagroup.graphql.plugin.gradle.config.GraphQLSerializer.JACKSON
-import com.expediagroup.graphql.plugin.gradle.tasks.GraphQLGenerateClientTask
-import com.expediagroup.graphql.plugin.gradle.tasks.GraphQLGenerateSDLTask
-import com.expediagroup.graphql.plugin.gradle.tasks.GraphQLIntrospectSchemaTask
-
 val graphQLKotlinVersion = "5.5.0"
 val testcontainersVersion = "1.17.3"
 val ktorVersion = "2.0.1"
@@ -19,44 +14,6 @@ dependencies {
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
     testImplementation("io.ktor:ktor-server-netty:$ktorVersion")
 }
-
-val graphqlIntrospectSchema by tasks.getting(GraphQLIntrospectSchemaTask::class) {
-    endpoint.set("https://spleis-api.dev.intern.nav.no/graphql")
-    outputFile.set(File("${project.projectDir}/src/main/resources/graphql/schema.graphql"))
-}
-
-// Disabler automatisk kjøring av introspection siden henting av schema feiler under bygg på Github.
-// Kan kjøres manuelt for å hente nytt schema.
-graphqlIntrospectSchema.enabled = System.getenv("CI") != "true"
-
-val graphqlGenerateClient by tasks.getting(GraphQLGenerateClientTask::class) {
-    val baseDir = "${project.projectDir}/src/main/resources/graphql"
-
-    serializer.set(JACKSON)
-    packageName.set("no.nav.helse.mediator.graphql")
-    schemaFile.set(graphqlIntrospectSchema.outputFile)
-    queryFiles.from(
-        listOf(
-            File("$baseDir/hentSnapshot.graphql")
-        )
-    )
-
-    if (graphqlIntrospectSchema.enabled) {
-        dependsOn("graphqlIntrospectSchema")
-    }
-}
-
-val graphqlGenerateSDL by tasks.getting(GraphQLGenerateSDLTask::class) {
-    packages.set(
-        listOf(
-            "no.nav.helse.mediator.api.graphql",
-            "no.nav.helse.mediator.api.graphql.schema",
-            "no.nav.helse.mediator.graphql"
-        )
-    )
-}
-
-graphqlGenerateSDL.enabled = false
 
 tasks {
     named<Jar>("jar") {
