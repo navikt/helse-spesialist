253e095ef626262756913265c4e6aa12b54668ca
Stephen Ramthun <stephenramthun@gmail.com>
2022-10-13 13:55:58 +0200

    Fjern ubrukt kode

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/arbeidsgiver/ArbeidsgiverApiDto.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/arbeidsgiver/ArbeidsgiverApiDto.kt
@@ -1,9 +1,6 @@
 package no.nav.helse.spesialist.api.arbeidsgiver
 
-import com.fasterxml.jackson.databind.JsonNode
-import no.nav.helse.spesialist.api.overstyring.OverstyringApiDto
 import java.time.LocalDate
-import java.util.*
 
 data class ArbeidsforholdApiDto(
     val organisasjonsnummer: String,
@@ -11,16 +8,4 @@ data class ArbeidsforholdApiDto(
     val stillingsprosent: Int,
     val startdato: LocalDate,
     val sluttdato: LocalDate?
-)
-
-data class ArbeidsgiverApiDto(
-    val organisasjonsnummer: String,
-    val navn: String,
-    val id: UUID,
-    val vedtaksperioder: List<JsonNode>,
-    val overstyringer: List<OverstyringApiDto>,
-    val bransjer: List<String>,
-    val utbetalingshistorikk: List<UtbetalingshistorikkElementApiDto>,
-    val generasjoner: JsonNode?,
-    val ghostPerioder: List<JsonNode>?
 )
