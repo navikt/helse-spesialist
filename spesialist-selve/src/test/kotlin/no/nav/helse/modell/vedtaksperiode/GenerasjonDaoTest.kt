7da1e45b911f9ce8c5d74cea20ebd76280940dc1
Elias A. Thøgersen <elias3004@msn.com>
2022-10-20 15:15:49 +0200

    Refaktorerer GenerasjonDaoTest.kt

--- spesialist-selve/src/test/kotlin/no/nav/helse/modell/vedtaksperiode/GenerasjonDaoTest.kt
+++ spesialist-selve/src/test/kotlin/no/nav/helse/modell/vedtaksperiode/GenerasjonDaoTest.kt
@@ -2,66 +2,60 @@ package no.nav.helse.modell.vedtaksperiode
 
 import DatabaseIntegrationTest
 import java.util.UUID
+import no.nav.helse.Testdata.VEDTAKSPERIODE_ID
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class GenerasjonDaoTest : DatabaseIntegrationTest() {
 
+    private companion object {
+        private val VEDTAKSPERIODE_ENDRET_HENDELSE_ID = UUID.randomUUID()
+        private val VEDTAK_FATTET_HENDELSE_ID = UUID.randomUUID()
+    }
+
     @Test
-    fun `finner generasjon for vedtaksperiode`() {
-        val vedtaksperiodeId = UUID.randomUUID()
-        val vedtaksperiodeEndretId = UUID.randomUUID()
-        val generasjon = generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
+    fun `oppretter generasjon for vedtaksperiode`() {
+        val generasjon = generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
 
         assertEquals(1L, generasjon)
     }
 
     @Test
-    fun `oppretter ikke generasjon, så lenge det finnes åpen`() {
-        val vedtaksperiodeId = UUID.randomUUID()
-        val vedtaksperiodeEndretId = UUID.randomUUID()
-        val spørring1 = generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
-        val spørring2 = generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
+    fun `oppretter ikke generasjon, så lenge det finnes en ulåst generasjon`() {
+        val førsteGenerasjon = generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
+        val andreGenerasjonBurdeIkkeOpprettes =
+            generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
 
-        assertEquals(1L, spørring1)
-        assertEquals(null, spørring2)
+        assertEquals(1L, førsteGenerasjon)
+        assertEquals(null, andreGenerasjonBurdeIkkeOpprettes)
     }
 
     @Test
-    fun `får ny generasjon når forrige er låst`() {
-        val vedtaksperiodeId = UUID.randomUUID()
-        val vedtaksperiodeEndretId = UUID.randomUUID()
-        val vedtakFattetId = UUID.randomUUID()
-        val spørring1 = generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
-        generasjonDao.låsGenerasjon(vedtaksperiodeId, vedtakFattetId)
-        val spørring2 = generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
+    fun `oppretter ny generasjon når forrige er låst`() {
+        val førsteGenerasjon = generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
+        generasjonDao.låsGenerasjon(VEDTAKSPERIODE_ID, VEDTAK_FATTET_HENDELSE_ID)
+        val andreGenerasjon = generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
 
-        assertEquals(1L, spørring1)
-        assertEquals(2L, spørring2)
+        assertEquals(1L, førsteGenerasjon)
+        assertEquals(2L, andreGenerasjon)
     }
 
     @Test
-    fun `sjekker at lås returnerer iden til raden`() {
-        val vedtaksperiodeId = UUID.randomUUID()
-        val vedtaksperiodeEndretId = UUID.randomUUID()
-        val vedtakFattetId = UUID.randomUUID()
-        val spørring1 = generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
-        val låstGenerasjon = generasjonDao.låsGenerasjon(vedtaksperiodeId, vedtakFattetId)
+    fun `sjekker at låsGenerasjon returnerer id'en til raden`() {
+        val førsteGenerasjon = generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
+        val låstGenerasjon = generasjonDao.låsGenerasjon(VEDTAKSPERIODE_ID, VEDTAK_FATTET_HENDELSE_ID)
 
-        assertEquals(1L, spørring1)
-        assertEquals(spørring1, låstGenerasjon)
+        assertEquals(1L, førsteGenerasjon)
+        assertEquals(førsteGenerasjon, låstGenerasjon)
     }
 
     @Test
     fun `sjekker at siste generasjon blir returnert`() {
-        val vedtaksperiodeId = UUID.randomUUID()
-        val vedtaksperiodeEndretId = UUID.randomUUID()
-        val vedtakFattetId = UUID.randomUUID()
-        generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
-        generasjonDao.låsGenerasjon(vedtaksperiodeId, vedtakFattetId)
-        val spørring2 = generasjonDao.prøvOpprett(vedtaksperiodeId, vedtaksperiodeEndretId)
-        val gjeldendeGenerasjon = generasjonDao.generasjon(vedtaksperiodeId)
+        generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
+        generasjonDao.låsGenerasjon(VEDTAKSPERIODE_ID, VEDTAK_FATTET_HENDELSE_ID)
+        val andreGenerasjon = generasjonDao.prøvOpprett(VEDTAKSPERIODE_ID, VEDTAKSPERIODE_ENDRET_HENDELSE_ID)
+        val gjeldendeGenerasjon = generasjonDao.generasjon(VEDTAKSPERIODE_ID)
 
-        assertEquals(gjeldendeGenerasjon, spørring2)
+        assertEquals(gjeldendeGenerasjon, andreGenerasjon)
     }
 }
