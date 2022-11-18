f51818050c76de560f88db67f2ceec69816cd7b0
Elias A. Thøgersen <elias3004@msn.com>
2022-11-18 10:27:49 +0100

    Endrer kode for definisjon for teknisk feil ved 8-4
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

---
+++ spesialist-felles/src/main/resources/db/migration/V251__endre_kode_for_definisjon_for_teknisk_feil_8_4.sql
@@ -0,0 +1,4 @@
+UPDATE api_varseldefinisjon
+SET kode = 'SB_RV_3'
+WHERE tittel =
+      'Arbeidsuførhet, aktivitetsplikt og/eller medvirkning må vurderes. Klarte ikke gjøre automatisk 8-4-vurdering p.g.a. teknisk feil. Kan godkjennes hvis alt ser greit ut.';
