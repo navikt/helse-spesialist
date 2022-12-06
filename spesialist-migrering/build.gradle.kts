01a5029cdfb9e5478fc828f2af719cb5b8cb5130
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2022-12-06 13:35:48 +0100

    Fiks dockerignore og dependencies på cloudsql
    Co-authored-by: Elias Andreassen Thøgersen <elias.andreassen.thogersen@nav.no>

--- spesialist-migrering/build.gradle.kts
+++ spesialist-migrering/build.gradle.kts
@@ -1,27 +1,29 @@
 val testcontainersVersion = "1.17.3"
+val cloudSqlVersion = "1.6.1"
+val postgresqlVersion = "42.3.6"
 
 dependencies {
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
+    implementation("com.google.cloud.sql:postgres-socket-factory:$cloudSqlVersion")
     implementation(project(":spesialist-felles"))
     testImplementation("org.testcontainers:postgresql:$testcontainersVersion")
 }
 
-tasks {
-    named<Jar>("jar") {
-        archiveBaseName.set("app")
+tasks.named<Jar>("jar") {
+    archiveBaseName.set("app")
 
-        manifest {
-            attributes["Main-Class"] = "no.nav.helse.migrering.AppKt"
-            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
-                it.name
-            }
+    manifest {
+        attributes["Main-Class"] = "no.nav.helse.migrering.AppKt"
+        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+            it.name
         }
+    }
 
-        doLast {
-            configurations.runtimeClasspath.get().forEach {
-                val file = File("$buildDir/libs/${it.name}")
-                if (!file.exists())
-                    it.copyTo(file)
-            }
+    doLast {
+        configurations.runtimeClasspath.get().forEach {
+            val file = File("$buildDir/libs/${it.name}")
+            if (!file.exists())
+                it.copyTo(file)
         }
     }
 }
