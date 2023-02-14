3bb9e601d512cfd67fd365e0c6ef303c3e774340
Elias A. Thøgersen <elias3004@msn.com>
2023-02-14 10:43:46 +0100

    Sletter feilende migrering
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- spesialist-felles/src/main/resources/db/migration/V302__grant_select_on_temp_manglende_varsler.sql
+++
@@ -1,8 +0,0 @@
-DO
-$do$
-    BEGIN
-        IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname='spesialist') THEN
-            GRANT SELECT ON ALL TABLES IN SCHEMA public TO "spesialist";
-        END IF;
-    END
-$do$
