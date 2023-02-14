3f241d6fcf2c61b89fcbfa7dadc1b263db210d5c
Elias A. Thøgersen <elias3004@msn.com>
2023-02-14 10:23:25 +0100

    Gir spesialist lesetilgang til temp_manglende_varsler
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- spesialist-felles/src/main/resources/db/migration/V302__grant_select_on_temp_manglende_varsler.sql
+++ spesialist-felles/src/main/resources/db/migration/V302__grant_select_on_temp_manglende_varsler.sql
@@ -2,7 +2,7 @@ DO
 $do$
     BEGIN
         IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname='spesialist') THEN
-            GRANT SELECT ON temp_manglende_varsler TO "spesialist";
+            GRANT SELECT ON ALL TABLES IN SCHEMA public TO "spesialist";
         END IF;
     END
 $do$
