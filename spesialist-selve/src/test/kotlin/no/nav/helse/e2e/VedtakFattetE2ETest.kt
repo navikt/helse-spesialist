9249aef44c4dcbb58e549c82bb9864d25981f04a
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-03-16 07:47:57 +0100

    🧑‍💻 Flytt VedtakFattetE2E over til nytt testoppsett

--- spesialist-selve/src/test/kotlin/no/nav/helse/e2e/VedtakFattetE2ETest.kt
+++ spesialist-selve/src/test/kotlin/no/nav/helse/e2e/VedtakFattetE2ETest.kt
@@ -1,22 +1,33 @@
 package no.nav.helse.e2e
 
-import AbstractE2ETest
+import AbstractE2ETestV2
 import java.util.UUID
-import no.nav.helse.Testdata.FØDSELSNUMMER
+import kotliquery.queryOf
+import kotliquery.sessionOf
+import no.nav.helse.Testdata.VEDTAKSPERIODE_ID
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 
-internal class VedtakFattetE2ETest: AbstractE2ETest() {
-
-    private companion object {
-        private val VEDTAKSPERIODE_ID = UUID.randomUUID()
-    }
+internal class VedtakFattetE2ETest: AbstractE2ETestV2() {
 
     @Test
     fun `vedtak fattet medfører låsing av vedtaksperiode-generasjon`() {
-        settOppBruker()
+        fremTilSaksbehandleroppgave()
+        håndterSaksbehandlerløsning()
         assertDoesNotThrow {
-            sendVedtakFattet(FØDSELSNUMMER, VEDTAKSPERIODE_ID)
+            håndterVedtakFattet()
         }
+        assertLåsteGenerasjoner(VEDTAKSPERIODE_ID)
+    }
+
+    private fun assertLåsteGenerasjoner(vedtaksperiodeId: UUID) {
+        @Language("PostgreSQL")
+        val query = "SELECT 1 FROM selve_vedtaksperiode_generasjon svg WHERE vedtaksperiode_id = :vedtaksperiode_id AND låst = false"
+        val antallUlåste = sessionOf(dataSource).use { session ->
+            session.run(queryOf(query, mapOf("vedtaksperiode_id" to vedtaksperiodeId)).map { it.int(1) }.asSingle) ?: 0
+        }
+        assertEquals(0, antallUlåste)
     }
 }
