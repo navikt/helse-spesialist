010cae78263ccc3c7ffcc0099126f527f02f1ee3
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-03-29 18:31:09 +0200

    🔥 Opprydding

--- spesialist-migrering/src/main/kotlin/no/nav/helse/migrering/ApplicationBuilder.kt
+++ spesialist-migrering/src/main/kotlin/no/nav/helse/migrering/ApplicationBuilder.kt
@@ -1,12 +1,11 @@
 package no.nav.helse.migrering
 
 import no.nav.helse.migrering.db.DataSourceBuilder
-import no.nav.helse.migrering.db.DataSourceBuilder.DbUser.SPESIALIST
 import no.nav.helse.migrering.db.SpesialistDao
 import no.nav.helse.rapids_rivers.RapidApplication
 
 internal class ApplicationBuilder(env: Map<String, String>) {
-    private val spesialistDataSourceBuilder = DataSourceBuilder(env, SPESIALIST)
+    private val spesialistDataSourceBuilder = DataSourceBuilder(env)
     private val rapidsConnection = RapidApplication.create(env)
 
     init {
