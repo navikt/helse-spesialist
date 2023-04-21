dc2c4b126a745dffdc0d5524be7112fffc4ee086
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-04-21 13:19:43 +0200

    🗃️ Setter strict-flagget i HelseDao sin Query.single
    Fordi vi alltid ønsker at det skal feile om det skulle være mer enn én
    rad i resultatsettet.

--- spesialist-felles/src/main/kotlin/no/nav/helse/HelseDao.kt
+++ spesialist-felles/src/main/kotlin/no/nav/helse/HelseDao.kt
@@ -26,7 +26,7 @@ abstract class HelseDao(private val dataSource: DataSource) {
     }
 
     fun asSQL(@Language("SQL") sql: String, argMap: Map<String, Any> = emptyMap()) = queryOf(sql, argMap)
-    fun <T> Query.single(mapping: (Row) -> T?) = sessionOf(dataSource).use { session -> session.run(this.map { mapping(it) }.asSingle) }
+    fun <T> Query.single(mapping: (Row) -> T?) = sessionOf(dataSource, strict = true).use { session -> session.run(this.map { mapping(it) }.asSingle) }
     fun <T> Query.list(mapping: (Row) -> T?) = sessionOf(dataSource).use { session -> session.run(this.map { mapping(it) }.asList) }
     fun Query.update() = sessionOf(dataSource).use { session -> session.run(this.asUpdate) }
     fun Query.updateAndReturnGeneratedKey() = sessionOf(dataSource, returnGeneratedKey = true).use { session -> session.run(this.asUpdateAndReturnGeneratedKey) }
