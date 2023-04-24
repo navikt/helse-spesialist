c669a6033dedf505123d74010a04f989ac9fdfed
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-04-24 12:07:49 +0200

    🔥 Fjern ubrukt funksjon

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/GenerasjonRepository.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/GenerasjonRepository.kt
@@ -9,7 +9,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 internal interface GenerasjonRepository {
-    fun sisteForLenient(vedtaksperiodeId: UUID): Generasjon? = null
     fun tilhørendeFor(utbetalingId: UUID): List<Generasjon>
 }
 
@@ -96,10 +95,6 @@ internal class ActualGenerasjonRepository(dataSource: DataSource) : GenerasjonRe
                 kv("generasjonId", generasjonId),
                 kv("hendelseId", hendelseId)
             )
-    }
-
-    override fun sisteForLenient(vedtaksperiodeId: UUID): Generasjon? {
-        return dao.finnSisteFor(vedtaksperiodeId)?.also { it.registrer(this) }
     }
 
     override fun tilhørendeFor(utbetalingId: UUID): List<Generasjon> {
