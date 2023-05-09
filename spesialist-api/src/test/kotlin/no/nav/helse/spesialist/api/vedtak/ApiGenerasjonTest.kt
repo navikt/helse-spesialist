aa57600bcdc19fb0bebfb54cc08ceb3e5f36c1bb
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2023-05-09 10:56:15 +0200

    ✅ tester tidligereEnnOgSammenhengende
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

---
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/vedtak/ApiGenerasjonTest.kt
@@ -0,0 +1,66 @@
+package no.nav.helse.spesialist.api.vedtak
+
+import java.time.LocalDate
+import java.util.UUID
+import no.nav.helse.spesialist.api.april
+import no.nav.helse.spesialist.api.februar
+import no.nav.helse.spesialist.api.januar
+import no.nav.helse.spesialist.api.mars
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class ApiGenerasjonTest {
+
+    @Test
+    fun `sammenhengende - samme periode`() {
+        val generasjon1 = opprettApiGenerasjon(1.januar, 31.januar, 1.januar)
+        val generasjon2 = opprettApiGenerasjon(1.januar, 31.januar, 1.januar)
+
+        assertTrue(generasjon1.tidligereEnnOgSammenhengende(generasjon2))
+    }
+
+    @Test
+    fun `sammenhengende - ligger tidligere enn og kant i kant og har samme skjæringstidspunkt`() {
+        val generasjon1 = opprettApiGenerasjon(1.januar, 31.januar, 1.januar)
+        val generasjon2 = opprettApiGenerasjon(1.februar, 28.februar, 1.januar)
+
+        assertTrue(generasjon1.tidligereEnnOgSammenhengende(generasjon2))
+    }
+
+    @Test
+    fun `sammenhengende - ligger tidligere enn og har opphold og har samme skjæringstidspunkt`() {
+        val generasjon1 = opprettApiGenerasjon(1.januar, 31.januar, 1.januar)
+        val generasjon2 = opprettApiGenerasjon(1.mars, 31.mars, 1.januar)
+
+        assertTrue(generasjon1.tidligereEnnOgSammenhengende(generasjon2))
+    }
+
+    @Test
+    fun `sammenhengende - samme skjæringstidspunkt og overlapper med én dag`() {
+        val generasjon1 = opprettApiGenerasjon(1.april, 30.april, 1.mars)
+        val generasjon2 = opprettApiGenerasjon(1.mars, 1.april, 1.mars)
+
+        assertTrue(generasjon1.tidligereEnnOgSammenhengende(generasjon2))
+    }
+
+    @Test
+    fun `ikke sammenhengende - ligger tidligere enn og ulikt skjæringstidspunkt`() {
+        val generasjon1 = opprettApiGenerasjon(1.januar, 31.januar, 1.januar)
+        val generasjon2 = opprettApiGenerasjon(1.mars, 31.mars, 1.mars)
+
+        assertFalse(generasjon1.tidligereEnnOgSammenhengende(generasjon2))
+    }
+
+    @Test
+    fun `ikke sammenhengende - samme skjæringstidspunkt men ligger senere enn`() {
+        val generasjon1 = opprettApiGenerasjon(1.april, 30.april, 1.mars)
+        val generasjon2 = opprettApiGenerasjon(1.mars, 31.mars, 1.mars)
+
+        assertFalse(generasjon1.tidligereEnnOgSammenhengende(generasjon2))
+    }
+
+    private fun opprettApiGenerasjon(fom: LocalDate, tom: LocalDate, skjæringstidspunkt: LocalDate): ApiGenerasjon {
+        return ApiGenerasjon(UUID.randomUUID(), fom, tom, skjæringstidspunkt, emptySet())
+    }
+}
