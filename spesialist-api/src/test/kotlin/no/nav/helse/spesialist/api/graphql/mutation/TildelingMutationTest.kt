7c4a437ebf32c3a3e090e2df0d2ed97c60b01ad0
Elias A. Thøgersen <elias3004@msn.com>
2023-05-15 15:31:29 +0200

    ✅ tester for fjernTildeling
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/mutation/TildelingMutationTest.kt
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/mutation/TildelingMutationTest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.module.kotlin.contains
 import java.util.UUID
 import no.nav.helse.spesialist.api.AbstractGraphQLApiTest
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
@@ -57,5 +58,64 @@ internal class TildelingMutationTest : AbstractGraphQLApiTest() {
         )
 
         assertTrue(body.contains("errors"))
+    }
+
+    @Test
+    fun `kan fjerne tildeling`() {
+        opprettSaksbehandler()
+        opprettVedtaksperiode(opprettPerson(), opprettArbeidsgiver())
+        val oppgaveId = finnOppgaveIdFor(PERIODE.id)
+        tildelOppgave(oppgaveId, SAKSBEHANDLER.oid)
+
+        val body = runQuery(
+            """
+                mutation FjernTildeling {
+                    fjernTildeling(
+                        oppgaveId: "$oppgaveId"
+                    )
+                }
+            """
+        )
+
+        assertTrue(body["data"]["fjernTildeling"].booleanValue())
+    }
+
+    @Test
+    fun `returnerer false hvis oppgaven ikke er tildelt`() {
+        opprettSaksbehandler()
+        opprettVedtaksperiode(opprettPerson(), opprettArbeidsgiver())
+        val oppgaveId = finnOppgaveIdFor(PERIODE.id)
+
+        val body = runQuery(
+            """
+                mutation FjernTildeling {
+                    fjernTildeling(
+                        oppgaveId: "$oppgaveId"
+                    )
+                }
+            """
+        )
+
+        assertFalse(body["data"]["fjernTildeling"].booleanValue())
+    }
+
+    @Test
+    fun `returnerer false hvis oppgaven ikke finnes`() {
+        opprettSaksbehandler()
+        opprettVedtaksperiode(opprettPerson(), opprettArbeidsgiver())
+        val oppgaveId = finnOppgaveIdFor(PERIODE.id)
+        tildelOppgave(oppgaveId, SAKSBEHANDLER.oid)
+
+        val body = runQuery(
+            """
+                mutation FjernTildeling {
+                    fjernTildeling(
+                        oppgaveId: "999"
+                    )
+                }
+            """
+        )
+
+        assertFalse(body["data"]["fjernTildeling"].booleanValue())
     }
 }
