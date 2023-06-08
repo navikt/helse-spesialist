4f6ebc3a6c1a2cdc8534b0e924190e1bcb44c985
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-06-08 20:35:44 +0200

    🗃️ Manglende generasjoner for to AUU-er fra desember 2022 😕

---
+++ spesialist-felles/src/main/resources/db/migration/V351__manglende_generasjoner.sql
@@ -0,0 +1,3 @@
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse, tilstand)
+values (gen_random_uuid(), '08a20197-8906-44d6-b149-a1dbbd5aa1c4', '00000000-0000-0000-0000-000000000000', 'Ulåst'),
+       (gen_random_uuid(), '7cbbad2a-c42a-4633-a7c2-2172daac1cfe', '00000000-0000-0000-0000-000000000000', 'Ulåst')
