68906be6b65c3f70dee7e45d191d278e350342bc
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-06-30 16:00:57 +0200

    🔥 Fjern ubrukt funksjonalitet

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/notat/NotatMediator.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/notat/NotatMediator.kt
@@ -1,8 +1,8 @@
 package no.nav.helse.spesialist.api.notat
 
-import org.slf4j.LoggerFactory
-import java.util.*
+import java.util.UUID
 import no.nav.helse.spesialist.api.graphql.schema.NotatType
+import org.slf4j.LoggerFactory
 
 class NotatMediator(
     private val notatDao: NotatDao,
@@ -10,32 +10,10 @@ class NotatMediator(
 
     private companion object {
         private val log = LoggerFactory.getLogger(NotatMediator::class.java)
-        private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
     }
-
-    fun lagre(vedtaksperiodeId: UUID, tekst: String, saksbehandler_oid: UUID, notatType: NotatType = NotatType.Generelt) =
-        notatDao.opprettNotat(vedtaksperiodeId, tekst, saksbehandler_oid, notatType)
 
     fun lagreForOppgaveId(oppgaveId: Long, tekst: String, saksbehandler_oid: UUID, notatType: NotatType = NotatType.Generelt): Long? {
         log.info("{} lagrer {} notat for oppgaveId {}", saksbehandler_oid, notatType, oppgaveId)
         return notatDao.opprettNotatForOppgaveId(oppgaveId, tekst, saksbehandler_oid, notatType)
-    }
-
-    fun finn(vedtaksperiodeIds: List<UUID>) =
-        notatDao.finnNotater(vedtaksperiodeIds)
-
-    fun feilregistrer(notatId: Int, saksbehandler_oid: UUID): Boolean {
-        val notat = notatDao.finnNotat(notatId)
-        if (notat == null ){
-            log.warn("Fant ikke notat til feilregistrering med id=${notatId}")
-            return false
-        }
-        if (notat.saksbehandlerOid != saksbehandler_oid){
-            sikkerLogg.warn("saksbehandler med oid=${saksbehandler_oid} kan ikke feilregistrere notat oppført av annen saksbehandler med oid=${notat.saksbehandlerOid}")
-            return false
-        }
-        notatDao.feilregistrerNotat(notatId)
-        sikkerLogg.info("notat med id=${notatId} ble feilregistrert: {}", notat)
-        return true
     }
 }
