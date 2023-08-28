a47134a1a720057b3ce9342d44c393be751eb2a2
Øyvind Hagen <oyvind.hagen@nav.no>
2023-08-28 15:48:33 +0200

    Revert "🐛 Skipper meldinger i dev som har oppgitt orgnummer som tekststreng"
    This reverts commit fce405a8cae63358f51ec623c95482a6996a96cd.

--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/VedtaksperiodeOpprettetRiver.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/VedtaksperiodeOpprettetRiver.kt
@@ -3,7 +3,6 @@ package no.nav.helse.mediator.meldinger
 import java.util.UUID
 import net.logstash.logback.argument.StructuredArguments
 import no.nav.helse.mediator.HendelseMediator
-import no.nav.helse.mediator.api.erDev
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 import no.nav.helse.rapids_rivers.RapidsConnection
@@ -42,14 +41,6 @@ internal class VedtaksperiodeOpprettetRiver(
             StructuredArguments.keyValue("vedtaksperiodeId", vedtaksperiodeId),
             StructuredArguments.keyValue("hendelseId", id),
         )
-        if (erDev()) {
-            try {
-                organisasjonsnummer.toLong()
-            } catch (exception: NumberFormatException) {
-                log.warn("Mottok '$organisasjonsnummer' som organisasjonsnummer og konvertering til Long feilet. Skipper melding...")
-                return
-            }
-        }
         mediator.vedtaksperiodeOpprettet(
             packet,
             id,
