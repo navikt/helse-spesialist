059d67323e91d1878957f1f3247d3a09f8d7713d
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-09-11 22:31:16 +0200

    ♻️ Egen komponent for å sette opp appen for http-testing

--- spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/endepunkter/AnnulleringApiTest.kt
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/endepunkter/AnnulleringApiTest.kt
@@ -1,53 +1,38 @@
 package no.nav.helse.spesialist.api.endepunkter
 
+import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.accept
 import io.ktor.client.request.post
-import io.ktor.client.request.preparePost
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.http.isSuccess
-import io.mockk.clearMocks
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
 import java.util.UUID
-import kotlinx.coroutines.runBlocking
-import no.nav.helse.spesialist.api.AbstractApiTest
 import no.nav.helse.spesialist.api.Saksbehandlerhåndterer
 import no.nav.helse.spesialist.api.saksbehandler.handlinger.AnnulleringHandling
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 
-@TestInstance(PER_CLASS)
-internal class AnnulleringApiTest : AbstractApiTest() {
+internal class AnnulleringApiTest {
 
     private val SAKSBEHANDLER_OID = UUID.randomUUID()
 
-    private lateinit var saksbehandlerhåndterer: Saksbehandlerhåndterer
+    private val saksbehandlerhåndterer = mockk<Saksbehandlerhåndterer>(relaxed = true)
 
-    @BeforeAll
-    fun setupTildeling() {
-        saksbehandlerhåndterer = mockk(relaxed = true)
-        setupServer {
-            annulleringApi(saksbehandlerhåndterer)
-        }
+    private val apiTesting = ApiTesting {
+        annulleringApi(saksbehandlerhåndterer)
     }
 
-    @AfterEach
-    fun tearDownEach() {
-        clearMocks(saksbehandlerhåndterer)
-    }
+    private fun HttpRequestBuilder.auth(oid: UUID, group: String? = null) = apiTesting.authentication(oid, group, this)
 
     @Test
     fun annulleringOk() {
         val slot = slot<AnnulleringHandling>()
-        val response = runBlocking {
+        val response = apiTesting.spesialistApi { client ->
             client.post("/api/annullering") {
                 contentType(ContentType.Application.Json)
                 accept(ContentType.Application.Json)
@@ -60,13 +45,12 @@ internal class AnnulleringApiTest : AbstractApiTest() {
                     "kommentar" to "Russekort",
                     "begrunnelser" to listOf("Ingen liker fisk", "En giraff!!")
                 ))
-                authentication(SAKSBEHANDLER_OID)
+                auth(UUID.randomUUID())
             }
         }
-        assertTrue(response.status.isSuccess(), "HTTP response burde returnere en OK verdi, fikk ${response.status}")
-        verify(exactly = 1) {
-            saksbehandlerhåndterer.håndter(capture(slot), any())
-        }
+
+        assertTrue(response.status.isSuccess(), "HTTP response burde returnere en OK-verdi, fikk ${response.status}")
+        verify(exactly = 1) { saksbehandlerhåndterer.håndter(capture(slot), any()) }
         assertEquals("Russekort", slot.captured.kommentar)
         assertEquals(listOf("Ingen liker fisk", "En giraff!!"), slot.captured.begrunnelser)
     }
@@ -74,8 +58,8 @@ internal class AnnulleringApiTest : AbstractApiTest() {
     @Test
     fun annulleringTommeVerdier() {
         val slot = slot<AnnulleringHandling>()
-        val response = runBlocking {
-            client.preparePost("/api/annullering") {
+        val response = apiTesting.spesialistApi { client ->
+            client.post("/api/annullering") {
                 contentType(ContentType.Application.Json)
                 accept(ContentType.Application.Json)
                 setBody(mapOf(
@@ -85,13 +69,12 @@ internal class AnnulleringApiTest : AbstractApiTest() {
                     "fagsystemId" to "en-fagsystem-id",
                     "saksbehandlerIdent" to "Z999999"
                 ))
-                authentication(SAKSBEHANDLER_OID)
-            }.execute()
+                auth(SAKSBEHANDLER_OID)
+            }
         }
-        assertTrue(response.status.isSuccess(), "HTTP response burde returnere en OK verdi, fikk ${response.status}")
-        verify(exactly = 1) {
-            saksbehandlerhåndterer.håndter(capture(slot), any())
-        }
+
+        assertTrue(response.status.isSuccess(), "HTTP response burde returnere en OK-verdi, fikk ${response.status}")
+        verify(exactly = 1) { saksbehandlerhåndterer.håndter(capture(slot), any()) }
         assertEquals(null, slot.captured.kommentar)
         assertEquals(emptyList<String>(), slot.captured.begrunnelser)
     }
---
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/endepunkter/ApiTesting.kt
@@ -0,0 +1,67 @@
+package no.nav.helse.spesialist.api.endepunkter
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.serialization.jackson.JacksonConverter
+import io.ktor.server.auth.authenticate
+import io.ktor.server.routing.Route
+import io.ktor.server.testing.ApplicationTestBuilder
+import io.ktor.server.testing.TestApplicationBuilder
+import io.ktor.server.testing.testApplication
+import java.util.UUID
+import no.nav.helse.spesialist.api.AzureAdAppConfig
+import no.nav.helse.spesialist.api.AzureConfig
+import no.nav.helse.spesialist.api.JwtStub
+import no.nav.helse.spesialist.api.azureAdAppAuthentication
+import no.nav.helse.spesialist.api.objectMapper
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation as ContentNegotiationServer
+
+internal class ApiTesting(
+    private val jwtStub: JwtStub = JwtStub(),
+    private val build: Route.() -> Unit
+) {
+    private val clientId = "client_id"
+    private val issuer = "https://jwt-provider-domain"
+
+    private fun TestApplicationBuilder.setUpApplication() {
+        install(ContentNegotiationServer) { register(ContentType.Application.Json, JacksonConverter(objectMapper)) }
+        val azureConfig = AzureConfig(
+            clientId = clientId,
+            issuer = issuer,
+            jwkProvider = jwtStub.getJwkProviderMock(),
+            tokenEndpoint = "",
+        )
+        application {
+            azureAdAppAuthentication(AzureAdAppConfig(azureConfig))
+        }
+        routing {
+            authenticate("oidc", build = build)
+        }
+    }
+
+    private fun ApplicationTestBuilder.httpClient() = createClient {
+        expectSuccess = false
+        install(ContentNegotiation) {
+            register(ContentType.Application.Json, JacksonConverter(objectMapper))
+        }
+    }
+
+    internal fun <T> spesialistApi(block: suspend ApplicationTestBuilder.(HttpClient) -> T): T {
+        var response: T? = null
+        testApplication {
+            setUpApplication()
+            response = block(httpClient())
+        }
+        return response!!
+    }
+
+    internal fun authentication(oid: UUID, group: String? = null, requestBuilder: HttpRequestBuilder) {
+        requestBuilder.header(
+            "Authorization",
+            "Bearer ${jwtStub.getToken(listOfNotNull(group), oid.toString(), "epostadresse", clientId, issuer)}"
+        )
+    }
+}
