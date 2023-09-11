8e6acce8f999eacb1143df9e7d54c6b70f94fa41
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-09-11 17:20:04 +0200

    🐛 Må sjekke om oppgaver finnes før vi kan gjøre insert

--- spesialist-felles/src/main/resources/db/migration/V413__rett_feilaktige_tildelinger.sql
+++ spesialist-felles/src/main/resources/db/migration/V413__rett_feilaktige_tildelinger.sql
@@ -19,9 +19,11 @@ $$
         IF EXISTS (SELECT 1 FROM saksbehandler WHERE oid IN (
              'ed50903c-3776-4037-ba17-ce4798346ff3', '3cac4e6a-56d0-4c9a-a539-7fe674f25c4e', '2eccf7c2-4015-4e21-9ef4-108464e8944b'
         )) THEN
-            INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('ed50903c-3776-4037-ba17-ce4798346ff3', 3098753, false);
-            INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('3cac4e6a-56d0-4c9a-a539-7fe674f25c4e', 3096236, false);
-            INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('2eccf7c2-4015-4e21-9ef4-108464e8944b', 3097625, false);
+            IF EXISTS (SELECT 1 FROM oppgave WHERE id IN (3098753, 3096236, 3097625)) THEN
+                INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('ed50903c-3776-4037-ba17-ce4798346ff3', 3098753, false);
+                INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('3cac4e6a-56d0-4c9a-a539-7fe674f25c4e', 3096236, false);
+                INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('2eccf7c2-4015-4e21-9ef4-108464e8944b', 3097625, false);
+            END IF;
         END IF;
     END
 $$;
@@ -33,8 +35,10 @@ $$
         IF EXISTS (SELECT 1 FROM saksbehandler WHERE oid IN (
             'fd395747-64b7-4247-a8c6-214d17d6c42f', '40c95f8d-e418-4eb6-9915-12b52c0368d3'
         )) THEN
-            INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('fd395747-64b7-4247-a8c6-214d17d6c42f', 3097880, false);
-            INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('40c95f8d-e418-4eb6-9915-12b52c0368d3', 3098617, false);
+            IF EXISTS (SELECT 1 FROM oppgave WHERE id IN (3097880, 3098617)) THEN
+                INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('fd395747-64b7-4247-a8c6-214d17d6c42f', 3097880, false);
+                INSERT INTO tildeling(saksbehandler_ref, oppgave_id_ref, på_vent) VALUES ('40c95f8d-e418-4eb6-9915-12b52c0368d3', 3098617, false);
+            END IF;
         END IF;
     END
 $$;
