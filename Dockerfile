e38064f69918371e2e894f5c1da8ab130ca35a61
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-09-14 17:18:21 +0200

    👷 Separate docker-lag for jar-filer
    Skal sjekke om det gir litt raskere bygg. Antagelse om at cachede lag
    ikke trenger å bli pushet etter at nytt image er bygget.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM ghcr.io/navikt/baseimages/temurin:17
 
+COPY spesialist-selve/build/deps/*.jar ./
 COPY spesialist-selve/build/libs/*.jar ./
 
 ENV JAVA_OPTS="-XX:MaxRAMPercentage=90"
--- spesialist-selve/build.gradle.kts
+++ spesialist-selve/build.gradle.kts
@@ -27,7 +27,7 @@ tasks {
 
         doLast {
             configurations.runtimeClasspath.get().forEach {
-                val file = File("$buildDir/libs/${it.name}")
+                val file = File("$buildDir/deps/${it.name}")
                 if (!file.exists())
                     it.copyTo(file)
             }
