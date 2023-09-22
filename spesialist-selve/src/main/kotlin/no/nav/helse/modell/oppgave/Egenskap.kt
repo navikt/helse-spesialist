498fa8f80a99053163dfacc0519390511f57366e
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-09-22 12:49:42 +0200

    🚧 Egenskaper har kategorier
    Oppgave er ikke så interessert i å se forskjellen på f.eks. mottaker
    og oppgavetype, mens visningsteknisk ønsker vi å skille på disse.
    Innfører derfor en egen kategori på Egenskap som representerer dette
    skillet.
    
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/oppgave/Egenskap.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/oppgave/Egenskap.kt
@@ -1,25 +1,58 @@
 package no.nav.helse.modell.oppgave
 
-sealed interface Egenskap
+import no.nav.helse.modell.oppgave.Egenskap.Kategori.Mottaker
+import no.nav.helse.modell.oppgave.Egenskap.Kategori.Oppgavetype
+import no.nav.helse.modell.oppgave.Egenskap.Kategori.Ukategorisert
+
+sealed interface Egenskap {
+    val kategori: Kategori
+
+    enum class Kategori {
+        Mottaker,
+        Inntektskilde,
+        Oppgavetype,
+        Ukategorisert
+    }
+}
 
 sealed interface TilgangsstyrtEgenskap : Egenskap
 
-data object RISK_QA: TilgangsstyrtEgenskap
+data object RISK_QA: TilgangsstyrtEgenskap {
+    override val kategori: Egenskap.Kategori = Ukategorisert
+}
 
-data object FORTROLIG_ADRESSE: TilgangsstyrtEgenskap
+data object FORTROLIG_ADRESSE: TilgangsstyrtEgenskap {
+    override val kategori: Egenskap.Kategori = Ukategorisert
+}
 
-data object EGEN_ANSATT: TilgangsstyrtEgenskap
+data object EGEN_ANSATT: TilgangsstyrtEgenskap {
+    override val kategori: Egenskap.Kategori = Ukategorisert
+}
 
-data object REVURDERING: Egenskap
+data object REVURDERING: Egenskap {
+    override val kategori: Egenskap.Kategori = Oppgavetype
+}
 
-data object SØKNAD: Egenskap
+data object SØKNAD: Egenskap {
+    override val kategori: Egenskap.Kategori = Oppgavetype
+}
 
-data object STIKKPRØVE: Egenskap
+data object STIKKPRØVE: Egenskap {
+    override val kategori: Egenskap.Kategori = Ukategorisert
+}
 
-data object UTBETALING_TIL_SYKMELDT: Egenskap
+data object UTBETALING_TIL_SYKMELDT: Egenskap {
+    override val kategori: Egenskap.Kategori = Mottaker
+}
 
-data object DELVIS_REFUSJON: Egenskap
+data object DELVIS_REFUSJON: Egenskap {
+    override val kategori: Egenskap.Kategori = Mottaker
+}
 
-data object UTBETALING_TIL_ARBEIDSGIVER: Egenskap
+data object UTBETALING_TIL_ARBEIDSGIVER: Egenskap {
+    override val kategori: Egenskap.Kategori = Mottaker
+}
 
-data object INGEN_UTBETALING: Egenskap
+data object INGEN_UTBETALING: Egenskap {
+    override val kategori: Egenskap.Kategori = Mottaker
+}
