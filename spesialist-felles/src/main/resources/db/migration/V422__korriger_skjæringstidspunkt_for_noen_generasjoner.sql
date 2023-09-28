9f8155f55263cae62e4ce789dd7665903d3b2572
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-09-28 12:31:03 +0200

    🐛 Sett riktig skjæringstidspunkt for enkelte perioder
    Noen perioder har det ikke blitt sendt ut et sykefraværstilfelle-event
    for, slik at skjæringstidspunktene på disse periodene har ikke blitt
    oppdatert riktig
    
    Co-authored-by: Elias Andreassen Thøgersen <elias.andreassen.thogersen@nav.no>

---
+++ spesialist-felles/src/main/resources/db/migration/V422__korriger_skjæringstidspunkt_for_noen_generasjoner.sql
@@ -0,0 +1,4 @@
+UPDATE selve_vedtaksperiode_generasjon svg SET skjæringstidspunkt = '2023-06-19' WHERE unik_id = '877f9f8c-62ba-439c-b7a8-862f89bf1de2';
+UPDATE selve_vedtaksperiode_generasjon svg SET skjæringstidspunkt = '2023-06-13' WHERE unik_id = 'e7200fb3-b680-40cf-a58b-b116702f4d6d';
+UPDATE selve_vedtaksperiode_generasjon svg SET skjæringstidspunkt = '2023-07-18' WHERE unik_id = 'b68b284f-eac8-4ba3-b69c-229b3b92705e';
+UPDATE selve_vedtaksperiode_generasjon svg SET skjæringstidspunkt = '2022-12-02' WHERE unik_id = '9f98caf6-b41e-4b6d-9e39-bd9406fe3986';
