d54547e5c5f263eebf52bca55dfb63544321085c
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-09-30 18:14:11 +0200

    ♻️ Tester for Totrinnsvurdering

---
+++ spesialist-modell/src/test/kotlin/no/nav/helse/modell/totrinnsvurdering/TotrinnsvurderingTest.kt
@@ -0,0 +1,175 @@
+package no.nav.helse.modell.totrinnsvurdering
+
+import java.time.LocalDateTime
+import java.util.UUID
+import no.nav.helse.modell.OppgaveAlleredeSendtBeslutter
+import no.nav.helse.modell.OppgaveAlleredeSendtIRetur
+import no.nav.helse.modell.OppgaveKreverVurderingAvToSaksbehandlere
+import no.nav.helse.modell.saksbehandler.Saksbehandler
+import no.nav.helse.modell.saksbehandler.Tilgangskontroll
+import no.nav.helse.modell.saksbehandler.handlinger.TilgangskontrollForTestHarIkkeTilgang
+import no.nav.helse.modell.totrinnsvurdering.TotrinnsvurderingTest.TotrinnsvurderingInspektør.Companion.inspektør
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import kotlin.properties.Delegates
+
+internal class TotrinnsvurderingTest {
+
+    @Test
+    fun `send til beslutter`() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        inspektør(totrinnsvurdering) {
+            assertEquals(behandlendeSaksbehandler, saksbehandler)
+            assertEquals(null, beslutter)
+            assertEquals(false, erRetur)
+            assertEquals(null, utbetalingId)
+        }
+    }
+
+    @Test
+    fun `send i retur`() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        val besluttendeSaksbehandler = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        totrinnsvurdering.sendIRetur(1L, besluttendeSaksbehandler)
+        inspektør(totrinnsvurdering) {
+            assertEquals(behandlendeSaksbehandler, saksbehandler)
+            assertEquals(besluttendeSaksbehandler, beslutter)
+            assertEquals(true, erRetur)
+            assertEquals(null, utbetalingId)
+        }
+    }
+
+    @Test
+    fun `send til beslutter etter retur`() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        val besluttendeSaksbehandler = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        totrinnsvurdering.sendIRetur(1L, besluttendeSaksbehandler)
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        inspektør(totrinnsvurdering) {
+            assertEquals(behandlendeSaksbehandler, saksbehandler)
+            assertEquals(besluttendeSaksbehandler, beslutter)
+            assertEquals(false, erRetur)
+            assertEquals(null, utbetalingId)
+        }
+    }
+
+    @Test
+    fun ferdigstill() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        val utbetalingId = UUID.randomUUID()
+        totrinnsvurdering.ferdigstill(utbetalingId)
+        inspektør(totrinnsvurdering) {
+            assertEquals(behandlendeSaksbehandler, saksbehandler)
+            assertEquals(false, erRetur)
+            assertEquals(utbetalingId, this.utbetalingId)
+        }
+    }
+
+    @Test
+    fun `Kan ikke sende til beslutter hvis allerede sendt til beslutter`() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        assertThrows<OppgaveAlleredeSendtBeslutter> {
+            totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        }
+    }
+
+    @Test
+    fun `Beslutter kan ikke sende til samme beslutter`() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        val beslutter = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        totrinnsvurdering.sendIRetur(1L, beslutter)
+        assertThrows<OppgaveKreverVurderingAvToSaksbehandlere> {
+            totrinnsvurdering.sendTilBeslutter(1L, beslutter)
+        }
+    }
+
+    @Test
+    fun `Kan ikke sende i retur hvis allerede sendt i retur`() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        val beslutter = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        totrinnsvurdering.sendIRetur(1L, beslutter)
+        assertThrows<OppgaveAlleredeSendtIRetur> {
+            totrinnsvurdering.sendIRetur(1L, beslutter)
+        }
+    }
+
+    @Test
+    fun `saksbehandler kan ikke sende i retur til samme saksbehandler`() {
+        val totrinnsvurdering = nyTotrinnsvurdering()
+        val behandlendeSaksbehandler = nySaksbehandler()
+        totrinnsvurdering.sendTilBeslutter(1L, behandlendeSaksbehandler)
+        assertThrows<OppgaveKreverVurderingAvToSaksbehandlere> {
+            totrinnsvurdering.sendIRetur(1L, behandlendeSaksbehandler)
+        }
+    }
+
+    private fun nySaksbehandler(
+        oid: UUID = UUID.randomUUID(),
+        tilgangskontroll: Tilgangskontroll = TilgangskontrollForTestHarIkkeTilgang
+    ) = Saksbehandler("epostadresse@nav.no", oid, "navn", "ident", tilgangskontroll)
+
+    private fun nyTotrinnsvurdering(
+        erRetur: Boolean = false,
+        saksbehandler: Saksbehandler? = null,
+        beslutter: Saksbehandler? = null,
+    ) = Totrinnsvurdering(
+        vedtaksperiodeId = UUID.randomUUID(),
+        erRetur = erRetur,
+        saksbehandler = saksbehandler,
+        beslutter = beslutter,
+        utbetalingId = null,
+        opprettet = LocalDateTime.now(),
+        oppdatert = null
+    )
+
+    private class TotrinnsvurderingInspektør private constructor(): TotrinnsvurderingVisitor {
+        lateinit var vedtaksperiodeId: UUID
+        var erRetur by Delegates.notNull<Boolean>()
+        var saksbehandler: Saksbehandler? = null
+        var beslutter: Saksbehandler? = null
+        var utbetalingId: UUID? = null
+        lateinit var opprettet: LocalDateTime
+        var oppdatert: LocalDateTime? = null
+        override fun visitTotrinnsvurdering(
+            vedtaksperiodeId: UUID,
+            erRetur: Boolean,
+            saksbehandler: Saksbehandler?,
+            beslutter: Saksbehandler?,
+            utbetalingId: UUID?,
+            opprettet: LocalDateTime,
+            oppdatert: LocalDateTime?
+        ) {
+            this.vedtaksperiodeId = vedtaksperiodeId
+            this.erRetur = erRetur
+            this.saksbehandler = saksbehandler
+            this.beslutter = beslutter
+            this.utbetalingId = utbetalingId
+            this.opprettet = opprettet
+            this.oppdatert = oppdatert
+        }
+
+        companion object {
+            fun inspektør(totrinnsvurdering: Totrinnsvurdering, block: TotrinnsvurderingInspektør.() -> Unit) {
+                val inspektør = TotrinnsvurderingInspektør()
+                totrinnsvurdering.accept(inspektør)
+                block(inspektør)
+            }
+        }
+
+    }
+}
