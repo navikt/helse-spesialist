504313dede5b9bf2e8a5ed198247b013b511a1d6
Elias Andreassen Thøgersen <elias3004@msn.com>
2023-10-06 10:21:32 +0200

    ✅ Legger til tester på ny oppgaver query
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/query/OppgaverQueryTest.kt
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/query/OppgaverQueryTest.kt
@@ -1,7 +1,15 @@
 package no.nav.helse.spesialist.api.graphql.query
 
+import io.mockk.every
+import io.mockk.verify
 import java.time.LocalDateTime
+import java.util.UUID
 import no.nav.helse.spesialist.api.AbstractGraphQLApiTest
+import no.nav.helse.spesialist.api.graphql.schema.AntallArbeidsforhold
+import no.nav.helse.spesialist.api.graphql.schema.Mottaker
+import no.nav.helse.spesialist.api.graphql.schema.OppgaveTilBehandling
+import no.nav.helse.spesialist.api.graphql.schema.Periodetype
+import no.nav.helse.spesialist.api.graphql.schema.Personnavn
 import no.nav.helse.spesialist.api.oppgave.Oppgavestatus
 import no.nav.helse.spesialist.api.oppgave.Oppgavetype
 import no.nav.helse.spesialist.api.person.Adressebeskyttelse
@@ -9,6 +17,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
+import no.nav.helse.spesialist.api.graphql.schema.Oppgavetype as OppgavetypeForApi
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class OppgaverQueryTest : AbstractGraphQLApiTest() {
@@ -92,4 +101,47 @@ internal class OppgaverQueryTest : AbstractGraphQLApiTest() {
 
         assertEquals("Jan Banan", body["data"]["behandledeOppgaver"].first()["ferdigstiltAv"].asText())
     }
+
+    @Test
+    fun `oppgaver query uten parametere returnerer oppgave`() {
+        opprettVedtaksperiode(opprettPerson(), opprettArbeidsgiver())
+        every { oppgavehåndterer.oppgaver(any(), any(), any()) } returns listOf(oppgaveTilBehandling())
+
+        val body = runQuery("""{ oppgaver { id } }""")
+        val antallOppgaver = body["data"]["oppgaver"].size()
+
+        verify(exactly = 1) { oppgavehåndterer.oppgaver(any(), 0, Int.MAX_VALUE) }
+        assertEquals(1, antallOppgaver)
+    }
+
+    @Test
+    fun `oppgaver query med parametere returnerer oppgave`() {
+        opprettVedtaksperiode(opprettPerson(), opprettArbeidsgiver())
+        every { oppgavehåndterer.oppgaver(any(), any(), any()) } returns listOf(oppgaveTilBehandling())
+
+        val body = runQuery("""{ oppgaver(startIndex: 2, pageSize: 5) { id } }""")
+        val antallOppgaver = body["data"]["oppgaver"].size()
+
+        verify(exactly = 1) { oppgavehåndterer.oppgaver(any(), 2, 5) }
+        assertEquals(1, antallOppgaver)
+    }
+
+    private fun oppgaveTilBehandling() = OppgaveTilBehandling(
+        id = UUID.randomUUID().toString(),
+        opprettet = LocalDateTime.now().toString(),
+        opprinneligSoknadsdato = LocalDateTime.now().toString(),
+        vedtaksperiodeId = UUID.randomUUID().toString(),
+        navn = Personnavn(
+            fornavn = "Aage",
+            etternavn = "Kurt",
+            mellomnavn = null,
+        ),
+        aktorId = "1017011111111",
+        tildeling = null,
+        egenskaper = emptyList(),
+        periodetype = Periodetype.FORSTEGANGSBEHANDLING,
+        oppgavetype = OppgavetypeForApi.SOKNAD,
+        mottaker = Mottaker.SYKMELDT,
+        antallArbeidsforhold = AntallArbeidsforhold.ET_ARBEIDSFORHOLD
+    )
 }
