3802bafab11cd50ad342514fc0dbbef4fc049238
Trine Linderud <trinelinderuds@gmail.com>
2023-12-08 09:37:23 +0100

    🗃️ Migrerer inn egenskap på vent i oppgave
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>

---
+++ spesialist-felles/src/main/resources/db/migration/V445__migrerer_inn_pa_vent_egenskap.sql
@@ -0,0 +1,9 @@
+update pa_vent
+set egenskaper = array_append(egenskaper, 'PÅ_VENT')
+where id in (select o.id
+             from oppgave o
+                      inner join vedtak v on o.vedtak_ref = v.id
+                      inner join pa_vent pv on v.vedtaksperiode_id = pv.vedtaksperiode_id
+             )
+  and status = 'AvventerSaksbehandler'
+  and not ('PÅ_VENT' = ANY (egenskaper));
