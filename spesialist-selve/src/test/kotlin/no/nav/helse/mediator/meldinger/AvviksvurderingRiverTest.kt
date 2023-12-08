c6b39b962505344896250d2e7edf62097ea10026
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2023-12-08 10:52:32 +0100

    ✅ avviksvurderingRiver tester
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- spesialist-selve/src/test/kotlin/no/nav/helse/mediator/meldinger/AvviksvurderingRiverTest.kt
+++ spesialist-selve/src/test/kotlin/no/nav/helse/mediator/meldinger/AvviksvurderingRiverTest.kt
@@ -1,9 +1,12 @@
 package no.nav.helse.mediator.meldinger
 
 import io.mockk.mockk
+import io.mockk.verify
 import no.nav.helse.mediator.HendelseMediator
+import no.nav.helse.modell.avviksvurdering.AvviksvurderingDto
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class AvviksvurderingRiverTest {
@@ -16,9 +19,21 @@ class AvviksvurderingRiverTest {
         AvviksvurderingRiver(testRapid, mediator)
     }
 
+    @BeforeEach
+    fun beforeEach() {
+       testRapid.reset()
+    }
+
     @Test
-    fun foo() {
+    fun `leser avviksvurdering fra kafka`() {
         testRapid.sendTestMessage(event())
+        verify(exactly = 1) { mediator.håndter(any<AvviksvurderingDto>()) }
+    }
+
+    @Test
+    fun `leser ikke inn avviksvurdering uten sammenligningsgrunnlag`() {
+        testRapid.sendTestMessage(eventUtenSammenligningsgrunnlag())
+        verify(exactly = 0) { mediator.håndter(any<AvviksvurderingDto>()) }
     }
 
     @Language("JSON")
@@ -60,6 +75,33 @@ class AvviksvurderingRiverTest {
                   "beløp": 10000.0
                 }
               ]
+            }
+          ]
+        },
+        "avviksprosent": 25.0
+        }
+    }"""
+
+    @Language("JSON")
+    private fun eventUtenSammenligningsgrunnlag() = """
+    {
+      "@event_name": "avviksvurdering",
+      "fødselsnummer": "12345678910",
+      "aktørId": "11111100000",
+      "skjæringstidspunkt": "2018-01-01",
+      "avviksvurdering": {
+        "id": "cc3af2c3-fa9e-4d84-a57e-a7972226cdae",
+        "opprettet": "2018-01-01T00:00:00.000",
+        "beregningsgrunnlag": {
+          "totalbeløp": 550000.0,
+          "omregnedeÅrsinntekter": [
+            {
+              "arbeidsgiverreferanse": "000000000",
+              "beløp": 250000.0
+            },
+            {
+              "arbeidsgiverreferanse": "000000000",
+              "beløp": 300000.0
             }
           ]
         },
