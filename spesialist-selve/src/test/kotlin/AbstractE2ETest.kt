bee9d397df20c9a649b3b6ddf6d9cc42da6a0a1f
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-02-23 15:47:10 +0100

    💬 Litt "friskere" orgnavn i testene

--- spesialist-selve/src/test/kotlin/AbstractE2ETest.kt
+++ spesialist-selve/src/test/kotlin/AbstractE2ETest.kt
@@ -78,13 +78,13 @@ fun lagAktørId() = nextLong(from = 1_000_000_000_000, until = 1_000_099_999_999
 fun lagOrganisasjonsnummer() = nextLong(from = 800_000_000, until = 999_999_999).toString()
 
 private val fornavnListe = listOf("Måteholden", "Dypsindig", "Ultrafiolett", "Urettferdig", "Berikende", "Upresis", "Stridlynt", "Rund", "Internasjonal")
-private val mellomnavnListe = listOf("Lysende", "Spennende", "Tidløs", "Hjertelig", "Storslått", "Sjarmerende", "Uforutsigbar", "Behagelig", "Robust", "Sofistikert", null, null, null, null, null, null, null, null, null, null)
+private val mellomnavnListe = listOf("Lysende", "Spennende", "Tidløs", "Hjertelig", "Storslått", "Sjarmerende", "Uforutsigbar", "Behagelig", "Robust", "Sofistikert") + List(10) { null }
 private val etternavnListe = listOf("Diode", "Flom", "Damesykkel", "Undulat", "Bakgrunn", "Genser", "Fornøyelse", "Campingvogn", "Bakkeklaring")
 
-private val organisasjonsnavnAdjektiver = listOf("Sjokkerende", "Pengeløs", "Realistisk", "Snill", "Hårreisende", "Fargerik", "Rask", "Melankolsk", "Gjennomsiktig", "Kreativ", "Høylytt", "Tung", "Glatt", "Vennlig", "Frodig")
-private val organisasjonsnavnSubstantiver = listOf("Frisør", "Torpedo", "Kontorbygg", "Sparebank", "Elektriker", "Fjell", "Datamaskin", "Bibliotek", "Drøm", "Elefant", "Restaurant", "Teleskop", "Mysterium", "Festival", "Melodi")
+private val organisasjonsnavnDel1 = listOf("NEPE", "KLOVNE", "BOBLEBAD-", "DUSTE", "SKIHOPP", "SMÅBARN", "SPANIA")
+private val organisasjonsnavnDel2 = listOf("AVDELINGEN", "SENTERET", "FORUM", "KLUBBEN", "SNEKKERIET")
 
-fun lagOrganisasjonsnavn() = listOf(organisasjonsnavnAdjektiver.random(), organisasjonsnavnSubstantiver.random()).joinToString { " " }
+fun lagOrganisasjonsnavn() = organisasjonsnavnDel1.random() + organisasjonsnavnDel2.random()
 
 fun fødselsdato(): LocalDate {
     val end = LocalDate.now().minusYears(18)
