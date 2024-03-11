f5d2b1f0436db14361e49490ee9f6cb8afc345f3
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-03-11 18:54:21 +0100

    🐛 Audit-logg oppslag på person igjen
    Dette falt ut da vi byttet fra REST til GraphQL for henting av person
    (muligvis på et senere tidspunkt, hvis vi først fjernet headeren
    'fodselsnummer' på et senere tidspunkt).

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/LoggingGraphQLRequestHandler.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/LoggingGraphQLRequestHandler.kt
@@ -6,23 +6,32 @@ import com.expediagroup.graphql.server.types.GraphQLServerRequest
 import com.expediagroup.graphql.server.types.GraphQLServerResponse
 import graphql.GraphQL
 import graphql.GraphQLContext
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class LoggingGraphQLRequestHandler(graphQL: GraphQL) : GraphQLRequestHandler(graphQL) {
 
-    private val sikkerLogg: Logger = LoggerFactory.getLogger("tjenestekall")
+    private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
+    private val auditLog = LoggerFactory.getLogger("auditLogger")
 
     override suspend fun executeRequest(
         graphQLRequest: GraphQLServerRequest,
-        graphQLContext: GraphQLContext
+        graphQLContext: GraphQLContext,
     ): GraphQLServerResponse {
         if (graphQLRequest is GraphQLRequest) {
-            graphQLRequest.operationName.let {
-                if (it != null) sikkerLogg.trace("GraphQL-kall mottatt, operationName: $it")
-                else sikkerLogg.warn("GraphQL-kall uten navngitt query, bør fikses i kallende kode. Requesten:\n${graphQLRequest.query}")
+            graphQLRequest.operationName.also { operationName ->
+                if (operationName != null) {
+                    sikkerLogg.trace("GraphQL-kall mottatt, operationName: $operationName")
+                    graphQLRequest.variables?.get("fnr")?.let { auditLog(graphQLContext, operationName, it as String) }
+                } else if (!graphQLRequest.query.contains("query IntrospectionQuery"))
+                    sikkerLogg.warn("GraphQL-kall uten navngitt query, bør fikses i kallende kode. Requesten:\n${graphQLRequest.query}")
             }
         }
         return super.executeRequest(graphQLRequest, graphQLContext)
+    }
+
+    private fun auditLog(graphQLContext: GraphQLContext, operationName: String, personId: String) {
+        val saksbehandlerIdent = graphQLContext.get<String>(ContextValues.SAKSBEHANDLER_IDENT.key)
+        auditLog.info("end=${System.currentTimeMillis()} suid=$saksbehandlerIdent duid=$personId operation=$operationName")
+        sikkerLogg.debug("audit-logget noe 👍")
     }
 }
