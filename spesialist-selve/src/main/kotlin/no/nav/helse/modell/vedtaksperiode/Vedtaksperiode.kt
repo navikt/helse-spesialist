9441273ad647819049769236430876d58c2adf4e
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2024-04-02 10:41:56 +0200

    Revert "Revert "🐛 Må sette spleisBehandlingId når vi oppretter ny vedtaksperiode""
    This reverts commit 1ab70b0c14d4e530c9c9ad6fe1edfe1f1b9a77cd.

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/Vedtaksperiode.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/Vedtaksperiode.kt
@@ -101,6 +101,7 @@ internal class Vedtaksperiode private constructor(
                     Generasjon(
                         id = UUID.randomUUID(),
                         vedtaksperiodeId = spleisBehandling.vedtaksperiodeId,
+                        spleisBehandlingId = spleisBehandling.spleisBehandlingId,
                         fom = spleisBehandling.fom,
                         tom = spleisBehandling.tom,
                         skjæringstidspunkt = spleisBehandling.fom // Spleis sender oss ikke skjæringstidspunkt på dette tidspunktet
