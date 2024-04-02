eaa7741750d9c56fc2bdc1a24b1d816914a0fef8
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-04-02 15:19:30 +0200

    🚑️ Ignorer meldinger som ikke lar seg behandle
    Disse har gått skeis i spinnvill og må kastes ut av en saksbehandler.
    
    Co-authored-by: Jonas Hasle <jonas.hasle@nav.no>
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
    Co-authored-by: Elias Andreassen Thøgersen <elias.andreassen.thogersen@nav.no>
    Co-authored-by: Camilla Bårnes Roark <camilla.barnes.roark@nav.no>
    Co-authored-by: Trine Linderud <trine.linderud@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/AvsluttetMedVedtakRiver.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/AvsluttetMedVedtakRiver.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.mediator.meldinger
 
+import net.logstash.logback.argument.StructuredArguments
 import no.nav.helse.db.AvviksvurderingDao
 import no.nav.helse.mediator.MeldingMediator
 import no.nav.helse.mediator.meldinger.hendelser.AvsluttetMedVedtakMessage
@@ -16,7 +17,7 @@ internal class AvsluttetMedVedtakRiver(
     rapidsConnection: RapidsConnection,
     private val mediator: MeldingMediator,
     private val avviksvurderingDao: AvviksvurderingDao,
-    private val generasjonDao: GenerasjonDao
+    private val generasjonDao: GenerasjonDao,
 ) : River.PacketListener {
 
     init {
@@ -26,11 +27,18 @@ internal class AvsluttetMedVedtakRiver(
                 it.requireKey("@id", "fødselsnummer", "aktørId", "vedtaksperiodeId", "organisasjonsnummer")
                 it.requireKey("fom", "tom", "skjæringstidspunkt")
                 it.requireArray("hendelser")
-                it.requireKey("sykepengegrunnlag", "grunnlagForSykepengegrunnlag", "grunnlagForSykepengegrunnlagPerArbeidsgiver")
+                it.requireKey(
+                    "sykepengegrunnlag",
+                    "grunnlagForSykepengegrunnlag",
+                    "grunnlagForSykepengegrunnlagPerArbeidsgiver"
+                )
                 it.requireKey("begrensning", "inntekt", "vedtakFattetTidspunkt", "tags")
                 it.requireKey("utbetalingId", "behandlingId")
 
-                it.requireAny("sykepengegrunnlagsfakta.fastsatt", listOf("EtterHovedregel", "IInfotrygd", "EtterSkjønn"))
+                it.requireAny(
+                    "sykepengegrunnlagsfakta.fastsatt",
+                    listOf("EtterHovedregel", "IInfotrygd", "EtterSkjønn")
+                )
                 it.requireKey("sykepengegrunnlagsfakta.omregnetÅrsinntekt")
                 it.require("sykepengegrunnlagsfakta.fastsatt") { fastsattNode ->
                     when (fastsattNode.asText()) {
@@ -41,6 +49,7 @@ internal class AvsluttetMedVedtakRiver(
                                 "sykepengegrunnlagsfakta.arbeidsgivere",
                             )
                         }
+
                         "EtterSkjønn" -> {
                             it.requireKey(
                                 "sykepengegrunnlagsfakta.6G",
@@ -49,6 +58,7 @@ internal class AvsluttetMedVedtakRiver(
                                 "sykepengegrunnlagsfakta.skjønnsfastsatt",
                             )
                         }
+
                         else -> {}
                     }
                 }
@@ -62,6 +72,22 @@ internal class AvsluttetMedVedtakRiver(
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         sikkerlogg.info("Mottok melding avsluttet_med_vedtak:\n${packet.toJson()}")
+        if (packet["@id"].asText() in listOf(
+                "7aa38978-beb6-4c92-8c4d-97aef476dc1f",
+                "74e9b587-ba93-44e7-a0b7-24bacc313a94",
+                "21634a26-1500-4bcd-9cb9-02bdc8f38f50",
+                "cfd2fcea-d61c-4a96-a6fa-434112214291",
+            )
+        ) {
+            sikkerlogg.info(
+                "Disse må følges opp manuelt, fordi spesialist ikke videresender vedtak_fattet",
+                StructuredArguments.kv("fødselsnummer", packet["fødselsnummer"]),
+                StructuredArguments.kv("vedtaksperiodeId", packet["vedtaksperiodeId"]),
+                StructuredArguments.kv("fom", packet["fom"]),
+                StructuredArguments.kv("tom", packet["tom"]),
+            )
+            return
+        }
         mediator.håndter(AvsluttetMedVedtakMessage(packet, avviksvurderingDao, generasjonDao), context)
     }
 
