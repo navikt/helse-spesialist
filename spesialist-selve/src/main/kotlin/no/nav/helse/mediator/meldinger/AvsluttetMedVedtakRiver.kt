e93863d32ffdea9b4552d34066657708656c6868
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-04-02 16:07:58 +0200

    🔥 Fjern midlertidig feilhåndteringskode

--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/AvsluttetMedVedtakRiver.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/AvsluttetMedVedtakRiver.kt
@@ -1,6 +1,5 @@
 package no.nav.helse.mediator.meldinger
 
-import net.logstash.logback.argument.StructuredArguments
 import no.nav.helse.db.AvviksvurderingDao
 import no.nav.helse.mediator.MeldingMediator
 import no.nav.helse.mediator.meldinger.hendelser.AvsluttetMedVedtakMessage
@@ -72,16 +71,6 @@ internal class AvsluttetMedVedtakRiver(
 
     override fun onPacket(packet: JsonMessage, context: MessageContext) {
         sikkerlogg.info("Mottok melding avsluttet_med_vedtak:\n${packet.toJson()}")
-        if (packet["@id"].asText() in listOf("fafefe36-3079-4e88-a17d-b884077df062")) {
-            sikkerlogg.info(
-                "Disse må følges opp manuelt, fordi spesialist ikke videresender vedtak_fattet",
-                StructuredArguments.kv("fødselsnummer", packet["fødselsnummer"]),
-                StructuredArguments.kv("vedtaksperiodeId", packet["vedtaksperiodeId"]),
-                StructuredArguments.kv("fom", packet["fom"]),
-                StructuredArguments.kv("tom", packet["tom"]),
-            )
-            return
-        }
         mediator.håndter(AvsluttetMedVedtakMessage(packet, avviksvurderingDao, generasjonDao), context)
     }
 
