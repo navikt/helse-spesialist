80c10387007096e605ce7005229e9ab1d7663c3b
Øyvind Hagen <oyvind.hagen@nav.no>
2024-04-23 11:13:19 +0200

    🗃️ Mer fiks for feil tilstand og/eller manglende spleisBehandlingId ...
    ... for generasjoner.

---
+++ spesialist-felles/src/main/resources/db/migration/V484__rette_flere_manglende_generasjon_og_feil_tilstander_del3.sql
@@ -0,0 +1,150 @@
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak' where id=15230330;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak' where id=17662871;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak' where id=17890932;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='5f51e099-d7f7-4dee-a752-04eb9b78f32d' where id=30953161;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak' where id=11273436;
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='fe5ecfd1-ef75-43fe-bd78-d98220d53980' where id=11381928;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('d24adfe9-425f-49be-933a-fb35f2ab9522',
+        'c2a1d5ab-9f84-4d48-8f9c-838c094807d3',
+        'b7ed8210-ddb0-4573-b980-d57e3985437f',
+        now(),
+        '2024-02-09',
+        '2024-02-20',
+        '2024-02-05',
+        'AvsluttetUtenVedtak',
+        'b8e518b9-6474-413e-8900-5825698c7e78',
+        array []::char[]);
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='d6ea1e04-fe6e-495c-b817-b744cf79f03e' where id=10523190;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='03a1bb76-9fe4-43a1-889d-f056f8d1c04a' where id=10851162;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='21a33ba2-2dd7-4983-90bb-81c4e04b5927' where id=11377721;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='385b61f6-5b87-4729-a195-c7ca6041b455' where id=27160595;
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='ba5d17e1-c98d-40cb-90a4-2c871451da2f' where id=11319399;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='82b59027-8935-4968-8652-ff899f7e6932' where id=11338013;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak' where id=11712757;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='2507f69d-8670-47ae-b358-5b6d1a50c031' where id=14336896;
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='edafe69d-38de-476e-9716-d2bcf68083a2' where id=8962369;
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='1cb5010a-af8b-48d7-9447-49efc0562023', skjæringstidspunkt='2023-09-22', fom='2023-09-22' where id=9770435;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='7bd17dfe-87af-4443-8b2a-afadd415e4ae' where id=11214251;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('de8d1412-5c3c-4427-a0bc-eb7b50187eaa',
+        '1f3d00b3-7226-4653-9e4d-1ca3cefdc5fb',
+        '0596613e-7462-4e55-9679-82222d8ac34d',
+        now(),
+        '2023-09-15',
+        '2023-10-08',
+        '2023-09-22',
+        'AvsluttetUtenVedtak',
+        '1da265af-72fa-4b64-8103-59c2f7473ffe',
+        array []::char[]);
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('9f12510d-a3e6-465a-b545-0b488414e67f',
+        '1f3d00b3-7226-4653-9e4d-1ca3cefdc5fb',
+        '5284d8ce-6bbc-4d63-ab85-f9c635d6cce6',
+        now(),
+        '2023-09-15',
+        '2023-10-08',
+        '2023-09-22',
+        'AvsluttetUtenVedtak',
+        '2ae0de34-bcad-4881-ad43-498f17ee56d8',
+        array []::char[]);
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='b1030849-3bc1-4ce6-b2bf-2435639264a0' where id=5712655;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='dafeddfd-e495-49f5-b26d-5837f0b3fc91' where id=5930782;
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='5688d392-a5f1-4cc8-bb03-42ace7e3cde0' where id=7235079;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak' where id=8089940;
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='c00c22c2-e697-4cb9-b436-b9bf683e537c' where id=8359576;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='92a8fc14-2d15-457b-8f89-67e001e739d7' where id=8715679;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('91089857-2474-48f7-b773-4f862956ff4f',
+        '62c057a5-e586-4e0e-8a68-8e1e944e2025',
+        '43ce4bf3-d104-4af4-ba6a-5833f9a100cc',
+        now(),
+        '2023-08-07',
+        '2023-08-30',
+        '2023-08-21',
+        'AvsluttetUtenVedtak',
+        'aaa38f55-a46b-4841-91a5-0328ae7b5c8f',
+        array []::char[]);
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='80a7ffb5-3bbd-4b04-96c5-8767613847bd' where id=8359602;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='ab0a6037-f15d-41d6-a76c-bb0e6ff8533b' where id=8715680;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('b257e968-17e5-4634-b909-a5e4b37774b0',
+        'bf239438-1aa8-4a17-9624-63631b25221f',
+        '8865c8be-3e55-4cfa-a376-67a3df1c68e5',
+        now(),
+        '2023-08-24',
+        '2023-08-30',
+        '2023-08-21',
+        'AvsluttetUtenVedtak',
+        'c369aefc-4a6c-46b3-bdb6-c5e4f4aef9db',
+        array []::char[]);
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='32ed2ccf-f4de-4f99-9b19-730de997acbd' where id=8359611;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='e5870203-c87a-4ece-a30c-4d1bff8f7b23' where id=8715681;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('ec2c09f0-4d2a-4a78-b7bb-6b681ae610d0',
+        '57508e6b-a851-4fdd-b82e-0e64d836fd49',
+        '06e6427c-fd32-46c0-8e89-8f5a4a6a790d',
+        now(),
+        '2023-08-31',
+        '2023-08-31',
+        '2023-08-21',
+        'AvsluttetUtenVedtak',
+        'afe8adf9-6c16-487a-aa0a-bba86ed3c951',
+        array []::char[]);
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='06c01352-6b65-4f36-b5ef-5efdef486ba4' where id=8359616;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='d18962e7-ccb5-4cf4-a411-9ef3757e60de' where id=8715682;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('06422a9f-c323-4f0c-8689-64423abeca85',
+        '4f27d8b0-7690-4af9-bc0c-7d6d4470fa0a',
+        'aef2cfa6-6170-4027-8b44-f24dd510bd22',
+        now(),
+        '2023-08-31',
+        '2023-08-31',
+        '2023-08-21',
+        'AvsluttetUtenVedtak',
+        'dcff8ddb-73f4-4fc3-a113-9311e68b4be9',
+        array []::char[]);
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='850e9ee1-737f-46ec-94ff-2d0423628d3b' where id=8419095;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='23c1ef66-acc2-45b7-acb1-d5015cd9a117' where id=8715683;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('551018b5-3cc8-4ecb-86df-58f4c95494ae',
+        'b61d1fad-9e04-44b3-9a2a-3e000a8d84c2',
+        '21b82b41-05fe-4696-a44f-24867e31a79c',
+        now(),
+        '2023-09-01',
+        '2023-09-03',
+        '2023-08-21',
+        'AvsluttetUtenVedtak',
+        '57574e79-d5ff-4661-93d3-fc323ba46734',
+        array []::char[]);
+update selve_vedtaksperiode_generasjon set spleis_behandling_id='ca9c1e36-420a-4a04-ad38-6e23e13187ce' where id=8419123;
+update selve_vedtaksperiode_generasjon set tilstand='AvsluttetUtenVedtak', spleis_behandling_id='ba7e1174-e0af-4742-b79c-05ad9dc7ff9e' where id=8715684;
+insert into selve_vedtaksperiode_generasjon (unik_id, vedtaksperiode_id, opprettet_av_hendelse,
+                                             tilstand_endret_tidspunkt, fom, tom, skjæringstidspunkt, tilstand,
+                                             spleis_behandling_id, tags)
+values ('302e4f0f-6ac3-44b6-8495-99d0bd36b903',
+        '1cff8055-4882-44ff-846a-101ab29cd43c',
+        'c84926bb-a1f0-4b8b-bd9f-4471fae4a30c',
+        now(),
+        '2023-09-01',
+        '2023-09-03',
+        '2023-08-21',
+        'AvsluttetUtenVedtak',
+        '7772afd0-375c-4025-b375-b418313a33ad',
+        array []::char[]);
