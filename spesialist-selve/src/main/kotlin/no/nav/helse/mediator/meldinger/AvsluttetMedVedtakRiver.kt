5034a4fbade764418a5543dcd0b647a9bda53099
Christian Bülow Skovborg <christianskovborg@gmail.com>
2024-05-14 11:25:38 +0200

    🚑️ Ignorer et bestemt avsluttet_med_vedtak
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
    Co-authored-by: Øyvind Hagen <oyvind.hagen@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/AvsluttetMedVedtakRiver.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/AvsluttetMedVedtakRiver.kt
@@ -76,6 +76,7 @@ internal class AvsluttetMedVedtakRiver(
         context: MessageContext,
     ) {
         sikkerlogg.info("Mottok melding avsluttet_med_vedtak:\n${packet.toJson()}")
+        if (packet["@id"].asText() == "dc162abd-7085-44d4-8334-a359763aa9ac") return
         mediator.håndter(AvsluttetMedVedtakMessage(packet, avviksvurderingDao, generasjonDao, avslagDao), context)
     }
 
