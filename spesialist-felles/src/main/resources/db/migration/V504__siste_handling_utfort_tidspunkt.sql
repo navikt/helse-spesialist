8b4c8013a7bc0f5334f628851922badeb3c0dd49
Christian Bülow Skovborg <christianskovborg@gmail.com>
2024-06-11 09:48:21 +0200

    ♻️ Lagre tidspunkt for siste handling ved lagring av saksbehandler

---
+++ spesialist-felles/src/main/resources/db/migration/V504__siste_handling_utfort_tidspunkt.sql
@@ -0,0 +1,1 @@
+ALTER TABLE saksbehandler ADD COLUMN siste_handling_utført_tidspunkt TIMESTAMP;
--- spesialist-selve/src/main/kotlin/no/nav/helse/db/SaksbehandlerDao.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/db/SaksbehandlerDao.kt
@@ -1,6 +1,7 @@
 package no.nav.helse.db
 
 import no.nav.helse.HelseDao
+import java.time.LocalDateTime
 import java.util.UUID
 import javax.sql.DataSource
 
@@ -16,13 +17,13 @@ class SaksbehandlerDao(dataSource: DataSource) : HelseDao(dataSource), Saksbehan
         ident: String,
     ) = asSQL(
         """ 
-            INSERT INTO saksbehandler(oid, navn, epost, ident) VALUES (:oid, :navn, :epost, :ident)
+            INSERT INTO saksbehandler(oid, navn, epost, ident, siste_handling_utført_tidspunkt) VALUES (:oid, :navn, :epost, :ident, :siste_handling_utfort_tidspunkt)
             ON CONFLICT (oid)
-                DO UPDATE SET navn = :navn, epost = :epost, ident = :ident
+                DO UPDATE SET navn = :navn, epost = :epost, ident = :ident, siste_handling_utført_tidspunkt = :siste_handling_utfort_tidspunkt
                 WHERE (saksbehandler.navn, saksbehandler.epost, saksbehandler.ident) IS DISTINCT FROM
                     (excluded.navn, excluded.epost, excluded.ident)
             """,
-        mapOf("oid" to oid, "navn" to navn, "epost" to epost, "ident" to ident),
+        mapOf("oid" to oid, "navn" to navn, "epost" to epost, "ident" to ident, "siste_handling_utfort_tidspunkt" to LocalDateTime.now()),
     ).update()
 
     override fun finnSaksbehandler(oid: UUID) =
