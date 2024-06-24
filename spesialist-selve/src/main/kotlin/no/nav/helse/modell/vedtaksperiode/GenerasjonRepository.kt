41e4bddc4b73ebc08481bef1064639ce65a9b0f8
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-06-24 12:00:34 +0200

    🔊 Logg hva som ble endret på lagrede generasjoner
    Så vi kan få innblikk i hvordan dette går for seg ute i virkeligheten,
    og skaffe innsikt til tuning av databaseoperasjoner.
    
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/GenerasjonRepository.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/GenerasjonRepository.kt
@@ -5,6 +5,8 @@ import kotliquery.sessionOf
 import no.nav.helse.mediator.builders.GenerasjonBuilder
 import no.nav.helse.modell.VedtakDao
 import no.nav.helse.modell.person.vedtaksperiode.IVedtaksperiodeObserver
+import no.nav.helse.modell.person.vedtaksperiode.VarselDto
+import org.slf4j.LoggerFactory
 import java.time.LocalDate
 import java.util.UUID
 import javax.sql.DataSource
@@ -12,6 +14,9 @@ import javax.sql.DataSource
 internal class GenerasjonRepository(private val dataSource: DataSource) : IVedtaksperiodeObserver {
     private val dao = GenerasjonDao(dataSource)
     private val vedtakDao = VedtakDao(dataSource)
+    private var hentedeGenerasjoner: List<GenerasjonDto> = emptyList()
+
+    private val sikkerLogger = LoggerFactory.getLogger("tjenestekall")
 
     internal fun brukVedtaksperiode(
         fødselsnummer: String,
@@ -53,7 +58,7 @@ internal class GenerasjonRepository(private val dataSource: DataSource) : IVedta
 
     private fun TransactionalSession.finnGenerasjoner(vedtaksperiodeId: UUID): List<GenerasjonDto> {
         return with(dao) {
-            finnGenerasjoner(vedtaksperiodeId)
+            finnGenerasjoner(vedtaksperiodeId).also { hentedeGenerasjoner = it }
         }
     }
 
@@ -65,6 +70,8 @@ internal class GenerasjonRepository(private val dataSource: DataSource) : IVedta
             lagreVedtaksperiode(fødselsnummer, vedtaksperiode)
         }
         with(dao) {
+            loggDiffMellomHentetOgSkalLagres(vedtaksperiode)
+            hentedeGenerasjoner = emptyList()
             vedtaksperiode.generasjoner.forEach { generasjonDto ->
                 lagreGenerasjon(generasjonDto)
             }
@@ -72,6 +79,56 @@ internal class GenerasjonRepository(private val dataSource: DataSource) : IVedta
         with(vedtakDao) {
             lagreOpprinneligSøknadsdato(vedtaksperiode.vedtaksperiodeId)
         }
+    }
+
+    private fun loggDiffMellomHentetOgSkalLagres(vedtaksperiode: VedtaksperiodeDto) {
+        val antallHentet = hentedeGenerasjoner.size
+        if (antallHentet == 0) return
+        val generasjonerForLagring = vedtaksperiode.generasjoner
+        val antallNå = generasjonerForLagring.size
+        val builder =
+            StringBuilder().appendLine(
+                "Hentet $antallHentet generasjon(er) for ${vedtaksperiode.vedtaksperiodeId}, skal lagre $antallNå.",
+            )
+
+        if (antallHentet == antallNå) {
+            val nyesteHentet = hentedeGenerasjoner.last()
+            val nyesteSkalLagres = generasjonerForLagring.last()
+            val nyesteErEendret = nyesteHentet != nyesteSkalLagres
+            builder.appendLine("Ingen generasjoner ble lagt til. Nyeste versjon ble endret: $nyesteErEendret")
+            if (nyesteErEendret) {
+                builder.appendLine()
+            }
+            builder.diffMellomToGenerasjoner(nyesteHentet, nyesteSkalLagres)
+        }
+        val hentedeBortsettFraSiste = hentedeGenerasjoner.dropLast(1)
+        if (hentedeBortsettFraSiste.isNotEmpty()) {
+            val historiskeForLagring = generasjonerForLagring.take(hentedeBortsettFraSiste.size)
+            val historiskeErUlike = hentedeBortsettFraSiste != historiskeForLagring
+            if (historiskeErUlike) {
+                builder.appendLine("Historiske generasjoner ble endret")
+                builder.appendLine("         før - etter:")
+                hentedeGenerasjoner.zip(historiskeForLagring).forEach { (hentet, skalLagres) ->
+                    builder.diffMellomToGenerasjoner(hentet, skalLagres)
+                }
+            } else {
+                builder.appendLine("Historiske generasjoner ble ikke endret")
+            }
+        }
+        sikkerLogger.info(builder.toString())
+    }
+
+    private fun StringBuilder.diffMellomToGenerasjoner(
+        hentet: GenerasjonDto,
+        skalLagres: GenerasjonDto,
+    ) {
+        appendLine("   utbetaling: ${hentet.utbetalingId} - ${skalLagres.utbetalingId}")
+        appendLine("      varsler: ${hentet.varsler.map { it.varselkode }} - ${skalLagres.varsler.map(VarselDto::varselkode)}")
+        appendLine("         tags: ${hentet.tags} - ${skalLagres.tags}")
+        appendLine("          fom: ${hentet.fom} - ${skalLagres.fom}")
+        appendLine("          tom: ${hentet.tom} - ${skalLagres.tom}")
+        appendLine("     tilstand: ${hentet.tilstand} - ${skalLagres.tilstand}")
+        appendLine("  skj.tidspkt: ${hentet.skjæringstidspunkt} - ${skalLagres.skjæringstidspunkt}")
     }
 
     internal fun byggGenerasjon(
