da2a88d2b7435c6a7680a5cb4a8f1abce7c6dc20
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-10-04 17:45:00 +0200

    🎨 Bruk hjelpefunksjoner for db-spørringer

--- spesialist-selve/src/test/kotlin/no/nav/helse/db/SaksbehandlerDaoTest.kt
+++ spesialist-selve/src/test/kotlin/no/nav/helse/db/SaksbehandlerDaoTest.kt
@@ -1,9 +1,6 @@
 package no.nav.helse.db
 
 import DatabaseIntegrationTest
-import kotliquery.queryOf
-import kotliquery.sessionOf
-import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
@@ -12,7 +9,7 @@ import java.time.LocalDateTime.now
 import java.time.temporal.ChronoUnit
 import java.util.UUID
 
-internal class SaksbehandlerDaoTest: DatabaseIntegrationTest() {
+internal class SaksbehandlerDaoTest : DatabaseIntegrationTest() {
     private val dao = SaksbehandlerDao(dataSource)
 
     @Test
@@ -46,26 +43,22 @@ internal class SaksbehandlerDaoTest: DatabaseIntegrationTest() {
     }
 
     private fun assertSaksbehandler(forventetAntall: Int, oid: UUID, navn: String, epost: String, ident: String) {
-        @Language("PostgreSQL")
-        val query = """
-           SELECT COUNT(1) FROM saksbehandler WHERE oid = ? AND navn = ? AND epost = ? AND ident = ?
-        """
+        val funnet = query(
+            """
+            SELECT COUNT(1) FROM saksbehandler
+            WHERE oid = :oid AND navn = :navn AND epost = :epost AND ident = :ident
+            """.trimIndent(), "oid" to oid, "navn" to navn, "epost" to epost, "ident" to ident
+        ).single { it.int(1) }
 
-        val funnet = sessionOf(dataSource).use {
-            it.run(queryOf(query, oid, navn, epost, ident).map { it.int(1) }.asSingle)
-        }
         assertEquals(forventetAntall, funnet)
     }
 
     private fun assertSisteTidspunkt(forventetSisteTidspunkt: LocalDateTime?, oid: UUID) {
-        @Language("PostgreSQL")
-        val query = """
-           SELECT siste_handling_utført_tidspunkt FROM saksbehandler WHERE oid = :oid
-        """
-
-        val tidspunktFraDb = sessionOf(dataSource).use {
-            it.run(queryOf(query, mapOf("oid" to oid)).map { it.localDateTimeOrNull(1) }.asSingle)
-        }
+        val tidspunktFraDb = query(
+            """
+            SELECT siste_handling_utført_tidspunkt FROM saksbehandler WHERE oid = :oid
+            """.trimIndent(), "oid" to oid
+        ).single { it.localDateTimeOrNull(1) }
 
         if (forventetSisteTidspunkt != null) assertEquals(
             forventetSisteTidspunkt.truncatedTo(ChronoUnit.MILLIS),
