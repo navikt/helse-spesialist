3b83d34936cdcf05b6076466bcd37465ee5baadc
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2024-10-09 12:45:54 +0200

    ✏️ posison -> poison

--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/PoisonPills.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/PoisonPills.kt
@@ -2,9 +2,9 @@ package no.nav.helse.mediator.meldinger
 
 import com.fasterxml.jackson.databind.JsonNode
 
-class PoisonPills(private val posisonPills: Map<String, Set<String>>) {
+class PoisonPills(private val poisonPills: Map<String, Set<String>>) {
     internal fun erPoisonPill(melding: JsonNode) =
-        posisonPills
+        poisonPills
             .any { (key, poisonPillValues) ->
                 melding[key]?.asText()?.let { verdiFraMelding ->
                     poisonPillValues.contains(verdiFraMelding)
