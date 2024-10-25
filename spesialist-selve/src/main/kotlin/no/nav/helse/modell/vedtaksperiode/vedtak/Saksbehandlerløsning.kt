e2986c31cf48655db67e9fc8e175850fa2709346
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-10-25 17:34:32 +0200

    ♻️ Saksbehandlerløsning "kjører transaksjonelt"
    Fun fact: den bruker ikke transaksjonen, den bruker bare person, som har
    sin egen transaksjon via personService.brukPersonHvisFinnes.

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/vedtak/Saksbehandlerløsning.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/vedtak/Saksbehandlerløsning.kt
@@ -1,6 +1,7 @@
 package no.nav.helse.modell.vedtaksperiode.vedtak
 
 import com.fasterxml.jackson.databind.JsonNode
+import kotliquery.TransactionalSession
 import no.nav.helse.mediator.Kommandostarter
 import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.Personmelding
@@ -98,12 +99,18 @@ internal class Saksbehandlerløsning private constructor(
         val epostadresse: String,
     )
 
+    override fun skalKjøresTransaksjonelt() = true
+
+    override fun transaksjonellBehandle(
+        person: Person,
+        kommandostarter: Kommandostarter,
+        transactionalSession: TransactionalSession,
+    ) = kommandostarter { løsGodkjenningsbehov(this@Saksbehandlerløsning, person) }
+
     override fun behandle(
         person: Person,
         kommandostarter: Kommandostarter,
-    ) {
-        kommandostarter { løsGodkjenningsbehov(this@Saksbehandlerløsning, person) }
-    }
+    ) = throw UnsupportedOperationException()
 
     override fun fødselsnummer() = fødselsnummer
 
