a28011edfd6baa72d26e0e5bf36344e395a34334
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-10-25 17:21:14 +0200

    ♻️ BehandlingOpprettet "kjører transaksjonelt"
    Fun fact: den bruker ikke transaksjonen, den bruker bare person, som har
    sin egen transaksjon via personService.brukPersonHvisFinnes.

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/BehandlingOpprettet.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/vedtaksperiode/BehandlingOpprettet.kt
@@ -1,6 +1,7 @@
 package no.nav.helse.modell.vedtaksperiode
 
 import com.fasterxml.jackson.databind.JsonNode
+import kotliquery.TransactionalSession
 import no.nav.helse.mediator.Kommandostarter
 import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.Vedtaksperiodemelding
@@ -51,11 +52,21 @@ internal class BehandlingOpprettet private constructor(
         json = jsonNode.toString(),
     )
 
+    override fun skalKjøresTransaksjonelt() = true
+
+    override fun transaksjonellBehandle(
+        person: Person,
+        kommandostarter: Kommandostarter,
+        transactionalSession: TransactionalSession,
+    ) {
+        person.nySpleisBehandling(SpleisBehandling(organisasjonsnummer, vedtaksperiodeId, spleisBehandlingId, fom, tom))
+    }
+
     override fun behandle(
         person: Person,
         kommandostarter: Kommandostarter,
     ) {
-        person.nySpleisBehandling(SpleisBehandling(organisasjonsnummer, vedtaksperiodeId, spleisBehandlingId, fom, tom))
+        throw UnsupportedOperationException()
     }
 
     override fun fødselsnummer(): String = fødselsnummer
