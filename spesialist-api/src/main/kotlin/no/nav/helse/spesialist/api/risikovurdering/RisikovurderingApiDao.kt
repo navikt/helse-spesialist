e96f42d32772d0d3cd4d6cb23c2946e2d01b3c84
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2024-10-30 15:16:08 +0100

    ♻️ Innfører QueryRunner i RisikovurderingApiDao

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/risikovurdering/RisikovurderingApiDao.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/risikovurdering/RisikovurderingApiDao.kt
@@ -2,17 +2,19 @@ package no.nav.helse.spesialist.api.risikovurdering
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.JsonNode
-import no.nav.helse.HelseDao
+import no.nav.helse.HelseDao.Companion.asSQL
+import no.nav.helse.db.MedDataSource
+import no.nav.helse.db.QueryRunner
 import no.nav.helse.spesialist.api.objectMapper
 import java.util.UUID
 import javax.sql.DataSource
 
-class RisikovurderingApiDao(dataSource: DataSource) : HelseDao(dataSource) {
+class RisikovurderingApiDao(dataSource: DataSource) : QueryRunner by MedDataSource(dataSource) {
     fun finnRisikovurdering(vedtaksperiodeId: UUID): RisikovurderingApiDto? =
         asSQL(
             " SELECT data FROM risikovurdering_2021 WHERE vedtaksperiode_id = :vedtaksperiodeId ORDER BY id DESC LIMIT 1 ",
             "vedtaksperiodeId" to vedtaksperiodeId,
-        ).single { objectMapper.readTree(it.string("data")) }?.let { data ->
+        ).singleOrNull { objectMapper.readTree(it.string("data")) }?.let { data ->
             RisikovurderingApiDto(funn = data["funn"].toList(), kontrollertOk = data["kontrollertOk"].toList())
         }
 
