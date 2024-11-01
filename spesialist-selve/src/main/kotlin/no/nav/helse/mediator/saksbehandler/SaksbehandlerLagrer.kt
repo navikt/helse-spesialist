8eed8b2bed0fea8b74907814d7f8320e3a51594e
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2024-11-01 14:47:12 +0100

    ♻️ Gidder ikke bruke SaksbehandlerVisitor ved lagring av saksbehandler

--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/saksbehandler/SaksbehandlerLagrer.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/saksbehandler/SaksbehandlerLagrer.kt
@@ -1,31 +1,18 @@
 package no.nav.helse.mediator.saksbehandler
 
 import no.nav.helse.db.SaksbehandlerDao
-import no.nav.helse.db.SaksbehandlerFraDatabase
 import no.nav.helse.modell.saksbehandler.Saksbehandler
-import no.nav.helse.modell.saksbehandler.SaksbehandlerVisitor
-import java.util.UUID
+import no.nav.helse.modell.saksbehandler.Saksbehandler.Companion.toDto
 
-class SaksbehandlerLagrer(private val saksbehandlerDao: SaksbehandlerDao) : SaksbehandlerVisitor {
-    private lateinit var saksbehandlerFraDatabase: SaksbehandlerFraDatabase
-
+class SaksbehandlerLagrer(private val saksbehandlerDao: SaksbehandlerDao) {
     internal fun lagre(saksbehandler: Saksbehandler) {
-        saksbehandler.accept(this)
+        val dto = saksbehandler.toDto()
         saksbehandlerDao.opprettEllerOppdater(
-            oid = saksbehandlerFraDatabase.oid,
-            navn = saksbehandlerFraDatabase.navn,
-            epost = saksbehandlerFraDatabase.epostadresse,
-            ident = saksbehandlerFraDatabase.ident,
+            oid = dto.oid,
+            navn = dto.navn,
+            epost = dto.epostadresse,
+            ident = dto.ident,
         )
-        saksbehandlerDao.oppdaterSistObservert(saksbehandlerFraDatabase.oid)
-    }
-
-    override fun visitSaksbehandler(
-        epostadresse: String,
-        oid: UUID,
-        navn: String,
-        ident: String,
-    ) {
-        saksbehandlerFraDatabase = SaksbehandlerFraDatabase(epostadresse, oid, navn, ident)
+        saksbehandlerDao.oppdaterSistObservert(dto.oid)
     }
 }
