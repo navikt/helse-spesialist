b6e9cf52da77b18fa3053ed2b723007a76600bfe
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-11-09 11:17:38 +0100

    🚑️ Bryter opp en spørring i to for å unngå seq scan
    På grunn av OR-en klarer ikke query planneren å benytte index, og det
    går tregt i speil å hente enkelte personer.

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/periodehistorikk/PeriodehistorikkApiDao.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/periodehistorikk/PeriodehistorikkApiDao.kt
@@ -12,11 +12,20 @@ class PeriodehistorikkApiDao(
 ) : QueryRunner by MedDataSource(dataSource) {
     fun finn(utbetalingId: UUID) =
         asSQL(
-            """              
-            SELECT ph.id, ph.type, ph.timestamp, ph.notat_id, ph.json, s.ident, ph.dialog_ref
+            // Bruker UNION som en quick fix for å unngå seq scan på en spørring som gjøres mange ganger når
+            // GraphQL skal bygge person-dataene.
+            // Må kaste til jsonb for at databasen skal klare å filtrere vekk duplikate rader
+            """
+            SELECT ph.id, ph.type, ph.timestamp, ph.notat_id, ph.json::jsonb, s.ident, ph.dialog_ref
             FROM periodehistorikk ph
-            LEFT JOIN saksbehandler s on ph.saksbehandler_oid = s.oid
-            WHERE ph.utbetaling_id = :utbetaling_id OR ph.generasjon_id IN (SELECT unik_id FROM selve_vedtaksperiode_generasjon svg WHERE svg.utbetaling_id = :utbetaling_id)
+            LEFT JOIN saksbehandler s ON ph.saksbehandler_oid = s.oid
+            WHERE ph.utbetaling_id = :utbetaling_id
+            UNION
+            SELECT ph.id, ph.type, ph.timestamp, ph.notat_id, ph.json::jsonb, s.ident, ph.dialog_ref
+            FROM periodehistorikk ph
+            LEFT JOIN saksbehandler s ON ph.saksbehandler_oid = s.oid
+            LEFT JOIN selve_vedtaksperiode_generasjon svg ON ph.generasjon_id = svg.unik_id
+            WHERE svg.utbetaling_id = :utbetaling_id
             """.trimIndent(),
             "utbetaling_id" to utbetalingId,
         ).list { periodehistorikkDto(it) }
