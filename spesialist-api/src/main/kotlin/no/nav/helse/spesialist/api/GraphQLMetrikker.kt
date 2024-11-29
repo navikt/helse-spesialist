1de3ffbff4c7ec2d7f7843959166d05d74c0016c
Christian Skovborg Gule <christianskovborg@gmail.com>
2024-11-29 08:01:23 +0100

    Revert "🚧 Juster litt på graphql-metrikker"
    This reverts commit 8cc630f526ecdd55613dc239eaa01ceac2f64190.

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/GraphQLMetrikker.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/GraphQLMetrikker.kt
@@ -12,7 +12,10 @@ import io.micrometer.core.instrument.Metrics
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 
-private val registry = Metrics.globalRegistry.add(PrometheusMeterRegistry(PrometheusConfig.DEFAULT))
+private val meterRegistry =
+    PrometheusMeterRegistry(PrometheusConfig.DEFAULT).also {
+        Metrics.globalRegistry.add(it)
+    }
 
 val GraphQLMetrikker =
     createRouteScopedPlugin("GraphQLMetrikker") {
@@ -21,10 +24,7 @@ val GraphQLMetrikker =
             if (call.request.httpMethod == HttpMethod.Get) return@onCallRespond
             (call.receive<JsonNode>()["operationName"]?.textValue() ?: "ukjent").let { operationName ->
                 val elapsed = call.processingTimeMillis()
-                graphQLResponstider
-                    .withRegistry(registry)
-                    .withTags(operationName)
-                    .record(elapsed.toDouble())
+                graphQLResponstider.tags(operationName).register(meterRegistry).record(elapsed.toDouble())
             }
         }
     }
@@ -38,4 +38,4 @@ internal val auditLogTeller =
     Counter
         .builder("auditlog_total")
         .description("Teller antall auditlogginnslag")
-        .register(registry)
+        .register(meterRegistry)
