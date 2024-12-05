6c4c7a272c7d503cad24c78e1f6c0078a7e9435e
Christian Skovborg Gule <christianskovborg@gmail.com>
2024-12-05 10:06:21 +0100

    ♻️ VarseldefinisjonRiver pakker ut felter selv

--- spesialist-selve/src/main/kotlin/no/nav/helse/kafka/VarseldefinisjonRiver.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/kafka/VarseldefinisjonRiver.kt
@@ -1,12 +1,16 @@
 package no.nav.helse.kafka
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers.River
+import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDateTime
+import com.github.navikt.tbd_libs.rapids_and_rivers.isMissingOrNull
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import io.micrometer.core.instrument.MeterRegistry
 import net.logstash.logback.argument.StructuredArguments.kv
 import no.nav.helse.mediator.MeldingMediator
+import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.hendelser.VarseldefinisjonMessage
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -47,7 +51,16 @@ internal class VarseldefinisjonRiver(
     ) {
         sikkerlogg.info("Mottok melding om ny definisjon for {}", kv("varselkode", packet["varselkode"].asText()))
 
-        val message = VarseldefinisjonMessage(packet)
+        val message =
+            VarseldefinisjonMessage(
+                id = packet["gjeldende_definisjon.id"].asUUID(),
+                varselkode = packet["varselkode"].asText(),
+                tittel = packet["gjeldende_definisjon.tittel"].asText(),
+                forklaring = packet["gjeldende_definisjon.forklaring"].takeUnless(JsonNode::isMissingOrNull)?.textValue(),
+                handling = packet["gjeldende_definisjon.handling"].takeUnless(JsonNode::isMissingOrNull)?.textValue(),
+                avviklet = packet["gjeldende_definisjon.avviklet"].asBoolean(),
+                opprettet = packet["gjeldende_definisjon.opprettet"].asLocalDateTime(),
+            )
         message.sendInnTil(mediator)
     }
 }
--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/hendelser/VarseldefinisjonMessage.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/hendelser/VarseldefinisjonMessage.kt
@@ -1,24 +1,19 @@
 package no.nav.helse.mediator.meldinger.hendelser
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
-import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDateTime
-import com.github.navikt.tbd_libs.rapids_and_rivers.isMissingOrNull
 import no.nav.helse.mediator.MeldingMediator
-import no.nav.helse.mediator.asUUID
 import no.nav.helse.modell.varsel.Varseldefinisjon
 import java.time.LocalDateTime
 import java.util.UUID
 
-internal class VarseldefinisjonMessage(packet: JsonMessage) {
-    private val id: UUID = packet["gjeldende_definisjon.id"].asUUID()
-    private val varselkode: String = packet["varselkode"].asText()
-    private val tittel: String = packet["gjeldende_definisjon.tittel"].asText()
-    private val forklaring: String? = packet["gjeldende_definisjon.forklaring"].takeUnless(JsonNode::isMissingOrNull)?.textValue()
-    private val handling: String? = packet["gjeldende_definisjon.handling"].takeUnless(JsonNode::isMissingOrNull)?.textValue()
-    private val avviklet: Boolean = packet["gjeldende_definisjon.avviklet"].asBoolean()
-    private val opprettet: LocalDateTime = packet["gjeldende_definisjon.opprettet"].asLocalDateTime()
-
+internal class VarseldefinisjonMessage(
+    private val id: UUID,
+    private val varselkode: String,
+    private val tittel: String,
+    private val forklaring: String?,
+    private val handling: String?,
+    private val avviklet: Boolean,
+    private val opprettet: LocalDateTime,
+) {
     private val varseldefinisjon get() = Varseldefinisjon(id, varselkode, tittel, forklaring, handling, avviklet, opprettet)
 
     internal fun sendInnTil(mediator: MeldingMediator) {
