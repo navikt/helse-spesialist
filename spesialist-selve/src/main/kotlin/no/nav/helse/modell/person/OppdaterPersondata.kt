4313a5239c2eb1eb09a813f5c7845e6603f9e01f
Christian Skovborg Gule <christianskovborg@gmail.com>
2024-12-05 09:41:21 +0100

    ♻️ OppdaterPersondataRiver pakker ut felter selv

--- spesialist-selve/src/main/kotlin/no/nav/helse/modell/person/OppdaterPersondata.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/modell/person/OppdaterPersondata.kt
@@ -1,12 +1,10 @@
 package no.nav.helse.modell.person
 
 import com.fasterxml.jackson.databind.JsonNode
-import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import kotliquery.TransactionalSession
 import no.nav.helse.db.OpptegnelseRepository
 import no.nav.helse.db.PersonRepository
 import no.nav.helse.mediator.Kommandostarter
-import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.Personmelding
 import no.nav.helse.modell.kommando.Command
 import no.nav.helse.modell.kommando.MacroCommand
@@ -17,17 +15,11 @@ import no.nav.helse.spesialist.api.abonnement.PersonOppdatertPayload
 import java.time.LocalDate
 import java.util.UUID
 
-internal class OppdaterPersondata private constructor(
+internal class OppdaterPersondata(
     override val id: UUID,
     private val fødselsnummer: String,
     private val json: String,
 ) : Personmelding {
-    internal constructor(packet: JsonMessage) : this(
-        id = packet["@id"].asUUID(),
-        fødselsnummer = packet["fødselsnummer"].asText(),
-        json = packet.toJson(),
-    )
-
     internal constructor(jsonNode: JsonNode) : this(
         id = UUID.fromString(jsonNode["@id"].asText()),
         fødselsnummer = jsonNode["fødselsnummer"].asText(),
