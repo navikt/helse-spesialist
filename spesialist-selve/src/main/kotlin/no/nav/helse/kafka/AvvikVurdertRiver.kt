fe3b8fe6c53c97c111e35b3247412f4b37539ffd
Christian Skovborg Gule <christianskovborg@gmail.com>
2024-12-05 09:13:49 +0100

    ♻️ AvvikVurdertRiver pakker ut felter selv

--- spesialist-selve/src/main/kotlin/no/nav/helse/kafka/AvvikVurdertRiver.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/kafka/AvvikVurdertRiver.kt
@@ -1,12 +1,23 @@
 package no.nav.helse.kafka
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers.River
+import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDate
+import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDateTime
+import com.github.navikt.tbd_libs.rapids_and_rivers.asYearMonth
+import com.github.navikt.tbd_libs.rapids_and_rivers.isMissingOrNull
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import io.micrometer.core.instrument.MeterRegistry
 import no.nav.helse.mediator.MeldingMediator
+import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.hendelser.AvvikVurdertMessage
+import no.nav.helse.modell.vilkårsprøving.BeregningsgrunnlagDto
+import no.nav.helse.modell.vilkårsprøving.InnrapportertInntektDto
+import no.nav.helse.modell.vilkårsprøving.InntektDto
+import no.nav.helse.modell.vilkårsprøving.OmregnetÅrsinntektDto
+import no.nav.helse.modell.vilkårsprøving.SammenligningsgrunnlagDto
 
 internal class AvvikVurdertRiver(
     private val mediator: MeldingMediator,
@@ -53,6 +64,57 @@ internal class AvvikVurdertRiver(
         metadata: MessageMetadata,
         meterRegistry: MeterRegistry,
     ) {
-        mediator.mottaMelding(AvvikVurdertMessage(packet), context)
+        mediator.mottaMelding(
+            AvvikVurdertMessage(
+                unikId = packet["avviksvurdering.id"].asUUID(),
+                vilkårsgrunnlagId = packet["avviksvurdering.vilkårsgrunnlagId"].takeUnless { it.isMissingOrNull() }?.asUUID(),
+                fødselsnummer = packet["fødselsnummer"].asText(),
+                vedtaksperiodeId = packet["vedtaksperiodeId"].asUUID(),
+                skjæringstidspunkt = packet["skjæringstidspunkt"].asLocalDate(),
+                opprettet = packet["avviksvurdering.opprettet"].asLocalDateTime(),
+                avviksprosent = packet["avviksvurdering.avviksprosent"].asDouble(),
+                beregningsgrunnlag = beregningsgrunnlag(packet["avviksvurdering.beregningsgrunnlag"]),
+                sammenligningsgrunnlag = sammenligningsgrunnlag(packet["avviksvurdering.sammenligningsgrunnlag"]),
+                id = packet["@id"].asUUID(),
+                json = packet.toJson(),
+            ),
+            context,
+        )
     }
+
+    private fun beregningsgrunnlag(json: JsonNode): BeregningsgrunnlagDto =
+        BeregningsgrunnlagDto(
+            totalbeløp = json["totalbeløp"].asDouble(),
+            omregnedeÅrsinntekter = omregnedeÅrsinntekter(json["omregnedeÅrsinntekter"]),
+        )
+
+    private fun omregnedeÅrsinntekter(json: JsonNode): List<OmregnetÅrsinntektDto> =
+        json.map {
+            OmregnetÅrsinntektDto(
+                arbeidsgiverreferanse = it["arbeidsgiverreferanse"].asText(),
+                beløp = it["beløp"].asDouble(),
+            )
+        }
+
+    private fun sammenligningsgrunnlag(json: JsonNode): SammenligningsgrunnlagDto =
+        SammenligningsgrunnlagDto(
+            totalbeløp = json["totalbeløp"].asDouble(),
+            innrapporterteInntekter = innrapporterteInntekter(json["innrapporterteInntekter"]),
+        )
+
+    private fun innrapporterteInntekter(json: JsonNode): List<InnrapportertInntektDto> =
+        json.map {
+            InnrapportertInntektDto(
+                arbeidsgiverreferanse = it["arbeidsgiverreferanse"].asText(),
+                inntekter = inntekter(it["inntekter"]),
+            )
+        }
+
+    private fun inntekter(json: JsonNode): List<InntektDto> =
+        json.map {
+            InntektDto(
+                årMåned = it["årMåned"].asYearMonth(),
+                beløp = it["beløp"].asDouble(),
+            )
+        }
 }
--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/hendelser/AvvikVurdertMessage.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/hendelser/AvvikVurdertMessage.kt
@@ -1,35 +1,29 @@
 package no.nav.helse.mediator.meldinger.hendelser
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
-import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDate
-import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDateTime
-import com.github.navikt.tbd_libs.rapids_and_rivers.asYearMonth
-import com.github.navikt.tbd_libs.rapids_and_rivers.isMissingOrNull
 import kotliquery.TransactionalSession
 import no.nav.helse.mediator.Kommandostarter
-import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.Vedtaksperiodemelding
 import no.nav.helse.modell.person.Person
 import no.nav.helse.modell.vilkårsprøving.AvviksvurderingDto
 import no.nav.helse.modell.vilkårsprøving.BeregningsgrunnlagDto
-import no.nav.helse.modell.vilkårsprøving.InnrapportertInntektDto
-import no.nav.helse.modell.vilkårsprøving.InntektDto
-import no.nav.helse.modell.vilkårsprøving.OmregnetÅrsinntektDto
 import no.nav.helse.modell.vilkårsprøving.SammenligningsgrunnlagDto
+import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.UUID
 
-internal class AvvikVurdertMessage(private val packet: JsonMessage) : Vedtaksperiodemelding {
-    private val unikId = packet["avviksvurdering.id"].asUUID()
-    private val vilkårsgrunnlagId = packet["avviksvurdering.vilkårsgrunnlagId"].takeUnless { it.isMissingOrNull() }?.asUUID()
-    private val fødselsnummer = packet["fødselsnummer"].asText()
-    private val vedtaksperiodeId = packet["vedtaksperiodeId"].asUUID()
-    private val skjæringstidspunkt = packet["skjæringstidspunkt"].asLocalDate()
-    private val opprettet = packet["avviksvurdering.opprettet"].asLocalDateTime()
-    private val avviksprosent = packet["avviksvurdering.avviksprosent"].asDouble()
-    private val beregningsgrunnlag = beregningsgrunnlag(packet["avviksvurdering.beregningsgrunnlag"])
-    private val sammenligningsgrunnlag = sammenligningsgrunnlag(packet["avviksvurdering.sammenligningsgrunnlag"])
-
+internal class AvvikVurdertMessage(
+    override val id: UUID,
+    private val unikId: UUID,
+    private val vilkårsgrunnlagId: UUID?,
+    private val fødselsnummer: String,
+    private val vedtaksperiodeId: UUID,
+    private val skjæringstidspunkt: LocalDate,
+    private val opprettet: LocalDateTime,
+    private val avviksprosent: Double,
+    private val beregningsgrunnlag: BeregningsgrunnlagDto,
+    private val sammenligningsgrunnlag: SammenligningsgrunnlagDto,
+    private val json: String,
+) : Vedtaksperiodemelding {
     private val avviksvurdering
         get() =
             AvviksvurderingDto(
@@ -43,44 +37,6 @@ internal class AvvikVurdertMessage(private val packet: JsonMessage) : Vedtaksper
                 beregningsgrunnlag = beregningsgrunnlag,
             )
 
-    private fun beregningsgrunnlag(json: JsonNode): BeregningsgrunnlagDto =
-        BeregningsgrunnlagDto(
-            totalbeløp = json["totalbeløp"].asDouble(),
-            omregnedeÅrsinntekter = omregnedeÅrsinntekter(json["omregnedeÅrsinntekter"]),
-        )
-
-    private fun omregnedeÅrsinntekter(json: JsonNode): List<OmregnetÅrsinntektDto> =
-        json.map {
-            OmregnetÅrsinntektDto(
-                arbeidsgiverreferanse = it["arbeidsgiverreferanse"].asText(),
-                beløp = it["beløp"].asDouble(),
-            )
-        }
-
-    private fun sammenligningsgrunnlag(json: JsonNode): SammenligningsgrunnlagDto =
-        SammenligningsgrunnlagDto(
-            totalbeløp = json["totalbeløp"].asDouble(),
-            innrapporterteInntekter = innrapporterteInntekter(json["innrapporterteInntekter"]),
-        )
-
-    private fun innrapporterteInntekter(json: JsonNode): List<InnrapportertInntektDto> =
-        json.map {
-            InnrapportertInntektDto(
-                arbeidsgiverreferanse = it["arbeidsgiverreferanse"].asText(),
-                inntekter = inntekter(it["inntekter"]),
-            )
-        }
-
-    private fun inntekter(json: JsonNode): List<InntektDto> =
-        json.map {
-            InntektDto(
-                årMåned = it["årMåned"].asYearMonth(),
-                beløp = it["beløp"].asDouble(),
-            )
-        }
-
-    override val id: UUID = packet["@id"].asUUID()
-
     override fun vedtaksperiodeId(): UUID = vedtaksperiodeId
 
     override fun behandle(
@@ -93,5 +49,5 @@ internal class AvvikVurdertMessage(private val packet: JsonMessage) : Vedtaksper
 
     override fun fødselsnummer(): String = fødselsnummer
 
-    override fun toJson(): String = packet.toJson()
+    override fun toJson(): String = json
 }
