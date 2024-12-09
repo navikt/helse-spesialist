70112f8d5704dfecec8ad980f12097b79e389d74
Christian Skovborg Gule <christianskovborg@gmail.com>
2024-12-09 10:25:19 +0100

    ♻️ AvsluttetMedVedtakRiver pakker ut felter selv

--- spesialist-selve/src/main/kotlin/no/nav/helse/kafka/AvsluttetMedVedtakRiver.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/kafka/AvsluttetMedVedtakRiver.kt
@@ -1,12 +1,18 @@
 package no.nav.helse.kafka
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.treeToValue
 import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers.River
+import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDateTime
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import io.micrometer.core.instrument.MeterRegistry
 import no.nav.helse.mediator.MeldingMediator
+import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.hendelser.AvsluttetMedVedtakMessage
+import no.nav.helse.modell.vedtak.Faktatype
+import no.nav.helse.modell.vedtak.Sykepengegrunnlagsfakta
 
 internal class AvsluttetMedVedtakRiver(
     private val mediator: MeldingMediator,
@@ -63,6 +69,83 @@ internal class AvsluttetMedVedtakRiver(
         metadata: MessageMetadata,
         meterRegistry: MeterRegistry,
     ) {
-        mediator.mottaMelding(AvsluttetMedVedtakMessage(packet), context)
+        mediator.mottaMelding(
+            melding =
+                AvsluttetMedVedtakMessage(
+                    id = packet["@id"].asUUID(),
+                    fødselsnummer = packet["fødselsnummer"].asText(),
+                    vedtakFattetTidspunkt = packet["vedtakFattetTidspunkt"].asLocalDateTime(),
+                    vedtaksperiodeId = packet["vedtaksperiodeId"].asUUID(),
+                    spleisBehandlingId = packet["behandlingId"].asUUID(),
+                    hendelser = packet["hendelser"].map { it.asUUID() },
+                    sykepengegrunnlag = packet["sykepengegrunnlag"].asDouble(),
+                    grunnlagForSykepengegrunnlag = packet["grunnlagForSykepengegrunnlag"].asDouble(),
+                    grunnlagForSykepengegrunnlagPerArbeidsgiver =
+                        jacksonObjectMapper().treeToValue<Map<String, Double>>(
+                            packet["grunnlagForSykepengegrunnlagPerArbeidsgiver"],
+                        ),
+                    begrensning = packet["begrensning"].asText(),
+                    inntekt = packet["inntekt"].asDouble(),
+                    sykepengegrunnlagsfakta = sykepengegrunnlagsfakta(packet, faktatype(packet)),
+                    json = packet.toJson(),
+                ),
+            messageContext = context,
+        )
+    }
+
+    private fun faktatype(packet: JsonMessage): Faktatype {
+        return when (val fastsattString = packet["sykepengegrunnlagsfakta.fastsatt"].asText()) {
+            "EtterSkjønn" -> Faktatype.ETTER_SKJØNN
+            "EtterHovedregel" -> Faktatype.ETTER_HOVEDREGEL
+            "IInfotrygd" -> Faktatype.I_INFOTRYGD
+            else -> throw IllegalArgumentException("FastsattType $fastsattString er ikke støttet")
+        }
+    }
+
+    private fun sykepengegrunnlagsfakta(
+        packet: JsonMessage,
+        faktatype: Faktatype,
+    ): Sykepengegrunnlagsfakta {
+        if (faktatype == Faktatype.I_INFOTRYGD) {
+            return Sykepengegrunnlagsfakta.Infotrygd(
+                omregnetÅrsinntekt = packet["sykepengegrunnlagsfakta.omregnetÅrsinntekt"].asDouble(),
+            )
+        }
+
+        return when (faktatype) {
+            Faktatype.ETTER_SKJØNN ->
+                Sykepengegrunnlagsfakta.Spleis.EtterSkjønn(
+                    omregnetÅrsinntekt = packet["sykepengegrunnlagsfakta.omregnetÅrsinntekt"].asDouble(),
+                    seksG = packet["sykepengegrunnlagsfakta.6G"].asDouble(),
+                    skjønnsfastsatt = packet["sykepengegrunnlagsfakta.skjønnsfastsatt"].asDouble(),
+                    tags = mutableSetOf(),
+                    arbeidsgivere =
+                        packet["sykepengegrunnlagsfakta.arbeidsgivere"].map { arbeidsgiver ->
+                            val organisasjonsnummer = arbeidsgiver["arbeidsgiver"].asText()
+                            Sykepengegrunnlagsfakta.Spleis.Arbeidsgiver.EtterSkjønn(
+                                organisasjonsnummer = organisasjonsnummer,
+                                omregnetÅrsinntekt = arbeidsgiver["omregnetÅrsinntekt"].asDouble(),
+                                skjønnsfastsatt = arbeidsgiver["skjønnsfastsatt"].asDouble(),
+                            )
+                        },
+                )
+
+            Faktatype.ETTER_HOVEDREGEL ->
+                Sykepengegrunnlagsfakta.Spleis.EtterHovedregel(
+                    omregnetÅrsinntekt = packet["sykepengegrunnlagsfakta.omregnetÅrsinntekt"].asDouble(),
+                    seksG = packet["sykepengegrunnlagsfakta.6G"].asDouble(),
+                    tags = mutableSetOf(),
+                    arbeidsgivere =
+                        packet["sykepengegrunnlagsfakta.arbeidsgivere"].map { arbeidsgiver ->
+                            val organisasjonsnummer = arbeidsgiver["arbeidsgiver"].asText()
+                            Sykepengegrunnlagsfakta.Spleis.Arbeidsgiver.EtterHovedregel(
+                                organisasjonsnummer = organisasjonsnummer,
+                                omregnetÅrsinntekt = arbeidsgiver["omregnetÅrsinntekt"].asDouble(),
+                            )
+                        },
+                )
+
+            else -> error("Her vet vi ikke hva som har skjedd. Feil i kompilatoren?")
+        }
     }
 }
--- spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/hendelser/AvsluttetMedVedtakMessage.kt
+++ spesialist-selve/src/main/kotlin/no/nav/helse/mediator/meldinger/hendelser/AvsluttetMedVedtakMessage.kt
@@ -1,37 +1,29 @@
 package no.nav.helse.mediator.meldinger.hendelser
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.treeToValue
-import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
-import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDateTime
 import kotliquery.TransactionalSession
 import no.nav.helse.mediator.Kommandostarter
-import no.nav.helse.mediator.asUUID
 import no.nav.helse.mediator.meldinger.Vedtaksperiodemelding
 import no.nav.helse.modell.person.Person
-import no.nav.helse.modell.vedtak.Faktatype
 import no.nav.helse.modell.vedtak.Sykepengegrunnlagsfakta
 import no.nav.helse.modell.vedtaksperiode.vedtak.AvsluttetMedVedtak
+import java.time.LocalDateTime
 import java.util.UUID
 
 internal class AvsluttetMedVedtakMessage(
-    private val packet: JsonMessage,
+    override val id: UUID,
+    private val fødselsnummer: String,
+    private val vedtakFattetTidspunkt: LocalDateTime,
+    private val vedtaksperiodeId: UUID,
+    private val spleisBehandlingId: UUID,
+    private val hendelser: List<UUID>,
+    private val sykepengegrunnlag: Double,
+    private val grunnlagForSykepengegrunnlag: Double,
+    private val grunnlagForSykepengegrunnlagPerArbeidsgiver: Map<String, Double>,
+    private val begrensning: String,
+    private val inntekt: Double,
+    private val sykepengegrunnlagsfakta: Sykepengegrunnlagsfakta,
+    private val json: String,
 ) : Vedtaksperiodemelding {
-    private val fødselsnummer = packet["fødselsnummer"].asText()
-    private val vedtakFattetTidspunkt = packet["vedtakFattetTidspunkt"].asLocalDateTime()
-    private val vedtaksperiodeId = packet["vedtaksperiodeId"].asUUID()
-    private val spleisBehandlingId = packet["behandlingId"].asUUID()
-    private val hendelser = packet["hendelser"].map { it.asUUID() }
-    private val sykepengegrunnlag = packet["sykepengegrunnlag"].asDouble()
-    private val grunnlagForSykepengegrunnlag = packet["grunnlagForSykepengegrunnlag"].asDouble()
-    private val grunnlagForSykepengegrunnlagPerArbeidsgiver =
-        jacksonObjectMapper().treeToValue<Map<String, Double>>(
-            packet["grunnlagForSykepengegrunnlagPerArbeidsgiver"],
-        )
-    private val begrensning = packet["begrensning"].asText()
-    private val inntekt = packet["inntekt"].asDouble()
-    private val sykepengegrunnlagsfakta = sykepengegrunnlagsfakta(packet, faktatype(packet))
-
     override fun fødselsnummer(): String = fødselsnummer
 
     override fun vedtaksperiodeId(): UUID = vedtaksperiodeId
@@ -42,9 +34,7 @@ internal class AvsluttetMedVedtakMessage(
         transactionalSession: TransactionalSession,
     ) = person.fattVedtak(avsluttetMedVedtak)
 
-    override val id: UUID = packet["@id"].asUUID()
-
-    override fun toJson(): String = packet.toJson()
+    override fun toJson(): String = json
 
     private val avsluttetMedVedtak get() =
         AvsluttetMedVedtak(
@@ -58,60 +48,4 @@ internal class AvsluttetMedVedtakMessage(
             sykepengegrunnlagsfakta = sykepengegrunnlagsfakta,
             vedtakFattetTidspunkt = vedtakFattetTidspunkt,
         )
-
-    private fun faktatype(packet: JsonMessage): Faktatype {
-        return when (val fastsattString = packet["sykepengegrunnlagsfakta.fastsatt"].asText()) {
-            "EtterSkjønn" -> Faktatype.ETTER_SKJØNN
-            "EtterHovedregel" -> Faktatype.ETTER_HOVEDREGEL
-            "IInfotrygd" -> Faktatype.I_INFOTRYGD
-            else -> throw IllegalArgumentException("FastsattType $fastsattString er ikke støttet")
-        }
-    }
-
-    private fun sykepengegrunnlagsfakta(
-        packet: JsonMessage,
-        faktatype: Faktatype,
-    ): Sykepengegrunnlagsfakta {
-        if (faktatype == Faktatype.I_INFOTRYGD) {
-            return Sykepengegrunnlagsfakta.Infotrygd(
-                omregnetÅrsinntekt = packet["sykepengegrunnlagsfakta.omregnetÅrsinntekt"].asDouble(),
-            )
-        }
-
-        return when (faktatype) {
-            Faktatype.ETTER_SKJØNN ->
-                Sykepengegrunnlagsfakta.Spleis.EtterSkjønn(
-                    omregnetÅrsinntekt = packet["sykepengegrunnlagsfakta.omregnetÅrsinntekt"].asDouble(),
-                    seksG = packet["sykepengegrunnlagsfakta.6G"].asDouble(),
-                    skjønnsfastsatt = packet["sykepengegrunnlagsfakta.skjønnsfastsatt"].asDouble(),
-                    tags = mutableSetOf(),
-                    arbeidsgivere =
-                        packet["sykepengegrunnlagsfakta.arbeidsgivere"].map { arbeidsgiver ->
-                            val organisasjonsnummer = arbeidsgiver["arbeidsgiver"].asText()
-                            Sykepengegrunnlagsfakta.Spleis.Arbeidsgiver.EtterSkjønn(
-                                organisasjonsnummer = organisasjonsnummer,
-                                omregnetÅrsinntekt = arbeidsgiver["omregnetÅrsinntekt"].asDouble(),
-                                skjønnsfastsatt = arbeidsgiver["skjønnsfastsatt"].asDouble(),
-                            )
-                        },
-                )
-
-            Faktatype.ETTER_HOVEDREGEL ->
-                Sykepengegrunnlagsfakta.Spleis.EtterHovedregel(
-                    omregnetÅrsinntekt = packet["sykepengegrunnlagsfakta.omregnetÅrsinntekt"].asDouble(),
-                    seksG = packet["sykepengegrunnlagsfakta.6G"].asDouble(),
-                    tags = mutableSetOf(),
-                    arbeidsgivere =
-                        packet["sykepengegrunnlagsfakta.arbeidsgivere"].map { arbeidsgiver ->
-                            val organisasjonsnummer = arbeidsgiver["arbeidsgiver"].asText()
-                            Sykepengegrunnlagsfakta.Spleis.Arbeidsgiver.EtterHovedregel(
-                                organisasjonsnummer = organisasjonsnummer,
-                                omregnetÅrsinntekt = arbeidsgiver["omregnetÅrsinntekt"].asDouble(),
-                            )
-                        },
-                )
-
-            else -> error("Her vet vi ikke hva som har skjedd. Feil i kompilatoren?")
-        }
-    }
 }
