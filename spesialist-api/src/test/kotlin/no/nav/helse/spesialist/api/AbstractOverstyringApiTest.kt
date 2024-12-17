e369d65af76700c456d624478956ff362ad6ebab
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2024-12-17 17:08:46 +0100

    🎨 Reformater graphql-spørring for tester
    Det ble for slitsomt å forholde seg til de litt ville innrykkene 😄

--- spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/AbstractOverstyringApiTest.kt
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/AbstractOverstyringApiTest.kt
@@ -16,23 +16,17 @@ internal abstract class AbstractOverstyringApiTest : AbstractGraphQLApiTest() {
                     fodselsnummer: "${overstyring.fodselsnummer}", 
                     organisasjonsnummer: "${overstyring.organisasjonsnummer}", 
                     vedtaksperiodeId: "${overstyring.vedtaksperiodeId}",
-                    dager: ${
-                overstyring.dager.map {
-                    """
-                                {
-                                dato: "${it.dato}",
-                                fraGrad: ${it.fraGrad},
-                                grad: ${it.grad},
-                                type: "${it.type}",
-                                fraType: "${it.fraType}",
-                                lovhjemmel: {bokstav: "A", ledd: "Albue", lovverk: "Norske lover", lovverksversjon: "1803", paragraf: "8-30"}
-                                }
-                            """
-                }
-            }
+                    dager: ${overstyring.dager.map { """ {
+                        dato: "${it.dato}",
+                        fraGrad: ${it.fraGrad},
+                        grad: ${it.grad},
+                        type: "${it.type}",
+                        fraType: "${it.fraType}",
+                        lovhjemmel: {bokstav: "A", ledd: "Albue", lovverk: "Norske lover", lovverksversjon: "1803", paragraf: "8-30"}
+                    } """ }}
                 })
             }
-        """,
+            """,
         )
 
     fun overstyrArbeidsforhold(overstyring: ArbeidsforholdOverstyringHandling): JsonNode =
@@ -43,22 +37,16 @@ internal abstract class AbstractOverstyringApiTest : AbstractGraphQLApiTest() {
                     aktorId: "${overstyring.aktorId}",
                     skjaringstidspunkt: "${overstyring.skjaringstidspunkt}",
                     fodselsnummer: "${overstyring.fodselsnummer}",
-                    overstyrteArbeidsforhold: ${
-                overstyring.overstyrteArbeidsforhold.map {
-                    """
-                                {
-                                begrunnelse: "${it.begrunnelse}",
-                                deaktivert: ${it.deaktivert},
-                                forklaring: "${it.forklaring}",
-                                orgnummer: "${it.orgnummer}",
-                                }
-                            """
-                }
-            },
+                    overstyrteArbeidsforhold: ${overstyring.overstyrteArbeidsforhold.map { """ {
+                        begrunnelse: "${it.begrunnelse}",
+                        deaktivert: ${it.deaktivert},
+                        forklaring: "${it.forklaring}",
+                        orgnummer: "${it.orgnummer}",
+                    } """ }},
                     vedtaksperiodeId: "${overstyring.vedtaksperiodeId}"
                 })
             }
-        """,
+            """,
         )
 
     fun overstyrInntektOgRefusjon(overstyring: InntektOgRefusjonOverstyring): JsonNode =
@@ -69,46 +57,27 @@ internal abstract class AbstractOverstyringApiTest : AbstractGraphQLApiTest() {
                     aktorId: "${overstyring.aktorId}",
                     skjaringstidspunkt: "${overstyring.skjaringstidspunkt}",
                     fodselsnummer: "${overstyring.fodselsnummer}",
-                    arbeidsgivere: ${
-                overstyring.arbeidsgivere.map {
-                    """
-                                            {
-                                                forklaring: "${it.forklaring}",
-                                                begrunnelse: "${it.begrunnelse}",
-                                                organisasjonsnummer: "${it.organisasjonsnummer}",
-                                                fraManedligInntekt: ${it.fraManedligInntekt},
-                                                manedligInntekt: ${it.manedligInntekt},
-                                                lovhjemmel: {bokstav: "A", ledd: "Albue", lovverk: "Norske lover", lovverksversjon: "1803", paragraf: "8-30"},
-                                                refusjonsopplysninger: ${
-                        it.refusjonsopplysninger?.map { opplysning ->
-                            """
-                                                            {
-                                                            belop: ${opplysning.belop},
-                                                            fom: "${opplysning.fom}",
-                                                            tom: ${opplysning.tom?.let { """"$it"""" }}
-                                                            }
-                                                        """
-                        }
-                    },
-                                                fraRefusjonsopplysninger: ${
-                        it.fraRefusjonsopplysninger?.map { opplysning ->
-                            """
-                                                            {
-                                                            belop: ${opplysning.belop},
-                                                            fom: "${opplysning.fom}",
-                                                            tom: ${opplysning.tom?.let { """"$it"""" }}
-                                                            }
-                                                        """
-                        }
-                    }
-                                            }
-                                        """
-                }
-            },
+                    arbeidsgivere: ${overstyring.arbeidsgivere.map {""" {
+                            forklaring: "${it.forklaring}",
+                            begrunnelse: "${it.begrunnelse}",
+                            organisasjonsnummer: "${it.organisasjonsnummer}",
+                            fraManedligInntekt: ${it.fraManedligInntekt},
+                            manedligInntekt: ${it.manedligInntekt},
+                            lovhjemmel: {bokstav: "A", ledd: "Albue", lovverk: "Norske lover", lovverksversjon: "1803", paragraf: "8-30"},
+                            refusjonsopplysninger: ${it.refusjonsopplysninger?.map { opplysning -> """ {
+                                belop: ${opplysning.belop},
+                                fom: "${opplysning.fom}",
+                                tom: ${opplysning.tom?.let { tom -> """"$tom"""" }}
+                            } """ }},
+                            fraRefusjonsopplysninger: ${it.fraRefusjonsopplysninger?.map { opplysning -> """ {
+                               belop: ${opplysning.belop},
+                               fom: "${opplysning.fom}",
+                               tom: ${opplysning.tom?.let { tom -> """"$tom"""" }}
+                            } """ }}
+                    } """ }},
                     vedtaksperiodeId: "${overstyring.vedtaksperiodeId}"
-                }
-            )
-        }
-        """,
+                })
+            }
+            """,
         )
 }
