1807bef49e6259f138c4761a0ac249f92ad82ebc
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-01-06 21:24:24 +0100

    🔥 Fjern miljøvariabel AUTOMATISER_SPESIALSAK, inkludert toggle

--- spesialist-modell/src/main/kotlin/no/nav/helse/modell/Toggle.kt
+++ spesialist-modell/src/main/kotlin/no/nav/helse/modell/Toggle.kt
@@ -4,6 +4,4 @@ abstract class Toggle(private var _enabled: Boolean) {
     private constructor(key: String, default: Boolean = false) : this(System.getenv()[key]?.toBoolean() ?: default)
 
     val enabled get() = _enabled
-
-    object AutomatiserSpesialsak : Toggle("AUTOMATISER_SPESIALSAK", true)
 }
