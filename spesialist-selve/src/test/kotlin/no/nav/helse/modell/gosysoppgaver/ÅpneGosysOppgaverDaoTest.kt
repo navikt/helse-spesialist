34685dbb09cf6099c3541d4961844726e4be2c40
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-01-10 18:01:39 +0100

    🩹 Riktig import for assertNotNull

--- spesialist-selve/src/test/kotlin/no/nav/helse/modell/gosysoppgaver/ÅpneGosysOppgaverDaoTest.kt
+++ spesialist-selve/src/test/kotlin/no/nav/helse/modell/gosysoppgaver/ÅpneGosysOppgaverDaoTest.kt
@@ -1,8 +1,8 @@
 package no.nav.helse.modell.gosysoppgaver
 
 import DatabaseIntegrationTest
-import graphql.Assert.assertNotNull
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
