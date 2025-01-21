f4b4f60f47c51fb165d4f12936f046d18e60009b
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-01-21 10:45:56 +0100

    ♻️ Ta bort avhengighet til Rapids & Rivers i selve
    Co-authored-by: Øydis Kind Refsum <oydis.kind.refsum@nav.no>

--- spesialist-selve/build.gradle.kts
+++ spesialist-selve/build.gradle.kts
@@ -1,5 +1,8 @@
 val graphQLKotlinVersion = "8.2.1"
 
+val micrometerRegistryPrometheusVersion = "1.13.6"
+val logbackEncoderVersion = "8.0"
+
 plugins {
     kotlin("plugin.serialization") version "2.0.20"
     id("com.expediagroup.graphql") version "8.2.1"
@@ -7,14 +10,17 @@ plugins {
 
 dependencies {
     api("com.nimbusds:nimbus-jose-jwt:9.37.3")
-    implementation(libs.rapids.and.rivers)
     api(libs.bundles.db)
     api(project(":spesialist-felles"))
     api(project(":spesialist-api"))
     api(project(":spesialist-modell"))
 
+    implementation("io.micrometer:micrometer-registry-prometheus:$micrometerRegistryPrometheusVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("net.logstash.logback:logstash-logback-encoder:$logbackEncoderVersion")
+
     testImplementation(testFixtures(project(":spesialist-felles")))
-    testImplementation(libs.rapids.and.rivers.test)
     testImplementation(libs.bundles.ktor.server)
     testImplementation("com.expediagroup:graphql-kotlin-ktor-server:$graphQLKotlinVersion")
     testImplementation(libs.testcontainers.postgresql)
