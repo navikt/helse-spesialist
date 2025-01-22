8231c7f4e5faa30cc1cb53af8ac02f8b5cbb6004
Martin Remøy Solheim <martin.solheim@me.com>
2025-01-22 13:32:31 +0100

    🔥 Fjern ubrukt mutation
    Bruker kun V2 i speil
    
    Co-authored-by: Trine Linderud <trinelinderuds@gmail.com>
    Co-authored-by: Christian Skovborg Gule <christian.skovborg.gule@nav.no>

--- spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/mutation/TotrinnsvurderingMutationTest.kt
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/mutation/TotrinnsvurderingMutationTest.kt
@@ -8,23 +8,6 @@ import org.junit.jupiter.api.Test
 internal class TotrinnsvurderingMutationTest : AbstractGraphQLApiTest() {
 
     @Test
-    fun `send oppgave til godkjenning`() {
-        opprettSaksbehandler()
-        opprettVedtaksperiode(opprettPerson(), opprettArbeidsgiver())
-        val oppgaveRef = finnOppgaveIdFor(PERIODE.id)
-
-        val body = runQuery(
-            """
-            mutation TotrinnsvurderingMutation {
-                sendTilGodkjenning(oppgavereferanse: "$oppgaveRef", avslag: null)
-            }
-        """
-        )
-
-        assertTrue(body["data"]["sendTilGodkjenning"].asBoolean())
-    }
-
-    @Test
     fun `send oppgave til godkjenning med V2`() {
         opprettSaksbehandler()
         opprettVedtaksperiode(opprettPerson(), opprettArbeidsgiver())
