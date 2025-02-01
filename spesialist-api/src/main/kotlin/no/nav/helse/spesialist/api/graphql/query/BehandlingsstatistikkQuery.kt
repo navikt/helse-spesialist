c1ba7c69c871fc9cfb4bb73ca4df3cf4e0735c28
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-02-01 09:06:42 +0100

    ♻️ Gjør statisk mapping til ApiBehandlingsstatistikk i Query

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/query/BehandlingsstatistikkQuery.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/query/BehandlingsstatistikkQuery.kt
@@ -2,14 +2,45 @@ package no.nav.helse.spesialist.api.graphql.query
 
 import com.expediagroup.graphql.server.operations.Query
 import graphql.execution.DataFetcherResult
+import no.nav.helse.spesialist.api.behandlingsstatistikk.BehandlingsstatistikkResponse
 import no.nav.helse.spesialist.api.behandlingsstatistikk.IBehandlingsstatistikkService
+import no.nav.helse.spesialist.api.behandlingsstatistikk.Statistikk
+import no.nav.helse.spesialist.api.graphql.schema.ApiAntall
 import no.nav.helse.spesialist.api.graphql.schema.ApiBehandlingsstatistikk
 
 class BehandlingsstatistikkQuery(private val behandlingsstatistikkMediator: IBehandlingsstatistikkService) : Query {
     @Suppress("unused")
-    fun behandlingsstatistikk(): DataFetcherResult<ApiBehandlingsstatistikk> {
-        val statistikk = ApiBehandlingsstatistikk(behandlingsstatistikkMediator.getBehandlingsstatistikk())
-        return DataFetcherResult.newResult<ApiBehandlingsstatistikk>()
-            .data(statistikk).build()
-    }
+    fun behandlingsstatistikk(): DataFetcherResult<ApiBehandlingsstatistikk> =
+        behandlingsstatistikkMediator.getBehandlingsstatistikk()
+            .tilApiBehandlingsstatistikk()
+            .tilDataFetcherResult()
+
+    private fun BehandlingsstatistikkResponse.tilApiBehandlingsstatistikk() =
+        ApiBehandlingsstatistikk(
+            enArbeidsgiver = enArbeidsgiver.tilApiAntall(),
+            flereArbeidsgivere = flereArbeidsgivere.tilApiAntall(),
+            forstegangsbehandling = forstegangsbehandling.tilApiAntall(),
+            forlengelser = forlengelser.tilApiAntall(),
+            forlengelseIt = forlengelseIt.tilApiAntall(),
+            utbetalingTilArbeidsgiver = utbetalingTilArbeidsgiver.tilApiAntall(),
+            utbetalingTilSykmeldt = utbetalingTilSykmeldt.tilApiAntall(),
+            faresignaler = faresignaler.tilApiAntall(),
+            fortroligAdresse = fortroligAdresse.tilApiAntall(),
+            stikkprover = stikkprover.tilApiAntall(),
+            revurdering = revurdering.tilApiAntall(),
+            delvisRefusjon = delvisRefusjon.tilApiAntall(),
+            beslutter = beslutter.tilApiAntall(),
+            egenAnsatt = egenAnsatt.tilApiAntall(),
+            antallAnnulleringer = antallAnnulleringer,
+            antallAvvisninger = antallAvvisninger,
+        )
+
+    private fun Statistikk.tilApiAntall() =
+        ApiAntall(
+            automatisk = automatisk,
+            manuelt = manuelt,
+            tilgjengelig = tilgjengelig,
+        )
+
+    private inline fun <reified T> T.tilDataFetcherResult(): DataFetcherResult<T> = DataFetcherResult.newResult<T>().data(this).build()
 }
--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/schema/ApiBehandlingsstatistikk.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/schema/ApiBehandlingsstatistikk.kt
@@ -1,8 +1,6 @@
 package no.nav.helse.spesialist.api.graphql.schema
 
 import com.expediagroup.graphql.generator.annotations.GraphQLName
-import no.nav.helse.spesialist.api.behandlingsstatistikk.BehandlingsstatistikkResponse
-import no.nav.helse.spesialist.api.behandlingsstatistikk.Statistikk
 
 @GraphQLName("Antall")
 data class ApiAntall(
@@ -13,44 +11,20 @@ data class ApiAntall(
 
 @GraphQLName("Behandlingsstatistikk")
 data class ApiBehandlingsstatistikk(
-    private val behandlingsstatistikkResponse: BehandlingsstatistikkResponse,
-) {
-    fun enArbeidsgiver(): ApiAntall = behandlingsstatistikkResponse.enArbeidsgiver.tilAntall()
-
-    fun flereArbeidsgivere(): ApiAntall = behandlingsstatistikkResponse.flereArbeidsgivere.tilAntall()
-
-    fun forstegangsbehandling(): ApiAntall = behandlingsstatistikkResponse.forstegangsbehandling.tilAntall()
-
-    fun forlengelser(): ApiAntall = behandlingsstatistikkResponse.forlengelser.tilAntall()
-
-    fun forlengelseIt(): ApiAntall = behandlingsstatistikkResponse.forlengelseIt.tilAntall()
-
-    fun utbetalingTilArbeidsgiver(): ApiAntall = behandlingsstatistikkResponse.utbetalingTilArbeidsgiver.tilAntall()
-
-    fun utbetalingTilSykmeldt(): ApiAntall = behandlingsstatistikkResponse.utbetalingTilSykmeldt.tilAntall()
-
-    fun faresignaler(): ApiAntall = behandlingsstatistikkResponse.faresignaler.tilAntall()
-
-    fun fortroligAdresse(): ApiAntall = behandlingsstatistikkResponse.fortroligAdresse.tilAntall()
-
-    fun stikkprover(): ApiAntall = behandlingsstatistikkResponse.stikkprover.tilAntall()
-
-    fun revurdering(): ApiAntall = behandlingsstatistikkResponse.revurdering.tilAntall()
-
-    fun delvisRefusjon(): ApiAntall = behandlingsstatistikkResponse.delvisRefusjon.tilAntall()
-
-    fun beslutter(): ApiAntall = behandlingsstatistikkResponse.beslutter.tilAntall()
-
-    fun egenAnsatt(): ApiAntall = behandlingsstatistikkResponse.egenAnsatt.tilAntall()
-
-    fun antallAnnulleringer(): Int = behandlingsstatistikkResponse.antallAnnulleringer
-
-    fun antallAvvisninger(): Int = behandlingsstatistikkResponse.antallAvvisninger
-}
-
-private fun Statistikk.tilAntall(): ApiAntall =
-    ApiAntall(
-        automatisk = automatisk,
-        manuelt = manuelt,
-        tilgjengelig = tilgjengelig,
-    )
+    val enArbeidsgiver: ApiAntall,
+    val flereArbeidsgivere: ApiAntall,
+    val forstegangsbehandling: ApiAntall,
+    val forlengelser: ApiAntall,
+    val forlengelseIt: ApiAntall,
+    val utbetalingTilArbeidsgiver: ApiAntall,
+    val utbetalingTilSykmeldt: ApiAntall,
+    val faresignaler: ApiAntall,
+    val fortroligAdresse: ApiAntall,
+    val stikkprover: ApiAntall,
+    val revurdering: ApiAntall,
+    val delvisRefusjon: ApiAntall,
+    val beslutter: ApiAntall,
+    val egenAnsatt: ApiAntall,
+    val antallAnnulleringer: Int,
+    val antallAvvisninger: Int,
+)
