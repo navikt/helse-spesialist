dac6c4d5a579bcbe052c5727b54619124f369063
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-02-06 14:15:47 +0100

    ⚗️ Er det deserialisering av responsen som gjør at fetchPerson feiler?
    Det lar seg ikke gjøre å hente enkelte personer til speil, typisk hvis
    de har en del data. Det er vanskelig å se hvor det stopper opp, la oss
    se om det henger sammen med loggingen.
    
    Burde finne en måte å lete etter errors i responsen som ikke innebærer
    å gjøre om en hel respons til en string... Person-responsen kan være på
    mange megabyte.
    
    Co-authored-by: Eirik Gallefoss <eirik.gallefoss@nav.no>

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/GraphQLCallLogging.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/GraphQLCallLogging.kt
@@ -1,19 +1,9 @@
 package no.nav.helse.spesialist.api
 
-import com.expediagroup.graphql.server.types.GraphQLBatchResponse
 import com.expediagroup.graphql.server.types.GraphQLRequest
-import com.expediagroup.graphql.server.types.GraphQLResponse
 import com.expediagroup.graphql.server.types.GraphQLServerRequest
-import com.expediagroup.graphql.server.types.GraphQLServerResponse
-import io.ktor.http.content.OutgoingContent
-import io.ktor.http.content.OutputStreamContent
-import io.ktor.http.content.TextContent
 import io.ktor.server.application.createRouteScopedPlugin
-import io.ktor.server.application.hooks.ResponseBodyReadyForSend
 import io.ktor.server.request.receive
-import io.ktor.utils.io.ByteChannel
-import io.ktor.utils.io.readUTF8LineTo
-import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 
 private val sikkerLogg = LoggerFactory.getLogger("tjenestekall")
@@ -23,9 +13,6 @@ val GraphQLCallLogging =
         onCall { call ->
             val graphQLRequest = call.receive<GraphQLServerRequest>()
             logRequest(graphQLRequest)
-        }
-        on(ResponseBodyReadyForSend) { _, body ->
-            logResponse(objectMapper.readValue(responseAsString(body), GraphQLResponse::class.java))
         }
     }
 
@@ -43,32 +30,3 @@ fun logRequest(graphQLRequest: GraphQLServerRequest) {
         }
     }
 }
-
-fun logResponse(response: GraphQLServerResponse) {
-    when (response) {
-        is GraphQLResponse<*> -> {
-            val errors = response.errors
-            if (!errors.isNullOrEmpty()) sikkerLogg.warn("GraphQL-respons inneholder feil: ${errors.joinToString()}")
-        }
-        // Vi bruker ikke batch-operasjoner per nå
-        is GraphQLBatchResponse -> Unit
-    }
-}
-
-private fun responseAsString(body: OutgoingContent) =
-    when (body) {
-        is OutputStreamContent -> {
-            val channel = ByteChannel(true)
-            runBlocking {
-                body.writeTo(channel)
-                val buffer = StringBuilder()
-                while (!channel.isClosedForRead) {
-                    channel.readUTF8LineTo(buffer)
-                }
-                buffer.toString()
-            }
-        }
-        is TextContent -> body.text
-
-        else -> "reponstekst ikke tilgjengelig"
-    }
