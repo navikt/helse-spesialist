9644183f29127b5e9c3249f5003745fc925e984a
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-02-11 17:07:00 +0100

    ✅ Lag test for KRR-klienten med Wiremock

--- spesialist-client-krr/build.gradle.kts
+++ spesialist-client-krr/build.gradle.kts
@@ -4,4 +4,6 @@ dependencies {
     implementation(libs.bundles.ktor.client)
     implementation(libs.jackson.datatype)
     implementation(libs.micrometer.prometheus)
+
+    testImplementation("org.wiremock:wiremock:3.11.0")
 }
---
+++ spesialist-client-krr/src/test/kotlin/no/nav/helse/spesialist/client/krr/KRRClientReservasjonshenterTest.kt
@@ -0,0 +1,58 @@
+package no.nav.helse.spesialist.client.krr
+
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMock.okJson
+import com.github.tomakehurst.wiremock.client.WireMock.stubFor
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo
+import com.github.tomakehurst.wiremock.junit5.WireMockTest
+import kotlinx.coroutines.runBlocking
+import no.nav.helse.spesialist.application.AccessTokenGenerator
+import no.nav.helse.spesialist.application.Reservasjonshenter.ReservasjonDto
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@WireMockTest
+class KRRClientReservasjonshenterTest {
+    @Test
+    fun `mapper svar som forventet`(wmRuntimeInfo: WireMockRuntimeInfo) {
+        test(
+            givenResponse = okJson("""{ "kanVarsles": false, "reservert": true }"""),
+            expectedReservasjon = ReservasjonDto(kanVarsles = false, reservert = true),
+            wmRuntimeInfo = wmRuntimeInfo
+        )
+    }
+
+    @Test
+    fun `tåler svar med ekstra felter`(wmRuntimeInfo: WireMockRuntimeInfo) {
+        test(
+            givenResponse = okJson("""{ "something-else": "?", "kanVarsles": false, "x": "YZ", "reservert": true }"""),
+            expectedReservasjon = ReservasjonDto(kanVarsles = false, reservert = true),
+            wmRuntimeInfo = wmRuntimeInfo
+        )
+    }
+
+    private fun test(
+        givenResponse: ResponseDefinitionBuilder?,
+        expectedReservasjon: ReservasjonDto,
+        wmRuntimeInfo: WireMockRuntimeInfo
+    ) {
+        // Given:
+        stubFor(get("/rest/v1/person").willReturn(givenResponse))
+        val client = KRRClientReservasjonshenter(
+            apiUrl = wmRuntimeInfo.httpBaseUrl,
+            scope = "scoap",
+            accessTokenGenerator = object : AccessTokenGenerator {
+                override suspend fun hentAccessToken(scope: String) = "gief axess plz"
+            }
+        )
+
+        // When:
+        val actualReservasjon: ReservasjonDto? = runBlocking {
+            client.hentForPerson("12345678910")
+        }
+
+        // Then:
+        assertEquals(expectedReservasjon, actualReservasjon)
+    }
+}
