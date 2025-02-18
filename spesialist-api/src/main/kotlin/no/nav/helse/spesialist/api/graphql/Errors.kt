796724603c6f018c38207d5a0056102e67bd60eb
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-02-18 16:26:36 +0100

    ♻️ Trekk ut feilhåndtering i VarselMutationHandler til felles bruk

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/Errors.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/Errors.kt
@@ -2,6 +2,9 @@ package no.nav.helse.spesialist.api.graphql
 
 import graphql.GraphQLError
 import graphql.GraphqlErrorException
+import graphql.execution.DataFetcherResult
+import graphql.execution.DataFetcherResult.newResult
+import no.nav.helse.spesialist.application.logg.sikkerlogg
 
 internal fun forbiddenError(fødselsnummer: String): GraphQLError =
     GraphqlErrorException.newErrorException()
@@ -23,3 +26,24 @@ internal fun personNotReadyError(
         .message("Person med fødselsnummer $fødselsnummer er ikke klar for visning ennå")
         .extensions(mapOf("code" to 409, "field" to "person", "persondata_hentes_for" to aktørId))
         .build()
+
+internal fun <T> notFound(message: String) = dataFetcherError<T>(404, message)
+
+internal fun <T> conflict(message: String) = dataFetcherError<T>(409, message)
+
+internal fun <T> internalServerError(message: String) = dataFetcherError<T>(500, message)
+
+private fun <T> dataFetcherError(
+    httpCode: Int,
+    message: String,
+): DataFetcherResult<T> =
+    newResult<T>()
+        .error(
+            GraphqlErrorException.newErrorException()
+                .message(message)
+                .extensions(mapOf("code" to httpCode))
+                .build(),
+        ).build()
+        .also {
+            sikkerlogg.error("Returnerer $httpCode-feil for GraphQL-operasjon: $message")
+        }
--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/mutation/VarselMutationHandler.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/graphql/mutation/VarselMutationHandler.kt
@@ -1,20 +1,16 @@
 package no.nav.helse.spesialist.api.graphql.mutation
 
-import graphql.GraphQLError
-import graphql.GraphqlErrorException.newErrorException
 import graphql.execution.DataFetcherResult
 import graphql.execution.DataFetcherResult.newResult
 import no.nav.helse.db.api.VarselApiRepository
+import no.nav.helse.spesialist.api.graphql.conflict
+import no.nav.helse.spesialist.api.graphql.internalServerError
 import no.nav.helse.spesialist.api.graphql.mapping.toVarselDto
+import no.nav.helse.spesialist.api.graphql.notFound
 import no.nav.helse.spesialist.api.graphql.schema.ApiVarselDTO
-import org.slf4j.LoggerFactory
 import java.util.UUID
 
 class VarselMutationHandler(private val varselRepository: VarselApiRepository) : VarselMutationSchema {
-    private companion object {
-        private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
-    }
-
     override fun settVarselstatus(
         generasjonIdString: String,
         varselkode: String,
@@ -25,85 +21,28 @@ class VarselMutationHandler(private val varselRepository: VarselApiRepository) :
 
         return if (definisjonIdString != null) {
             when (varselRepository.erAktiv(varselkode, generasjonId)) {
-                false -> varselError(getNotActiveError(varselkode, generasjonId))
-                null -> varselError(getNotFoundError(varselkode, generasjonId))
+                false -> return conflict("Varsel med varselkode=$varselkode, generasjonId=$generasjonId har ikke status AKTIV")
+                null -> return notFound("Kunne ikke oppdatere varsel med varselkode=$varselkode, generasjonId=$generasjonId fordi varselet ikke finnes")
                 true ->
                     varselRepository.settStatusVurdert(
                         generasjonId = generasjonId,
                         definisjonId = UUID.fromString(definisjonIdString),
                         varselkode = varselkode,
                         ident = ident,
-                    )?.toVarselDto().graphQlResult(varselkode, generasjonId)
+                    )
             }
         } else {
             when (varselRepository.erGodkjent(varselkode, generasjonId)) {
-                true -> varselError(getIsGodkjentError(varselkode, generasjonId))
-                null -> varselError(getNotFoundError(varselkode, generasjonId))
+                true -> return conflict("Varsel med varselkode=$varselkode, generasjonId=$generasjonId har status GODKJENT")
+                null -> return notFound("Kunne ikke oppdatere varsel med varselkode=$varselkode, generasjonId=$generasjonId fordi varselet ikke finnes")
                 false ->
                     varselRepository.settStatusAktiv(
                         generasjonId = generasjonId,
                         varselkode = varselkode,
                         ident = ident,
-                    )?.toVarselDto().graphQlResult(varselkode, generasjonId)
+                    )
             }
-        }
-    }
-
-    private fun varselError(error: GraphQLError): DataFetcherResult<ApiVarselDTO?> = newResult<ApiVarselDTO>().error(error).build()
-
-    private fun ApiVarselDTO?.graphQlResult(
-        varselkode: String,
-        generasjonId: UUID,
-    ): DataFetcherResult<ApiVarselDTO?> =
-        this?.let { newResult<ApiVarselDTO>().data(it).build() }
-            ?: varselError(getUpdateError(varselkode, generasjonId))
-
-    private fun getUpdateError(
-        varselkode: String,
-        generasjonId: UUID,
-    ): GraphQLError {
-        val message = "Kunne ikke oppdatere varsel med varselkode=$varselkode, generasjonId=$generasjonId"
-        sikkerlogg.error(message)
-        return newErrorException()
-            .message(message)
-            .extensions(mapOf("code" to 500))
-            .build()
-    }
-
-    private fun getNotFoundError(
-        varselkode: String,
-        generasjonId: UUID,
-    ): GraphQLError {
-        val message =
-            "Kunne ikke oppdatere varsel med varselkode=$varselkode, generasjonId=$generasjonId fordi varselet ikke finnes"
-        sikkerlogg.error(message)
-        return newErrorException()
-            .message(message)
-            .extensions(mapOf("code" to 404))
-            .build()
-    }
-
-    private fun getNotActiveError(
-        varselkode: String,
-        generasjonId: UUID,
-    ): GraphQLError {
-        val message = "Varsel med varselkode=$varselkode, generasjonId=$generasjonId har ikke status AKTIV"
-        sikkerlogg.error(message)
-        return newErrorException()
-            .message(message)
-            .extensions(mapOf("code" to 409))
-            .build()
-    }
-
-    private fun getIsGodkjentError(
-        varselkode: String,
-        generasjonId: UUID,
-    ): GraphQLError {
-        val message = "Varsel med varselkode=$varselkode, generasjonId=$generasjonId har status GODKJENT"
-        sikkerlogg.error(message)
-        return newErrorException()
-            .message(message)
-            .extensions(mapOf("code" to 409))
-            .build()
+        }?.toVarselDto()?.let { newResult<ApiVarselDTO>().data(it).build() }
+            ?: internalServerError("Kunne ikke oppdatere varsel med varselkode=$varselkode, generasjonId=$generasjonId")
     }
 }
