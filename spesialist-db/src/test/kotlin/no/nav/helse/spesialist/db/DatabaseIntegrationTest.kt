3dea4a33ed178967236537196be1268e92760cb1
Trine Linderud <trinelinderuds@gmail.com>
2025-02-18 15:13:56 +0100

    ♻️ Slår sammen AbstractDatabaseTest og DatabaseIntegrationTest
    Co-authored-by: Svein Elgstøen <189128153+navelgen@users.noreply.github.com>

--- spesialist-db/src/test/kotlin/no/nav/helse/spesialist/db/DatabaseIntegrationTest.kt
+++ spesialist-db/src/test/kotlin/no/nav/helse/spesialist/db/DatabaseIntegrationTest.kt
@@ -3,7 +3,11 @@ package no.nav.helse.spesialist.db
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import kotliquery.queryOf
 import kotliquery.sessionOf
+import no.nav.helse.db.DBRepositories
 import no.nav.helse.db.DBSessionContext
 import no.nav.helse.db.DbQuery
 import no.nav.helse.db.EgenskapForDatabase
@@ -36,15 +40,20 @@ import no.nav.helse.modell.vedtaksperiode.Periodetype.FØRSTEGANGSBEHANDLING
 import no.nav.helse.spesialist.api.overstyring.Dagtype
 import no.nav.helse.spesialist.modell.Dialog
 import no.nav.helse.spesialist.typer.Kjønn
+import org.flywaydb.core.Flyway
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.AfterEach
+import org.testcontainers.containers.PostgreSQLContainer
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.Random
 import java.util.UUID
+import javax.sql.DataSource
 import kotlin.random.Random.Default.nextLong
 
-abstract class DatabaseIntegrationTest : AbstractDatabaseTest() {
+abstract class DatabaseIntegrationTest {
     protected val dbQuery = DbQuery(dataSource)
+    protected val repositories = DBRepositories(dataSource) { _, _ -> false }
     private val testperson = TestPerson()
     protected open val HENDELSE_ID: UUID = UUID.randomUUID()
 
@@ -99,6 +108,33 @@ abstract class DatabaseIntegrationTest : AbstractDatabaseTest() {
             jacksonObjectMapper()
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .registerModule(JavaTimeModule())
+
+        private val postgres = PostgreSQLContainer<Nothing>("postgres:14").apply {
+            withReuse(true)
+            withLabel("app-navn", "spesialist-db")
+            start()
+            println("Database: jdbc:postgresql://localhost:$firstMappedPort/test startet opp, credentials: test og test")
+        }
+
+        val dataSource =
+            HikariDataSource(HikariConfig().apply {
+                jdbcUrl = postgres.jdbcUrl
+                username = postgres.username
+                password = postgres.password
+                maximumPoolSize = 100
+                connectionTimeout = 1500
+                initializationFailTimeout = 5000
+            })
+
+        init {
+            Flyway.configure()
+                .dataSource(dataSource)
+                .placeholders(mapOf("spesialist_oid" to UUID.randomUUID().toString()))
+                .load()
+                .migrate()
+
+            resetDatabase(dataSource)
+        }
     }
 
     private var personId: Long = -1
@@ -661,4 +697,28 @@ abstract class DatabaseIntegrationTest : AbstractDatabaseTest() {
         val prosent: Int,
     )
 
+}
+
+private fun resetDatabase(dataSource: DataSource) {
+    sessionOf(dataSource).use {
+        @Language("PostgreSQL")
+        val query = """
+            CREATE OR REPLACE FUNCTION truncate_tables() RETURNS void AS $$
+            DECLARE
+            truncate_statement text;
+            BEGIN
+                SELECT 'TRUNCATE ' || string_agg(format('%I.%I', schemaname, tablename), ',') || ' RESTART IDENTITY CASCADE'
+                    INTO truncate_statement
+                FROM pg_tables
+                WHERE schemaname='public'
+                AND tablename not in ('enhet', 'flyway_schema_history', 'global_snapshot_versjon');
+                UPDATE global_snapshot_versjon SET versjon = 0 WHERE id = 1;
+
+                EXECUTE truncate_statement;
+            END;
+            $$ LANGUAGE plpgsql;
+        """.trimIndent()
+        it.run(queryOf(query).asExecute)
+        it.run(queryOf("SELECT truncate_tables()").asExecute)
+    }
 }
