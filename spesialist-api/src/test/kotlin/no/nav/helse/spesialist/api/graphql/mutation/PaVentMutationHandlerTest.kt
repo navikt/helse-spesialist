a5624485dc1071ee83a1d9d202d7076c5b7ccbbe
Christian Skovborg Gule <christianskovborg@gmail.com>
2025-03-18 12:06:52 +0100

    🚚 Flytt PaVentMutationHandlerTest til api

---
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/mutation/PaVentMutationHandlerTest.kt
@@ -0,0 +1,55 @@
+package no.nav.helse.spesialist.api.graphql.mutation
+
+import no.nav.helse.TestRunner.runQuery
+import no.nav.helse.spesialist.api.testfixtures.lagSaksbehandlerFraApi
+import no.nav.helse.spesialist.api.testfixtures.mutation.fjernPåVentMutation
+import no.nav.helse.spesialist.api.testfixtures.mutation.leggPåVentMutation
+import no.nav.helse.spesialist.testfixtures.jan
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import kotlin.random.Random.Default.nextLong
+
+internal class PaVentMutationHandlerTest {
+
+    @Test
+    fun `legger på vent`() {
+        val saksbehandler = lagSaksbehandlerFraApi()
+        runQuery(
+            saksbehandlerFraApi = saksbehandler,
+            whenever = leggPåVentMutation(nextLong(), """Dette er et notat""", 1 jan 2024, true),
+            then = { _, body, _ ->
+                assertEquals(saksbehandler.oid.toString(), body["data"]["leggPaVent"]["oid"].asText())
+            }
+        )
+    }
+
+    @Test
+    fun `legger på vent med årsaker`() {
+        val saksbehandler = lagSaksbehandlerFraApi()
+
+        runQuery(
+            saksbehandlerFraApi = saksbehandler,
+            whenever = leggPåVentMutation(
+                nextLong(),
+                "Dette er et notat",
+                1 jan 2024,
+                true,
+                arsaker = mapOf("_key" to "en_key", "arsak" to "en_arsak")
+            ),
+            then = { _, body, _ ->
+                assertEquals(saksbehandler.oid.toString(), body["data"]["leggPaVent"]["oid"].asText())
+            }
+        )
+    }
+
+    @Test
+    fun `fjern fra på vent`() {
+        runQuery(
+            whenever = fjernPåVentMutation(nextLong()),
+            then = { _, body, _ ->
+                assertTrue(body["data"]["fjernPaVent"].asBoolean())
+            }
+        )
+    }
+}
