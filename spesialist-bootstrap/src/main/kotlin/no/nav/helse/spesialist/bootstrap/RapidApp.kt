d601870a902034332573cfccaae4ef4c08e8e4aa
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-03-18 14:32:49 +0100

    ♻️ Trekk ut Configuration i egen fil

--- spesialist-bootstrap/src/main/kotlin/no/nav/helse/spesialist/bootstrap/RapidApp.kt
+++ spesialist-bootstrap/src/main/kotlin/no/nav/helse/spesialist/bootstrap/RapidApp.kt
@@ -4,19 +4,13 @@ import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import io.micrometer.core.instrument.Metrics
 import io.micrometer.prometheusmetrics.PrometheusConfig
 import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
-import no.nav.helse.bootstrap.EnvironmentToggles
-import no.nav.helse.modell.automatisering.Stikkprøver
 import no.nav.helse.rapids_rivers.RapidApplication
-import no.nav.helse.spesialist.api.AzureConfig
-import no.nav.helse.spesialist.api.bootstrap.SpeilTilgangsgrupper
-import no.nav.helse.spesialist.api.bootstrap.Tilgangsgrupper
 import no.nav.helse.spesialist.application.Reservasjonshenter
 import no.nav.helse.spesialist.client.entraid.EntraIDAccessTokenGenerator
 import no.nav.helse.spesialist.client.entraid.MsGraphGruppekontroll
 import no.nav.helse.spesialist.client.krr.KRRClientReservasjonshenter
 import no.nav.helse.spesialist.client.spleis.SpleisClient
 import no.nav.helse.spesialist.client.spleis.SpleisClientSnapshothenter
-import no.nav.helse.spesialist.db.bootstrap.DBModule
 import org.slf4j.LoggerFactory
 
 fun main() {
@@ -24,35 +18,6 @@ fun main() {
 }
 
 internal class RapidApp(env: Map<String, String>) {
-    data class Configuration(
-        val azureConfig: AzureConfig,
-        val accessTokenGeneratorConfig: EntraIDAccessTokenGenerator.Configuration,
-        val spleisClientConfig: SpleisClient.Configuration,
-        val krrConfig: KRRClientReservasjonshenter.Configuration,
-        val dbConfig: DBModule.Configuration,
-        val unleashFeatureToggles: UnleashFeatureToggles.Configuration,
-        val versjonAvKode: String,
-        val tilgangsgrupper: Tilgangsgrupper,
-        val environmentToggles: EnvironmentToggles,
-        val stikkprøver: Stikkprøver,
-    ) {
-        companion object {
-            fun fraEnv(env: Map<String, String>): Configuration =
-                Configuration(
-                    azureConfig = AzureConfig.fraEnv(env),
-                    accessTokenGeneratorConfig = EntraIDAccessTokenGenerator.Configuration.fraEnv(env),
-                    spleisClientConfig = SpleisClient.Configuration.fraEnv(env),
-                    krrConfig = KRRClientReservasjonshenter.Configuration.fraEnv(env),
-                    dbConfig = DBModule.Configuration.fraEnv(env),
-                    unleashFeatureToggles = UnleashFeatureToggles.Configuration.fraEnv(env),
-                    versjonAvKode = env.getValue("NAIS_APP_IMAGE"),
-                    tilgangsgrupper = SpeilTilgangsgrupper(env),
-                    environmentToggles = EnvironmentTogglesImpl(env),
-                    stikkprøver = Stikkprøver.fraEnv(env),
-                )
-        }
-    }
-
     private val logger = LoggerFactory.getLogger(RapidApp::class.java)
     private val rapidsConnection: RapidsConnection
     private val configuration = Configuration.fraEnv(env)
