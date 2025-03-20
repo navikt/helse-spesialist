bed9070379ed29bfb7861681bcca74f5049e5c8f
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-03-20 10:37:01 +0100

    🐛 Bare hent KRRClientReservasjonshenter config om krr ikke er skrudd av
    Co-authored-by: Svein Elgstøen <189128153+navelgen@users.noreply.github.com>

--- spesialist-bootstrap/src/main/kotlin/no/nav/helse/spesialist/bootstrap/EnvironmentTogglesImpl.kt
+++ spesialist-bootstrap/src/main/kotlin/no/nav/helse/spesialist/bootstrap/EnvironmentTogglesImpl.kt
@@ -3,7 +3,6 @@ package no.nav.helse.spesialist.bootstrap
 import no.nav.helse.bootstrap.EnvironmentToggles
 
 class EnvironmentTogglesImpl(env: Map<String, String>) : EnvironmentToggles {
-    override val brukDummyForKRR = env.containsKey("BRUK_DUMMY_FOR_KONTAKT_OG_RESERVASJONSREGISTERET")
     override val ignorerMeldingerForUkjentePersoner = env.containsKey("IGNORER_MELDINGER_FOR_UKJENTE_PERSONER")
     override val kanBeslutteEgneSaker = env.containsKey("TILLAT_GODKJENNING_AV_EGEN_SAK")
     override val kanGodkjenneUtenBesluttertilgang = env.containsKey("TILLAT_GODKJENNING_UTEN_BESLUTTERTILGANG")
