8435fb5273aba59c372ccdd2ac5b6f0cea7e0a3e
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-03-22 14:42:49 +0100

    ♻️ Rydd opp og inline en del, testen er nå grønn

--- spesialist-e2e-tests/src/test/kotlin/no/nav/helse/spesialist/e2etests/ManglerInntektsmeldingE2ETest.kt
+++ spesialist-e2e-tests/src/test/kotlin/no/nav/helse/spesialist/e2etests/ManglerInntektsmeldingE2ETest.kt
@@ -1,17 +1,39 @@
 package no.nav.helse.spesialist.e2etests
 
 import no.nav.helse.modell.oppgave.Egenskap
-import no.nav.helse.spesialist.test.TestPerson
 import org.junit.jupiter.api.Test
+import java.util.UUID
 
 class ManglerInntektsmeldingE2ETest : AbstractE2EIntegrationTest() {
     @Test
     fun `legger til egenskap på oppgaven når varsel RV_IV_10 er til stede`() {
-        val testPerson = TestPerson()
-        vedtaksløsningenMottarNySøknad(testPerson)
-        spleisOppretterNyBehandling(testPerson = testPerson)
-        spesialistBehandlerGodkjenningsbehovFremTilOppgave(regelverksvarsler = listOf("RV_IV_10"))
+        sendSøknadSendt()
+        assertIngenEtterspurteBehov()
+        assertPersonEksisterer()
+        val spleisBehandlingId = UUID.randomUUID()
+        sendBehandlingOpprettet(spleisBehandlingId)
+        assertIngenEtterspurteBehov()
+        assertArbeidsgiverEksisterer(testPerson.orgnummer)
+        assertVedtaksperiodeEksisterer(testPerson.vedtaksperiodeId1)
 
+        håndterAktivitetsloggNyAktivitet(varselkoder = listOf("RV_IV_10"))
+        håndterVedtaksperiodeNyUtbetaling()
+        håndterUtbetalingOpprettet()
+        håndterVedtaksperiodeEndret()
+        sendGodkjenningsbehov(spleisBehandlingId = spleisBehandlingId)
+        håndterAvviksvurderingløsning()
+        sendPersoninfoløsning()
+        sendEnhetløsning()
+        sendInfotrygdutbetalingerløsning()
+        håndterArbeidsgiverinformasjonløsning()
+        sendArbeidsforholdløsning()
+        sendEgenAnsattløsning()
+        sendVergemålOgFullmaktløsning()
+        sendÅpneGosysOppgaverløsning()
+        sendRisikovurderingløsning()
+        sendInntektløsning()
+
+        // Then:
         assertHarOppgaveegenskap(Egenskap.MANGLER_IM)
     }
 }
