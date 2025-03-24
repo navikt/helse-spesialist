ccebf5a6de319e5f7cf5f188b103dfb8e28508be
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-03-24 16:50:22 +0100

    🔥 Fjern flyttet test
    Co-authored-by: Svein Elgstøen <189128153+navelgen@users.noreply.github.com>

--- spesialist-e2e-tests/src/test/kotlin/no/nav/helse/e2e/GosysOppgaveEndretE2ETest.kt
+++ spesialist-e2e-tests/src/test/kotlin/no/nav/helse/e2e/GosysOppgaveEndretE2ETest.kt
@@ -1,6 +1,5 @@
 package no.nav.helse.e2e
 
-import no.nav.helse.spesialist.api.oppgave.Oppgavestatus
 import org.junit.jupiter.api.Test
 
 class GosysOppgaveEndretE2ETest : AbstractE2ETest() {
@@ -20,24 +19,5 @@ class GosysOppgaveEndretE2ETest : AbstractE2ETest() {
         spesialistInnvilgerAutomatisk()
         håndterGosysOppgaveEndret()
         assertIngenEtterspurteBehov()
-    }
-
-    @Test
-    fun `fatter vedtak automatisk ved åpen oppgave i Speil men ikke lenger åpen oppgave i Gosys`() {
-        vedtaksløsningenMottarNySøknad()
-        spleisOppretterNyBehandling()
-        spesialistBehandlerGodkjenningsbehovFremTilÅpneOppgaver()
-        håndterÅpneOppgaverløsning(
-            antallÅpneOppgaverIGosys = 1,
-        )
-        håndterRisikovurderingløsning()
-        assertSaksbehandleroppgave(oppgavestatus = Oppgavestatus.AvventerSaksbehandler)
-        håndterGosysOppgaveEndret()
-
-        håndterÅpneOppgaverløsning(
-            antallÅpneOppgaverIGosys = 0,
-        )
-        assertSaksbehandleroppgave(oppgavestatus = Oppgavestatus.Invalidert)
-        assertGodkjenningsbehovBesvart(godkjent = true, automatiskBehandlet = true)
     }
 }
