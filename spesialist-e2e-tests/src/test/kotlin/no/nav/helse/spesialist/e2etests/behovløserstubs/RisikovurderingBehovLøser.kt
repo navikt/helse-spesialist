9fdfbaf15fb2455d41e6051812544d58344cf21a
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-03-24 13:26:04 +0100

    ♻️ kanGodkjennesAutomatisk default true i stub for risikovurderingbehov
    Co-authored-by: Martin Remøy Solheim <3852471+martinmekk@users.noreply.github.com>

--- spesialist-e2e-tests/src/test/kotlin/no/nav/helse/spesialist/e2etests/behovløserstubs/RisikovurderingBehovLøser.kt
+++ spesialist-e2e-tests/src/test/kotlin/no/nav/helse/spesialist/e2etests/behovløserstubs/RisikovurderingBehovLøser.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 
 class RisikovurderingBehovLøser : AbstractBehovLøser("Risikovurdering") {
     override fun løsning(behovJson: JsonNode) = mapOf(
-        "kanGodkjennesAutomatisk" to false,
+        "kanGodkjennesAutomatisk" to true,
         "funn" to emptyList<Any>(),
         "kontrollertOk" to emptyList<Any>(),
     )
