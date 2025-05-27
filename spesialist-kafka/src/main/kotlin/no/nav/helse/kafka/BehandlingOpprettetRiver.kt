bd7dad4532c93bef9b860be5bde0e7afd8c9a09e
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-05-27 15:09:55 +0200

    🚩 Slipp gjennom SELVSTENDIG i BehandlingOpprettetRiver bak toggle
    Co-authored-by: Eirik Gallefoss <eirik.gallefoss@nav.no>

--- spesialist-kafka/src/main/kotlin/no/nav/helse/kafka/BehandlingOpprettetRiver.kt
+++ spesialist-kafka/src/main/kotlin/no/nav/helse/kafka/BehandlingOpprettetRiver.kt
@@ -6,12 +6,15 @@ import com.github.navikt.tbd_libs.rapids_and_rivers.asLocalDate
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
 import io.micrometer.core.instrument.MeterRegistry
+import no.nav.helse.FeatureToggles
 import no.nav.helse.mediator.MeldingMediator
 import no.nav.helse.mediator.asUUID
 import no.nav.helse.modell.vedtaksperiode.BehandlingOpprettet
+import no.nav.helse.spesialist.application.logg.logg
 
 class BehandlingOpprettetRiver(
     private val mediator: MeldingMediator,
+    private val featureToggles: FeatureToggles,
 ) : SpesialistRiver {
     override fun preconditions(): River.PacketValidation {
         return River.PacketValidation {
@@ -32,17 +35,30 @@ class BehandlingOpprettetRiver(
             it.requireKey("fom", "tom")
         }
 
+    fun behandlerIkke(organisasjonsnummer: String) {
+        logg.info("Tar ikke imot behandling opprettet for: $organisasjonsnummer")
+    }
+
     override fun onPacket(
         packet: JsonMessage,
         context: MessageContext,
         metadata: MessageMetadata,
         meterRegistry: MeterRegistry,
     ) {
+        val organisasjonsnummer = packet["organisasjonsnummer"].asText()
+        if (organisasjonsnummer in listOf("ARBEIDSLEDIG", "FRILANS")) {
+            behandlerIkke(organisasjonsnummer)
+            return
+        }
+        if (organisasjonsnummer == "SELVSTENDIG" && !featureToggles.skalBehandleSelvstendig()) {
+            behandlerIkke(organisasjonsnummer)
+            return
+        }
         mediator.mottaMelding(
             BehandlingOpprettet(
                 id = packet["@id"].asUUID(),
                 fødselsnummer = packet["fødselsnummer"].asText(),
-                organisasjonsnummer = packet["organisasjonsnummer"].asText(),
+                organisasjonsnummer = organisasjonsnummer,
                 vedtaksperiodeId = packet["vedtaksperiodeId"].asUUID(),
                 spleisBehandlingId = packet["behandlingId"].asUUID(),
                 fom = packet["fom"].asLocalDate(),
