27fa44cf10242e2c6c640261799694173a3831a9
Trine Linderud <trine.linderud@nav.no>
2025-06-10 15:35:57 +0200

    ⚗️ Logger ut et snapshot for en fiktiv person i dev
    Co-authored-by: Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
    Co-authored-by: Håvard Ola Eggen <11397871+gammalost@users.noreply.github.com>

--- spesialist-client-spleis/src/main/kotlin/no/nav/helse/spesialist/client/spleis/SpleisClient.kt
+++ spesialist-client-spleis/src/main/kotlin/no/nav/helse/spesialist/client/spleis/SpleisClient.kt
@@ -123,6 +123,7 @@ class SpleisClient(
         }
 
         val graphQLResponse = serializer.deserialize(responseBody, request.responseType())
+        if ("02458241409" in responseBody) sikkerlogg.error("Spleis: $responseBody")
 
         if (graphQLResponse.data == null && graphQLResponse.errors == null) {
             logg.error("GraphQL-svar fra Spleis manglet både data og feil. Se sikkerlogg for mer info.")
