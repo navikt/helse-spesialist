2d819bd21cbbb0bac25af68a9eb4c56d8776b7b1
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2025-06-17 21:26:42 +0200

    🎨 Whitespace-opprydding i OppgaverQueryHandlerTest

--- spesialist-e2e-tests/src/test/kotlin/no/nav/helse/spesialist/api/graphql/query/OppgaverQueryHandlerTest.kt
+++ spesialist-e2e-tests/src/test/kotlin/no/nav/helse/spesialist/api/graphql/query/OppgaverQueryHandlerTest.kt
@@ -34,9 +34,9 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
             apiOppgaveService.oppgaver(any(), any(), any(), any(), any())
         } returns ApiOppgaverTilBehandling(oppgaver = listOf(oppgaveTilBehandling()), totaltAntallOppgaver = 1)
 
-        val body =
-            runQuery(
-                """{
+        val body = runQuery(
+            """
+            {
                 oppgaveFeed(
                     offset: 0,
                     limit: 14,
@@ -48,8 +48,9 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
                         ingenUkategoriserteEgenskaper: false
                     }
                 ) { oppgaver { id } }
-            }""",
-            )
+            }
+            """.trimIndent(),
+        )
         val antallOppgaver = body["data"]["oppgaveFeed"].size()
 
         verify(exactly = 1) { apiOppgaveService.oppgaver(any(), 0, 14, any(), any()) }
@@ -62,11 +63,11 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
             apiOppgaveService.oppgaver(any(), any(), any(), any(), any())
         } returns ApiOppgaverTilBehandling(oppgaver = listOf(oppgaveTilBehandling()), totaltAntallOppgaver = 1)
 
-        val body =
-            runQuery(
-                """{ 
+        val body = runQuery(
+            """
+            {
                 oppgaveFeed(
-                    offset: 14, 
+                    offset: 14,
                     limit: 14,
                     sortering: [{nokkel: TILDELT_TIL, stigende: true}],
                     filtrering: {
@@ -75,9 +76,10 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
                         egneSakerPaVent: false
                         ingenUkategoriserteEgenskaper: false
                     }
-                )  { oppgaver { id } }
-        }""",
-            )
+                ) { oppgaver { id } }
+            }
+            """.trimIndent(),
+        )
         val antallOppgaver = body["data"]["oppgaveFeed"].size()
 
         verify(exactly = 1) {
@@ -86,11 +88,10 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
                 offset = 14,
                 limit = 14,
                 sortering = listOf(ApiOppgavesortering(ApiSorteringsnokkel.TILDELT_TIL, true)),
-                filtrering =
-                    ApiFiltrering(
-                        egenskaper = listOf(ApiOppgaveegenskap(ApiEgenskap.DELVIS_REFUSJON, ApiKategori.Mottaker)),
-                        egneSaker = true,
-                    ),
+                filtrering = ApiFiltrering(
+                    egenskaper = listOf(ApiOppgaveegenskap(ApiEgenskap.DELVIS_REFUSJON, ApiKategori.Mottaker)),
+                    egneSaker = true,
+                ),
             )
         }
         assertEquals(1, antallOppgaver)
@@ -102,11 +103,11 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
             apiOppgaveService.oppgaver(any(), any(), any(), any(), any())
         } returns ApiOppgaverTilBehandling(oppgaver = listOf(oppgaveTilBehandling()), totaltAntallOppgaver = 1)
 
-        val body =
-            runQuery(
-                """{ 
+        val body = runQuery(
+            """
+            {
                 oppgaveFeed(
-                    offset: 14, 
+                    offset: 14,
                     limit: 14,
                     sortering: [{nokkel: TIDSFRIST, stigende: true}],
                     filtrering: {
@@ -115,9 +116,10 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
                         egneSakerPaVent: false
                         ingenUkategoriserteEgenskaper: false
                     }
-                )  { oppgaver { id } }
-        }""",
-            )
+                ) { oppgaver { id } }
+            }
+            """.trimIndent(),
+        )
         val antallOppgaver = body["data"]["oppgaveFeed"].size()
 
         verify(exactly = 1) {
@@ -126,11 +128,10 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
                 offset = 14,
                 limit = 14,
                 sortering = listOf(ApiOppgavesortering(ApiSorteringsnokkel.TIDSFRIST, true)),
-                filtrering =
-                    ApiFiltrering(
-                        egenskaper = listOf(ApiOppgaveegenskap(ApiEgenskap.DELVIS_REFUSJON, ApiKategori.Mottaker)),
-                        egneSaker = true,
-                    ),
+                filtrering = ApiFiltrering(
+                    egenskaper = listOf(ApiOppgaveegenskap(ApiEgenskap.DELVIS_REFUSJON, ApiKategori.Mottaker)),
+                    egneSaker = true,
+                ),
             )
         }
         assertEquals(1, antallOppgaver)
@@ -142,17 +143,18 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
             apiOppgaveService.behandledeOppgaver(any(), any(), any(), any(), any())
         } returns ApiBehandledeOppgaver(oppgaver = listOf(behandletOppgave()), totaltAntallOppgaver = 1)
 
-        val body =
-            runQuery(
-                """{
+        val body = runQuery(
+            """
+            {
                 behandledeOppgaverFeed(
                     offset: 0,
                     limit: 14,
                     fom: "2025-06-01"
                     tom: "2025-06-02"
                 ) { oppgaver { id } }
-            }""",
-            )
+            }
+            """.trimIndent(),
+        )
         val antallOppgaver = body["data"]["behandledeOppgaverFeed"].size()
 
         verify(exactly = 1) { apiOppgaveService.behandledeOppgaver(any(), 0, 14, 1.juni(2025), 2.juni(2025)) }
@@ -165,26 +167,23 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
             apiOppgaveService.behandledeOppgaver(any(), any(), any(), any(), any())
         } returns ApiBehandledeOppgaver(oppgaver = listOf(behandletOppgave()), totaltAntallOppgaver = 1)
 
-        val body =
-            runQuery(
-                """{ 
+        val body = runQuery(
+            """
+            {
                 behandledeOppgaverFeed(
-                    offset: 14, 
+                    offset: 14,
                     limit: 14,
                     fom: "2025-06-10",
                     tom: "2025-06-13"
-                )  { oppgaver { id } }
-        }""",
-            )
+                ) { oppgaver { id } }
+            }
+            """.trimIndent(),
+        )
         val antallOppgaver = body["data"]["behandledeOppgaverFeed"].size()
 
         verify(exactly = 1) {
             apiOppgaveService.behandledeOppgaver(
-                saksbehandlerFraApi = any(),
-                offset = 14,
-                limit = 14,
-                fom = 10.juni(2025),
-                tom = 13.juni(2025)
+                saksbehandlerFraApi = any(), offset = 14, limit = 14, fom = 10.juni(2025), tom = 13.juni(2025)
             )
         }
         assertEquals(1, antallOppgaver)
@@ -192,17 +191,20 @@ class OppgaverQueryHandlerTest : AbstractGraphQLApiTest() {
 
     @Test
     fun `antallOppgaver returnerer antall oppgaver`() {
-        every { apiOppgaveService.antallOppgaver(any()) } returns ApiAntallOppgaver(antallMineSaker = 2, antallMineSakerPaVent = 1)
+        every {
+            apiOppgaveService.antallOppgaver(any())
+        } returns ApiAntallOppgaver(antallMineSaker = 2, antallMineSakerPaVent = 1)
 
-        val body =
-            runQuery(
-                """{
-            antallOppgaver {
-                antallMineSaker
-                antallMineSakerPaVent
+        val body = runQuery(
+            """
+            {
+                antallOppgaver {
+                    antallMineSaker
+                    antallMineSakerPaVent
+                }
             }
-        }""",
-            )
+            """.trimIndent(),
+        )
 
         val antallMineSaker = body["data"]["antallOppgaver"]["antallMineSaker"].asInt()
         val antallMineSakerPåVent = body["data"]["antallOppgaver"]["antallMineSakerPaVent"].asInt()
