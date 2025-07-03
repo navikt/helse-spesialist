eb59499216b9dfd13bab582c11ed52ae7b2687a9
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-07-03 16:25:19 +0200

    🤡 Stub Unleash og KRR sånn at vi ikke får warnings lokalt / i tester

--- spesialist-client-krr/src/testFixtures/kotlin/no/nav/helse/spesialist/client/krr/testfixtures/ClientKRRModuleIntegationTestFixture.kt
+++ spesialist-client-krr/src/testFixtures/kotlin/no/nav/helse/spesialist/client/krr/testfixtures/ClientKRRModuleIntegationTestFixture.kt
@@ -1,13 +1,33 @@
 package no.nav.helse.spesialist.client.krr.testfixtures
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.okJson
+import com.github.tomakehurst.wiremock.client.WireMock.post
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import no.nav.helse.spesialist.client.krr.ClientKrrModule
 
 object ClientKRRModuleIntegationTestFixture {
     private val wireMockServer: WireMockServer = WireMockServer(WireMockConfiguration.options().dynamicPort()).also(
         WireMockServer::start
-    )
+    ).also {
+        it.stubFor(
+            post("/rest/v1/personer").willReturn(
+                okJson(
+                    """
+                        {
+                          "personer": {
+                            "{{jsonPath request.body '$.personidenter[0]'}}": {
+                              "kanVarsles": true,
+                              "reservert": false
+                            }
+                          },
+                          "feil": {}
+                        }
+                        """.trimIndent()
+                ).withTransformers("response-template")
+            )
+        )
+    }
 
     val moduleConfiguration = ClientKrrModule.Configuration(
         ClientKrrModule.Configuration.Client(
--- spesialist-client-unleash/src/testFixtures/kotlin/no/nav/helse/spesialist/client/unleash/testfixtures/ClientUnleashModuleIntegrationTestFixture.kt
+++ spesialist-client-unleash/src/testFixtures/kotlin/no/nav/helse/spesialist/client/unleash/testfixtures/ClientUnleashModuleIntegrationTestFixture.kt
@@ -1,13 +1,16 @@
 package no.nav.helse.spesialist.client.unleash.testfixtures
 
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.any
+import com.github.tomakehurst.wiremock.client.WireMock.anyUrl
+import com.github.tomakehurst.wiremock.client.WireMock.notFound
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration
 import no.nav.helse.spesialist.client.unleash.ClientUnleashModule
 
 object ClientUnleashModuleIntegrationTestFixture {
     private val wireMockServer: WireMockServer = WireMockServer(WireMockConfiguration.options().dynamicPort()).also(
         WireMockServer::start
-    )
+    ).also { it.stubFor(any(anyUrl()).willReturn(notFound())) }
 
     val moduleConfiguration = ClientUnleashModule.Configuration(
         apiKey = "123abc",
