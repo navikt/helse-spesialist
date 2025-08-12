fd08f27f2c33203e4d938d2615587c77fe3c3cd4
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2025-08-12 12:59:42 +0200

    🗃️ Drop indexer som krasjer i prod

---
+++ spesialist-db-migrations/src/main/resources/db/migration/V631__drop_indexer_for_raskere_queries_for_vedtaksperiode_for_annullerte.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS idx_annullert_person_arb;
+DROP INDEX IF EXISTS idx_utbetaling_annullert;
+DROP INDEX IF EXISTS idx_behandling_utbetaling;
+DROP INDEX IF EXISTS idx_annullert_vedtak;
