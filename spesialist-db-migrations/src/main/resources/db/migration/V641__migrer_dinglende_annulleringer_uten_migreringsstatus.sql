a5285c000639b82f8e9ffea919cd8701df8ded75
Håvard Eggen <11397871+gammalost@users.noreply.github.com>
2025-09-11 11:13:43 +0200

    🗃️ Migrer annulleringer der migreringsstatus & vedtaksperiodeid er null
    Co-authored-by: Martin Remøy Solheim <3852471+martinmekk@users.noreply.github.com>

---
+++ spesialist-db-migrations/src/main/resources/db/migration/V641__migrer_dinglende_annulleringer_uten_migreringsstatus.sql
@@ -0,0 +1,18 @@
+INSERT INTO ukoblede_annulleringer (id,
+                                    annullert_tidspunkt,
+                                    saksbehandler_ref,
+                                    årsaker,
+                                    begrunnelse_ref,
+                                    arbeidsgiver_fagsystem_id,
+                                    person_fagsystem_id,
+                                    migreringsstatus)
+SELECT id,
+       annullert_tidspunkt,
+       saksbehandler_ref,
+       årsaker,
+       begrunnelse_ref,
+       arbeidsgiver_fagsystem_id,
+       person_fagsystem_id,
+       migreringsstatus
+FROM annullert_av_saksbehandler
+WHERE migreringsstatus IS NULL AND vedtaksperiode_id IS NULL;
