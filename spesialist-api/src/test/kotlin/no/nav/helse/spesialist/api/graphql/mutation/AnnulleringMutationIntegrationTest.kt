1575371b01d15f346e16d3ad2ecf35576cdb00bd
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2025-09-14 14:19:03 +0200

    ✅ Forbedre AnnulleringMutation-testene ved å sjekke om ting blir lagret

--- spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/mutation/AnnulleringMutationIntegrationTest.kt
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/graphql/mutation/AnnulleringMutationIntegrationTest.kt
@@ -2,63 +2,92 @@ package no.nav.helse.spesialist.api.graphql.mutation
 
 import no.nav.helse.spesialist.api.IntegrationTestFixture
 import no.nav.helse.spesialist.api.graphql.schema.ApiAnnulleringData
+import no.nav.helse.spesialist.api.testfixtures.lagSaksbehandler
 import no.nav.helse.spesialist.api.testfixtures.mutation.annullerMutation
 import no.nav.helse.spesialist.domain.testfixtures.lagAktørId
 import no.nav.helse.spesialist.domain.testfixtures.lagFødselsnummer
 import no.nav.helse.spesialist.domain.testfixtures.lagOrganisasjonsnummer
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertNotNull
 import java.util.UUID
 
 class AnnulleringMutationIntegrationTest {
     private val integrationTestFixture = IntegrationTestFixture()
 
+    private val annulleringRepository = integrationTestFixture.daos.annulleringRepository
+
     @Test
     fun `annullering ok`() {
+        // Given:
+        val saksbehandler = lagSaksbehandler()
+
+        val arbeidsgiverFagsystemId = "EN_ARBEIDSGIVER_FAGSYSTEM_ID"
+        val kommentar = "kommentar"
+        val personFagsystemId = "EN_PERSON_FAGSYSTEM_ID"
+        val vedtaksperiodeId = UUID.randomUUID()
+        val årsaker = listOf("Ferie", "Ekstra ferie")
+
         // When:
         val responseJson = integrationTestFixture.executeQuery(
+            saksbehandler = saksbehandler,
             query = annullerMutation(
                 ApiAnnulleringData(
                     organisasjonsnummer = lagOrganisasjonsnummer(),
                     fodselsnummer = lagFødselsnummer(),
                     aktorId = lagAktørId(),
                     utbetalingId = UUID.randomUUID(),
-                    arbeidsgiverFagsystemId = "EN-FAGSYSTEMID",
-                    personFagsystemId = "EN-FAGSYSTEMID",
-                    vedtaksperiodeId = UUID.randomUUID(),
-                    kommentar = "kommentar",
-                    arsaker = listOf(
+                    arbeidsgiverFagsystemId = arbeidsgiverFagsystemId,
+                    personFagsystemId = personFagsystemId,
+                    vedtaksperiodeId = vedtaksperiodeId,
+                    kommentar = kommentar,
+                    arsaker = årsaker.mapIndexed { index, årsak ->
                         ApiAnnulleringData.ApiAnnulleringArsak(
-                            _key = "en key",
-                            arsak = "Ferie",
-                        ),
-                        ApiAnnulleringData.ApiAnnulleringArsak(
-                            _key = "en annen key",
-                            arsak = "Ekstra ferie",
+                            _key = "årsak-$index",
+                            arsak = årsak,
                         )
-                    ),
-                )
-            ),
+                    }
+                ),
+            )
         )
 
         // Then:
         assertEquals(true, responseJson.get("data")?.get("annuller")?.asBoolean())
+
+        val lagretAnnullering = annulleringRepository.finnAnnullering(arbeidsgiverFagsystemId, personFagsystemId)
+        assertNotNull(lagretAnnullering)
+
+        assertEquals(saksbehandler.ident, lagretAnnullering.saksbehandlerIdent)
+        assertEquals(arbeidsgiverFagsystemId, lagretAnnullering.arbeidsgiverFagsystemId)
+        assertEquals(personFagsystemId, lagretAnnullering.personFagsystemId)
+        assertEquals(årsaker.sorted(), lagretAnnullering.arsaker.sorted())
+        assertEquals(kommentar, lagretAnnullering.begrunnelse)
+        assertEquals(vedtaksperiodeId, lagretAnnullering.vedtaksperiodeId)
     }
 
     @Test
     fun `annullering av tomme verdier`() {
+        // Given:
+        val saksbehandler = lagSaksbehandler()
+
+        val arbeidsgiverFagsystemId = "EN_ARBEIDSGIVER_FAGSYSTEM_ID"
+        val kommentar = "kommentar"
+        val personFagsystemId = "EN_PERSON_FAGSYSTEM_ID"
+        val vedtaksperiodeId = UUID.randomUUID()
+
         // When:
         val responseJson = integrationTestFixture.executeQuery(
+            saksbehandler = saksbehandler,
             query = annullerMutation(
                 ApiAnnulleringData(
                     organisasjonsnummer = lagOrganisasjonsnummer(),
                     fodselsnummer = lagFødselsnummer(),
                     aktorId = lagAktørId(),
                     utbetalingId = UUID.randomUUID(),
-                    arbeidsgiverFagsystemId = "EN-FAGSYSTEMID",
-                    personFagsystemId = "EN-FAGSYSTEMID",
-                    vedtaksperiodeId = UUID.randomUUID(),
-                    kommentar = "kommentar",
+                    arbeidsgiverFagsystemId = arbeidsgiverFagsystemId,
+                    personFagsystemId = personFagsystemId,
+                    vedtaksperiodeId = vedtaksperiodeId,
+                    kommentar = kommentar,
                     arsaker = emptyList(),
                 )
             ),
@@ -66,5 +95,15 @@ class AnnulleringMutationIntegrationTest {
 
         // Then:
         assertEquals(true, responseJson.get("data")?.get("annuller")?.asBoolean())
+
+        val lagretAnnullering = annulleringRepository.finnAnnullering(arbeidsgiverFagsystemId, personFagsystemId)
+        assertNotNull(lagretAnnullering)
+
+        assertEquals(saksbehandler.ident, lagretAnnullering.saksbehandlerIdent)
+        assertEquals(arbeidsgiverFagsystemId, lagretAnnullering.arbeidsgiverFagsystemId)
+        assertEquals(personFagsystemId, lagretAnnullering.personFagsystemId)
+        assertEquals(emptyList<String>(), lagretAnnullering.arsaker)
+        assertEquals(kommentar, lagretAnnullering.begrunnelse)
+        assertEquals(vedtaksperiodeId, lagretAnnullering.vedtaksperiodeId)
     }
 }
