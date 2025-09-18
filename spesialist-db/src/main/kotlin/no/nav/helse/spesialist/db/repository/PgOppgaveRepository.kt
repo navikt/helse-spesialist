c78e152a6e16405aa50bcca83c47b6269bf61c73
Håvard Eggen <11397871+gammalost@users.noreply.github.com>
2025-09-18 14:34:11 +0200

    Revert "🗃️ Setter første opprettet ved id-konflikt"
    This reverts commit a910e9bd97e37ab5b38eb5cbc9b26665dd1101bb.

--- spesialist-db/src/main/kotlin/no/nav/helse/spesialist/db/repository/PgOppgaveRepository.kt
+++ spesialist-db/src/main/kotlin/no/nav/helse/spesialist/db/repository/PgOppgaveRepository.kt
@@ -95,8 +95,7 @@ class PgOppgaveRepository private constructor(
                 ferdigstilt_av_oid = excluded.ferdigstilt_av_oid,
                 egenskaper = excluded.egenskaper,
                 kan_avvises = excluded.kan_avvises,
-                hendelse_id_godkjenningsbehov = excluded.hendelse_id_godkjenningsbehov,
-                første_opprettet = excluded.første_opprettet
+                hendelse_id_godkjenningsbehov = excluded.hendelse_id_godkjenningsbehov
             """,
             "id" to oppgave.id,
             "opprettet" to oppgave.opprettet,
