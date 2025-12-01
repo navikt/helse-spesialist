1306a3e83979f9d87f4a138605628bca640ba0a2
Christian Skovborg Gule <christianskovborg@gmail.com>
2025-12-01 12:39:04 +0100

    🔥 Fjern toggles fra AbstractDatabaseTest

--- spesialist-e2e-tests/src/test/kotlin/no/nav/helse/e2e/AbstractDatabaseTest.kt
+++ spesialist-e2e-tests/src/test/kotlin/no/nav/helse/e2e/AbstractDatabaseTest.kt
@@ -1,14 +1,8 @@
 package no.nav.helse.e2e
 
-import no.nav.helse.bootstrap.EnvironmentToggles
 import no.nav.helse.spesialist.db.testfixtures.DBTestFixture
 
 abstract class AbstractDatabaseTest {
-    protected val environmentToggles: EnvironmentToggles = object : EnvironmentToggles, Map<String, String> by emptyMap() {
-        override val kanBeslutteEgneSaker = false
-        override val kanGodkjenneUtenBesluttertilgang = false
-    }
-
     protected val dataSource = DBTestFixture.module.dataSource
     protected val daos = DBTestFixture.module.daos
     protected val sessionFactory = DBTestFixture.module.sessionFactory
