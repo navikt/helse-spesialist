9e4e4d19213a91d24153ad00a38d717a9d6f14f6
Håvard Eggen <11397871+gammalost@users.noreply.github.com>
2025-12-18 12:42:18 +0100

    🗃️ Sjekk at bruker finnes før grant gjøres på tabell
    Feiler lokalt ellers.

--- spesialist-db-migrations/src/main/resources/db/migration/V662__vedtak_grant_iamuser.sql
+++ spesialist-db-migrations/src/main/resources/db/migration/V662__vedtak_grant_iamuser.sql
@@ -1,1 +1,9 @@
-grant delete, insert, references, select, trigger, truncate, update on vedtak to cloudsqliamuser;
+DO
+$$
+    BEGIN
+        IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'cloudsqliamuser')
+        THEN
+            grant delete, insert, references, select, trigger, truncate, update on vedtak to cloudsqliamuser;
+        END IF;
+    END
+$$;
