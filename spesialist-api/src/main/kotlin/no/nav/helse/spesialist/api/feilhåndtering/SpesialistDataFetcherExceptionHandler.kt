bf1a25e91f449baf714b7c22c9e2b53fa24e0713
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-01-29 09:39:38 +0100

    🔊 Logg klientfeil fra GraphQL som warn heller enn error

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/feilhåndtering/SpesialistDataFetcherExceptionHandler.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/feilhåndtering/SpesialistDataFetcherExceptionHandler.kt
@@ -1,14 +1,21 @@
 package no.nav.helse.spesialist.api.feilhåndtering
 
 import graphql.ExceptionWhileDataFetching
+import graphql.GraphqlErrorException
 import graphql.execution.SimpleDataFetcherExceptionHandler
 import no.nav.helse.spesialist.application.logg.loggThrowable
+import no.nav.helse.spesialist.application.logg.loggWarnThrowable
 
 class SpesialistDataFetcherExceptionHandler : SimpleDataFetcherExceptionHandler() {
     override fun logException(
         error: ExceptionWhileDataFetching,
         exception: Throwable,
     ) {
-        loggThrowable("Uhåndtert feil ved GraphQL-kall til ${error.path}", exception)
+        val httpCode = (exception as? GraphqlErrorException)?.extensions?.get("code") as? Int
+        if (httpCode != null && (httpCode !in 400..499)) {
+            loggWarnThrowable("Returnerer klientfeil (kode ${httpCode}) for GraphQL-kall til ${error.path}", exception)
+        } else {
+            loggThrowable("Uhåndtert feil ved GraphQL-kall til ${error.path}", exception)
+        }
     }
 }
