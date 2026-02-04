48ff0a07727cd9cd7966b2be48bd2a0b76e782c6
Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
2026-02-04 13:48:12 +0100

    ✅ Må ta med fiks av feilende test også..
    Co-authored-by: Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>

--- spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/rest/GetBrukerrollerIntegrationTest.kt
+++ spesialist-api/src/test/kotlin/no/nav/helse/spesialist/api/rest/GetBrukerrollerIntegrationTest.kt
@@ -3,8 +3,6 @@ package no.nav.helse.spesialist.api.rest
 import io.ktor.http.HttpStatusCode
 import no.nav.helse.spesialist.api.IntegrationTestFixture
 import no.nav.helse.spesialist.domain.tilgangskontroll.Tilgang
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.EnumSource
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -19,19 +17,7 @@ class GetBrukerrollerIntegrationTest {
 
         // Then: Responsen inneholder de forventede rollene
         val roller = response.body<Set<String>>()
-        assertEquals(Tilgang.entries.map { it.name }.toSet(), roller)
-    }
-
-    @ParameterizedTest
-    @EnumSource(Tilgang::class)
-    fun `alle roller har tilgang til endepunktet`(tilgang: Tilgang) {
-        // When: Kaller GET /api/brukerroller
-        val response = integrationTestFixture.get("api/brukerroller", tilganger = setOf(tilgang))
-        assertEquals(HttpStatusCode.OK.value, response.status)
-
-        // Then: Responsen inneholder de forventede rollene
-        val roller = response.body<Set<String>>()
-        assertEquals(setOf(tilgang.name), roller)
+        assertEquals(setOf("LESETILGANG", "SAKSBEHANDLER"), roller)
     }
 
     @Test
