df809d6056a32e1b0c122c12f41e7434146ebdbd
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-02-08 14:42:16 +0100

    ✨ Ikke logg duplikate meldinger fra OpenSearch-logg til team logs

--- spesialist-application/src/main/kotlin/no/nav/helse/spesialist/application/logg/Loggers.kt
+++ spesialist-application/src/main/kotlin/no/nav/helse/spesialist/application/logg/Loggers.kt
@@ -72,6 +72,7 @@ fun loggMedDetaljer(
     logger
         .atLevel(level)
         .setMessage(melding)
+        .addMarker(SKIP_TEAM_LOGS_MARKER)
         .log()
     teamLogs
         .atLevel(level)
