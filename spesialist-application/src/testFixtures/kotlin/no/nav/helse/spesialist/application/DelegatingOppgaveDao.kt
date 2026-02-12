8957bc2830010c63df3a6e2f0cb3f9059542bcf9
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-02-12 09:23:15 +0100

    ♻️ Ta bort Partial-implementasjoner av Dao'er

--- spesialist-application/src/testFixtures/kotlin/no/nav/helse/spesialist/application/DelegatingOppgaveDao.kt
+++ spesialist-application/src/testFixtures/kotlin/no/nav/helse/spesialist/application/DelegatingOppgaveDao.kt
@@ -1,15 +1,19 @@
 package no.nav.helse.spesialist.application
 
-import no.nav.helse.db.PartialOppgaveDao
+import no.nav.helse.db.AntallOppgaverFraDatabase
+import no.nav.helse.db.BehandletOppgaveFraDatabaseForVisning
+import no.nav.helse.db.EgenskapForDatabase
+import no.nav.helse.db.OppgaveDao
 import no.nav.helse.modell.oppgave.Oppgave
 import no.nav.helse.spesialist.domain.BehandlingUnikId
+import java.time.LocalDate
 import java.util.UUID
 
 class DelegatingOppgaveDao(
     private val oppgaveRepository: InMemoryOppgaveRepository,
     private val behandlingRepository: InMemoryBehandlingRepository,
     private val vedtaksperiodeRepository: InMemoryVedtaksperiodeRepository
-) : PartialOppgaveDao {
+) : OppgaveDao {
     override fun finnOppgaveId(fødselsnummer: String): Long? {
         val vedtaksperiodeIder =
             vedtaksperiodeRepository.alle().filter { it.identitetsnummer.value == fødselsnummer }.map { it.id.value }
@@ -31,5 +35,65 @@ class DelegatingOppgaveDao(
         return oppgaveRepository.alle()
             .filter { it.vedtaksperiodeId in vedtaksperiodeIder }
             .maxOf { it.id }
+    }
+
+    override fun finnBehandlingId(oppgaveId: Long): UUID {
+        error("Not implemented for this test")
+    }
+
+    override fun finnOppgaveId(utbetalingId: UUID): Long? {
+        error("Not implemented for this test")
+    }
+
+    override fun finnVedtaksperiodeId(oppgaveId: Long): UUID {
+        error("Not implemented for this test")
+    }
+
+    override fun invaliderOppgave(oppgaveId: Long) {
+        error("Not implemented for this test")
+    }
+
+    override fun reserverNesteId(): Long {
+        error("Not implemented for this test")
+    }
+
+    override fun finnAntallOppgaver(saksbehandlerOid: UUID): AntallOppgaverFraDatabase {
+        error("Not implemented for this test")
+    }
+
+    override fun finnBehandledeOppgaver(
+        behandletAvOid: UUID,
+        offset: Int,
+        limit: Int,
+        fom: LocalDate,
+        tom: LocalDate,
+    ): List<BehandletOppgaveFraDatabaseForVisning> {
+        error("Not implemented for this test")
+    }
+
+    override fun finnEgenskaper(
+        vedtaksperiodeId: UUID,
+        utbetalingId: UUID,
+    ): Set<EgenskapForDatabase>? {
+        error("Not implemented for this test")
+    }
+
+    override fun finnIdForAktivOppgave(vedtaksperiodeId: UUID): Long? {
+        error("Not implemented for this test")
+    }
+
+    override fun finnFødselsnummer(oppgaveId: Long): String {
+        error("Not implemented for this test")
+    }
+
+    override fun harFerdigstiltOppgave(vedtaksperiodeId: UUID): Boolean {
+        error("Not implemented for this test")
+    }
+
+    override fun oppdaterPekerTilGodkjenningsbehov(
+        godkjenningsbehovId: UUID,
+        utbetalingId: UUID,
+    ) {
+        error("Not implemented for this test")
     }
 }
