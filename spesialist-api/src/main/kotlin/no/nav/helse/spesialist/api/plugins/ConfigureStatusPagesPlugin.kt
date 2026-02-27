5f0151f34195932bc6a53bd55dd39fde3f724618
Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
2026-02-27 11:07:36 +0100

    🐛 Må visst fange CancellationException i status pages
    Når coroutine'n i sse-endepunktet blir kansellert kastes det en
    CancellationException. Denne må fanges i Status Pages for å ikke lage
    unødvendig støy i loggene.
    
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/plugins/ConfigureStatusPagesPlugin.kt
+++ spesialist-api/src/main/kotlin/no/nav/helse/spesialist/api/plugins/ConfigureStatusPagesPlugin.kt
@@ -9,11 +9,18 @@ import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
 import no.nav.helse.spesialist.api.graphql.Modellfeil
 import no.nav.helse.spesialist.application.logg.loggError
+import no.nav.helse.spesialist.application.logg.loggInfo
+import java.util.concurrent.CancellationException
 
 fun StatusPagesConfig.configureStatusPagesPlugin() {
     exception<Modellfeil> { call: ApplicationCall, modellfeil: Modellfeil ->
         modellfeil.logger()
         call.respond(status = modellfeil.httpkode, message = modellfeil.tilFeilDto())
+    }
+    exception<CancellationException> { call: ApplicationCall, exception: CancellationException ->
+        val uri = call.request.uri
+        if (!uri.endsWith("/sse")) return@exception onThrowable(call, exception)
+        loggInfo("SSE-tilkobling lukket")
     }
     exception<Throwable> { call, cause ->
         onThrowable(call, cause)
