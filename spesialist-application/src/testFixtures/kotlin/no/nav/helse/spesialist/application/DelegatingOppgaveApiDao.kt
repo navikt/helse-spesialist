074e0b1fd9ad47d2f5890f09df75acc26b1c96ca
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-03-02 18:37:53 +0100

    ♻️ Implementer DelegatingOppgaveApiDao

--- spesialist-application/src/testFixtures/kotlin/no/nav/helse/spesialist/application/DelegatingOppgaveApiDao.kt
+++ spesialist-application/src/testFixtures/kotlin/no/nav/helse/spesialist/application/DelegatingOppgaveApiDao.kt
@@ -1,16 +1,25 @@
 package no.nav.helse.spesialist.application
 
 import no.nav.helse.db.api.OppgaveApiDao
+import no.nav.helse.modell.oppgave.Oppgave
 import no.nav.helse.spesialist.api.oppgave.OppgaveForPeriodevisningDto
 import java.util.UUID
 
-class DelegatingOppgaveApiDao : OppgaveApiDao {
+class DelegatingOppgaveApiDao(
+    private val oppgaveRepository: InMemoryOppgaveRepository,
+    private val vedtaksperiodeRepository: InMemoryVedtaksperiodeRepository,
+) : OppgaveApiDao {
     override fun finnOppgaveId(fødselsnummer: String): Long? {
-        TODO("Not yet implemented")
+        val vedtaksperiodeIder =
+            vedtaksperiodeRepository.alle().filter { it.identitetsnummer.value == fødselsnummer }.map { it.id.value }
+        return oppgaveRepository.alle()
+            .filter { it.vedtaksperiodeId in vedtaksperiodeIder }
+            .firstOrNull { it.tilstand is Oppgave.AvventerSaksbehandler }
+            ?.id
     }
 
-    override fun finnPeriodeoppgave(vedtaksperiodeId: UUID): OppgaveForPeriodevisningDto? {
-        TODO("Not yet implemented")
-    }
-
+    override fun finnPeriodeoppgave(vedtaksperiodeId: UUID): OppgaveForPeriodevisningDto? =
+        oppgaveRepository.alle()
+            .firstOrNull { it.vedtaksperiodeId == vedtaksperiodeId && it.tilstand is Oppgave.AvventerSaksbehandler }
+            ?.let { OppgaveForPeriodevisningDto(id = it.id.toString(), kanAvvises = it.kanAvvises) }
 }
