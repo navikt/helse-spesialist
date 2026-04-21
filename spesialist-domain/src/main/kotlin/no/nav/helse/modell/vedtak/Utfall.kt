788fecac139c61bc6d59b141551f72bee2475115
Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
2026-04-21 12:44:28 +0200

    ♻️ Fjern ubrukt VedtakBegrunnelse-klasse

--- spesialist-domain/src/main/kotlin/no/nav/helse/modell/vedtak/Utfall.kt
+++ spesialist-domain/src/main/kotlin/no/nav/helse/modell/vedtak/Utfall.kt
@@ -1,10 +1,5 @@
 package no.nav.helse.modell.vedtak
 
-data class VedtakBegrunnelse(
-    val utfall: Utfall,
-    val begrunnelse: String?,
-)
-
 enum class Utfall {
     AVSLAG,
     DELVIS_INNVILGELSE,
