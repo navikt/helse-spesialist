accfa96e7f9dadf0e2ee0917c4093912085aeee9
Martin Remøy Solheim <3852471+MartinMekk@users.noreply.github.com>
2026-08-28 13:47:56 +0200

    Revert "🔥 Fjern gammle E2E-tester for overstyring"
    This reverts commit d850ca9ac63206f315b7267fb6a1ac8fe2b1dc57.

---
+++ spesialist-e2e-tests/src/test/kotlin/no/nav/helse/e2e/TotrinnsvurderingE2ETest.kt
@@ -0,0 +1,91 @@
+package no.nav.helse.e2e
+
+import no.nav.helse.spesialist.api.graphql.schema.ApiLovhjemmel
+import no.nav.helse.spesialist.api.graphql.schema.ApiOverstyringArbeidsforhold
+import no.nav.helse.spesialist.domain.testfixtures.lagOrganisasjonsnummer
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class TotrinnsvurderingE2ETest : AbstractE2ETest() {
+    @Test
+    fun `sak blir trukket til totrinnsvurdering ved overstyring av inntekt og refusjon`() {
+        vedtaksløsningenMottarNySøknad()
+        spleisOppretterNyBehandling()
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave()
+        håndterOverstyrInntektOgRefusjon(vedtaksperiodeId = VEDTAKSPERIODE_ID)
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave(
+            harRisikovurdering = true,
+            harOppdatertMetadata = true,
+            godkjenningsbehovTestdata = godkjenningsbehovTestdata.copy(utbetalingId = UUID.randomUUID()),
+        )
+
+        assertOverstyringer(FØDSELSNUMMER)
+        assertTotrinnsvurdering(2.oppgave(VEDTAKSPERIODE_ID))
+    }
+
+    @Test
+    fun `sak blir trukket til totrinnsvurdering ved overstyring av arbeidsforhold`() {
+        val orgnrGhost = lagOrganisasjonsnummer()
+        vedtaksløsningenMottarNySøknad()
+        spleisOppretterNyBehandling()
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave(
+            godkjenningsbehovTestdata =
+                godkjenningsbehovTestdata.copy(
+                    orgnummereMedRelevanteArbeidsforhold = listOf(orgnrGhost),
+                ),
+        )
+        håndterOverstyrArbeidsforhold(
+            vedtaksperiodeId = VEDTAKSPERIODE_ID,
+            overstyrteArbeidsforhold =
+                listOf(
+                    ApiOverstyringArbeidsforhold(
+                        orgnummer = orgnrGhost,
+                        deaktivert = true,
+                        begrunnelse = "begrunnelse",
+                        forklaring = "forklaring",
+                        lovhjemmel = ApiLovhjemmel("8-15", null, null, "folketrygdloven", "1998-12-18"),
+                    ),
+                ),
+        )
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave(
+            harRisikovurdering = true,
+            harOppdatertMetadata = true,
+            godkjenningsbehovTestdata = godkjenningsbehovTestdata.copy(utbetalingId = UUID.randomUUID()),
+        )
+
+        assertOverstyringer(FØDSELSNUMMER)
+        assertTotrinnsvurdering(2.oppgave(VEDTAKSPERIODE_ID))
+    }
+
+    @Test
+    fun `sak blir trukket til totrinnsvurdering ved overstyring av tidslinje`() {
+        vedtaksløsningenMottarNySøknad()
+        spleisOppretterNyBehandling()
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave()
+        håndterOverstyrTidslinje(vedtaksperiodeId = VEDTAKSPERIODE_ID)
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave(
+            harRisikovurdering = true,
+            harOppdatertMetadata = true,
+            godkjenningsbehovTestdata = godkjenningsbehovTestdata.copy(utbetalingId = UUID.randomUUID()),
+        )
+
+        assertOverstyringer(FØDSELSNUMMER)
+        assertTotrinnsvurdering(2.oppgave(VEDTAKSPERIODE_ID))
+    }
+
+    @Test
+    fun `Totrinns får vedtaksperiodeForkastet satt til true hvis vedtaksperiodene overstyringene er knyttet til blir forkastet`() {
+        vedtaksløsningenMottarNySøknad()
+        spleisOppretterNyBehandling()
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave()
+        håndterOverstyrTidslinje(vedtaksperiodeId = VEDTAKSPERIODE_ID)
+        spesialistBehandlerGodkjenningsbehovFremTilOppgave(
+            harRisikovurdering = true,
+            harOppdatertMetadata = true,
+            godkjenningsbehovTestdata = godkjenningsbehovTestdata.copy(utbetalingId = UUID.randomUUID()),
+        )
+        håndterVedtaksperiodeForkastet(vedtaksperiodeId = VEDTAKSPERIODE_ID)
+
+        assertTotrinnsvurderingForkastet(FØDSELSNUMMER)
+    }
+}
