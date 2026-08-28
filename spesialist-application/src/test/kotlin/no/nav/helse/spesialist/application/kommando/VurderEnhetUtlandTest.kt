f69495189fb752513598708bd394a434b1ea1e64
Nourhan Ragab <16683497+nurrebass1@users.noreply.github.com>
2026-08-28 11:11:23 +0200

    ♻️ VurderEnhetUtland bruker Varsel direkte istedenfor via Sykefraværstilfelle
    Co-authored-by: Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>

--- spesialist-application/src/test/kotlin/no/nav/helse/spesialist/application/kommando/VurderEnhetUtlandTest.kt
+++ spesialist-application/src/test/kotlin/no/nav/helse/spesialist/application/kommando/VurderEnhetUtlandTest.kt
@@ -1,47 +1,57 @@
 package no.nav.helse.spesialist.application.kommando
 
-import io.mockk.mockk
-import io.mockk.slot
-import io.mockk.verify
 import no.nav.helse.modell.kommando.CommandContext
-import no.nav.helse.modell.person.Sykefraværstilfelle
-import no.nav.helse.modell.person.vedtaksperiode.LegacyVarsel
 import no.nav.helse.modell.varsel.VurderEnhetUtland
 import no.nav.helse.spesialist.domain.Person
+import no.nav.helse.spesialist.domain.VedtaksperiodeId
+import no.nav.helse.spesialist.domain.testfixtures.lagBehandling
+import no.nav.helse.spesialist.domain.testfixtures.lagVedtaksperiode
 import no.nav.helse.spesialist.domain.testfixtures.testdata.lagPerson
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import java.util.UUID
+import java.util.*
 
 internal class VurderEnhetUtlandTest : ApplicationTest() {
     private val commandContext: CommandContext = CommandContext(UUID.randomUUID())
 
-    private val sykefraværstilfelle = mockk<Sykefraværstilfelle>(relaxed = true)
+    fun settOppTestdata(enhet: Int): Person {
+        val person =
+            lagPerson(enhet = enhet)
+                .also(sessionContext.personRepository::lagre)
+        val vedtaksperiode =
+            lagVedtaksperiode(id = VedtaksperiodeId(vedtaksperiodeId))
+                .also(sessionContext.vedtaksperiodeRepository::lagre)
+        lagBehandling(vedtaksperiodeId = vedtaksperiode.id)
+            .also(sessionContext.behandlingRepository::lagre)
 
-    @Test
-    fun `skal legge på varsel om utland`() {
-        val person = lagPerson(enhet = 393).also(sessionContext.personRepository::lagre)
-        val slot = slot<LegacyVarsel>()
-        assertTrue(hentCommand(person).execute(commandContext, sessionContext, outbox))
-        verify(exactly = 1) { sykefraværstilfelle.håndter(capture(slot)) }
-        assertEquals("SB_EX_5", slot.captured.toDto().varselkode)
+        return person
     }
 
     @Test
-    fun `skal legge på varsel for utland også ved revurdering`() {
-        val person = lagPerson(enhet = 393).also(sessionContext.personRepository::lagre)
-        val slot = slot<LegacyVarsel>()
+    fun `skal legge på varsel om utland`() {
+        val person = settOppTestdata(enhet = 393)
         assertTrue(hentCommand(person).execute(commandContext, sessionContext, outbox))
-        verify(exactly = 1) { sykefraværstilfelle.håndter(capture(slot)) }
-        assertEquals("SB_EX_5", slot.captured.toDto().varselkode)
+        assertEquals(
+            "SB_EX_5",
+            sessionContext.varselRepository
+                .alle()
+                .single()
+                .kode,
+        )
+    }
+
+    @Test
+    fun `legger ikke på varsel om utland for enhet som ikke er utland`() {
+        val person = settOppTestdata(enhet = 1001)
+        assertTrue(hentCommand(person).execute(commandContext, sessionContext, outbox))
+        assertEquals(0, sessionContext.varselRepository.alle().size)
     }
 
     private fun hentCommand(person: Person) =
         VurderEnhetUtland(
             fødselsnummer = person.id.value,
-            vedtaksperiodeId = vedtaksperiodeId,
-            sykefraværstilfelle = sykefraværstilfelle,
+            vedtaksperiodeId = VedtaksperiodeId(vedtaksperiodeId),
         )
 
     private companion object {
