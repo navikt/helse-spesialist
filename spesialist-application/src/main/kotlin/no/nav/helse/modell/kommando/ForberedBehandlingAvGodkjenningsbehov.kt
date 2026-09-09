eeaa5475989ee18be12fbbe311b0600f38258c00
Christian Skovborg Gule <59872984+chsko@users.noreply.github.com>
2026-09-09 08:22:23 +0200

    ♻️ Samle oppførsel i fornuftig navngitte funksjoner

--- spesialist-application/src/main/kotlin/no/nav/helse/modell/kommando/ForberedBehandlingAvGodkjenningsbehov.kt
+++ spesialist-application/src/main/kotlin/no/nav/helse/modell/kommando/ForberedBehandlingAvGodkjenningsbehov.kt
@@ -3,6 +3,7 @@ package no.nav.helse.modell.kommando
 import no.nav.helse.db.SessionContext
 import no.nav.helse.modell.vedtaksperiode.GodkjenningsbehovData
 import no.nav.helse.spesialist.application.Outbox
+import no.nav.helse.spesialist.domain.Behandling
 import no.nav.helse.spesialist.domain.SpleisBehandlingId
 import no.nav.helse.spesialist.domain.UtbetalingId
 
@@ -14,6 +15,34 @@ internal class ForberedBehandlingAvGodkjenningsbehov(
         sessionContext: SessionContext,
         outbox: Outbox,
     ): Boolean {
+        oppdaterDatoer(sessionContext)
+        val behandlingTilGodkjenning = sessionContext.behandlingRepository.finn(SpleisBehandlingId(godkjenningsbehovData.spleisBehandlingId)) ?: error("Fant ikke behandling med id ${godkjenningsbehovData.spleisBehandlingId}")
+
+        flyttKanskjeAvviksvarsel(sessionContext, behandlingTilGodkjenning)
+        behandlingTilGodkjenning.oppdaterTags(godkjenningsbehovData.tags)
+        behandlingTilGodkjenning.oppdaterUtbetalingId(UtbetalingId(godkjenningsbehovData.utbetalingId))
+        sessionContext.behandlingRepository.lagre(behandlingTilGodkjenning)
+
+        return true
+    }
+
+    private fun flyttKanskjeAvviksvarsel(
+        sessionContext: SessionContext,
+        behandling: Behandling,
+    ) {
+        val andreBehandlinger = sessionContext.behandlingRepository.finnAndreBehandlingerISykefraværstilfelle(behandling, godkjenningsbehovData.fødselsnummer)
+
+        val varslerForAndreBehandlinger = sessionContext.varselRepository.finnAktiveVarslerFor(andreBehandlinger)
+
+        val avviksvarsel = varslerForAndreBehandlinger.find { it.erVarselOmAvvik() }
+
+        if (avviksvarsel != null) {
+            avviksvarsel.flyttTil(behandling.id, behandling.spleisBehandlingId)
+            sessionContext.varselRepository.lagre(avviksvarsel)
+        }
+    }
+
+    private fun oppdaterDatoer(sessionContext: SessionContext) {
         godkjenningsbehovData.spleisVedtaksperioder
             .mapNotNull { spleisVedtaksperiode ->
                 sessionContext.behandlingRepository.finn(SpleisBehandlingId(spleisVedtaksperiode.spleisBehandlingId))?.let {
@@ -23,22 +52,5 @@ internal class ForberedBehandlingAvGodkjenningsbehov(
                 behandling.oppdaterDatoer(fom = spleisVedtaksperiode.fom, tom = spleisVedtaksperiode.tom, skjæringstidspunkt = spleisVedtaksperiode.skjæringstidspunkt)
                 sessionContext.behandlingRepository.lagre(behandling)
             }
-
-        val behandlingTilGodkjenning = sessionContext.behandlingRepository.finn(SpleisBehandlingId(godkjenningsbehovData.spleisBehandlingId)) ?: error("Fant ikke behandling med id ${godkjenningsbehovData.spleisBehandlingId}")
-        val andreBehandlinger = sessionContext.behandlingRepository.finnAndreBehandlingerISykefraværstilfelle(behandlingTilGodkjenning, godkjenningsbehovData.fødselsnummer)
-
-        val varslerForAndreBehandlinger = sessionContext.varselRepository.finnAktiveVarslerFor(andreBehandlinger)
-
-        val avviksvarsel = varslerForAndreBehandlinger.find { it.erVarselOmAvvik() }
-
-        if (avviksvarsel != null) {
-            avviksvarsel.flyttTil(behandlingTilGodkjenning.id, behandlingTilGodkjenning.spleisBehandlingId)
-            sessionContext.varselRepository.lagre(avviksvarsel)
-        }
-        behandlingTilGodkjenning.oppdaterTags(godkjenningsbehovData.tags)
-        behandlingTilGodkjenning.oppdaterUtbetalingId(UtbetalingId(godkjenningsbehovData.utbetalingId))
-        sessionContext.behandlingRepository.lagre(behandlingTilGodkjenning)
-
-        return true
     }
 }
